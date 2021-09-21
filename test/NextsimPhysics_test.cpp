1b555c0fd63964efddaa0990bd1f112bcab45f02
Tim Spain <timothy.spain@nersc.no>
2021-09-21 16:34:33 +0200

    Edits to make the test functional.

--- test/NextsimPhysics_test.cpp
+++ test/NextsimPhysics_test.cpp
@@ -1,12 +1,29 @@
 /*
- * NextsimPhysics_test.cpp
+ * @file NextsimPhysics_test.cpp
  *
- *  Created on: Sep 9, 2021
- *      Author: timpai
+ * @date Sep 9, 2021
+ * @author Tim Spain
  */
 
-#include <NextsimPhysics.hpp>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
+#include "../src/include/NextsimPhysics.hpp"
+#include "../src/include/ElementData.hpp"
+#include "../src/include/constants.hpp"
 namespace Nextsim {
 
+TEST_CASE("Outgoing LW (OW)", "[NextsimPhysics]")
+{
+    ElementData<NextsimPhysics> data;
+
+    const double t = 280.; // kelvin
+    data = PrognosticData::generate(0., 0., celsius(t), 0.);
+
+    NextsimPhysics::heatFluxOpenWater(data);
+
+    double target = PhysicalConstants::sigma * t * t * t * t;
+
+    REQUIRE(data.QLongwave() == target);
+}
 } /* namespace Nextsim */
