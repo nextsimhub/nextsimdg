6dcc170945e7c1429ad6b52026f0a5491bbd4684
Tim Spain <timothy.spain@nersc.no>
2021-10-08 15:06:22 +0200

    Write a real test for the CommandLineParser class.

--- test/CommandLineParser_test.cpp
+++ test/CommandLineParser_test.cpp
@@ -7,14 +7,60 @@
 
 #include "CommandLineParser.hpp"
 
+#include <vector>
+#include <string>
+
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
 namespace Nextsim {
 
+// A class to work around the limitations on casting string literals to char*.
+class ArgV {
+public:
+    ArgV(std::vector<std::string> vs)
+    {
+        nStrings = vs.size();
+        ppc = new char*[nStrings];
+        for (int i = 0; i < nStrings; ++i) {
+            int len = vs[i].size();
+            char* pc = new char[len + 1];
+            for (int j = 0; j < len; ++j) {
+                pc[j] = vs[i][j];
+            }
+            pc[len] = '\0';
+            ppc[i] = pc;
+        }
+
+    }
+    ~ArgV()
+    {
+        for (int i = 0; i < nStrings; ++i) {
+            delete[] ppc[i];
+        }
+        delete[] ppc;
+    }
+
+    char** operator()()
+    {
+        return ppc;
+    }
+private:
+    int nStrings;
+    char** ppc;
+};
+
 TEST_CASE("Parse config file names", "[CommandLineParser]")
 {
-    REQUIRE(true);
+    // Parse one file
+    const int argc1 = 4;
+    ArgV argv1({"nextsimdg", "--config-file", "config.cfg", ""});
+
+    CommandLineParser clp1(argc1, argv1());
+    std::vector<std::string> cfgs = clp1.getConfigFileNames();
+
+    REQUIRE(cfgs.size() == 1);
+    REQUIRE(cfgs[0] == std::string(argv1()[argc1 - 2]));
 }
 
 } /* namespace Nextsim */
