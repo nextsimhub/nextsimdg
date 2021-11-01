020d93e1281aaa258225d5d95825be05fb11b3b8
Tim Spain <timothy.spain@nersc.no>
2021-11-01 11:12:41 +0100

    Import the changes to ModuleLoader.

--- src/include/ModuleLoader.hpp
+++ src/include/ModuleLoader.hpp
@@ -8,22 +8,24 @@
 #ifndef SRC_INCLUDE_MODULELOADER_HPP
 #define SRC_INCLUDE_MODULELOADER_HPP
 
+#include <map>
 #include <memory>
 #include <string>
 #include <set>
-#include <boost/program_options.hpp>
 
 class ModuleLoader {
 public:
     static ModuleLoader& getLoader()
     {
         static ModuleLoader instance; // C++11 magic static
+        instance.init();
         return instance;
     }
 
     //typedef boost::program_options::variables_map VariablesMap;
     typedef std::map<std::string, std::string> VariablesMap;
 
+    void init();
     void init(const VariablesMap&);
     inline const std::set<std::string>& listModules() const
     {
@@ -34,17 +36,23 @@ public:
         return m_availableImplementationNames.at(module);
     }
     template<class T>
-    std::unique_ptr<T> getImplementation() const;
+    std::unique_ptr<T> getInstance() const;
+
+    template<class T>
+    T& getImplementation();
 
     void setImplementation(const std::string& module, const std::string& impl);
     // Singleton function definitions
 private:
     ModuleLoader() {};
+
 public:
     ModuleLoader(const ModuleLoader&)   = delete;
     void operator=(const ModuleLoader&) = delete;
 
 private:
+    // Make sure initialization happens
+    bool isInit = false;
     // One module could have many names (but probably shouldn't)
     std::set<std::string> m_modules;
     // Names of available implementations
