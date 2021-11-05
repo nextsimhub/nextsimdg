99bbeff16af76a09b556bb712c64fd919d67cca6
Piotr Minakowski <piotr.minakowski@ovgu.de>
2021-11-05 15:03:46 +0100

    Testing Timer from feat_elementdata on example1

--- dynamics/tests/example1.cpp
+++ dynamics/tests/example1.cpp
@@ -9,13 +9,16 @@
 #include "timemesh.hpp"
 #include "dgvisu.hpp"
 #include "dgtransport.hpp"
-#include "stopwatch.hpp"
+//#include "stopwatch.hpp"
 #include "testtools.hpp"
 
-namespace Nextsim
+
+#include "../../src/include/Timer.hpp"
+
+/*namespace Nextsim
 {
   extern Timer GlobalTimer;
-}
+}*/
 
 
 bool WRITE_VTK = false; //!< set to true for vtk output
@@ -27,23 +30,23 @@ double TOL = 1.e-10;    //!< tolerance for checking test results
 //! Initially a smooth bump centered at (0.4,0.4)
 //! This will be transported in a circle with (-y, x) for one complete revolution
 class InitialPhi : virtual public Nextsim::InitialBase {
-  
+
 public:
 
-  
+
   double smooth(double x) const // smooth transition from 0 to 1 on [0,1]
   {
     if (x<=0)
       return 0;
     if (x>=1.0)
       return 0;
-    
+
     if (x<0.5)
       return 0.5*exp(-1./x)/exp(-2.0);
     else
       return 1.-0.5*exp(-1./(1.-x))/exp(-2.0);
   }
-  
+
 
   double operator()(double x, double y) const
   {
@@ -58,27 +61,27 @@ public:
 
 // Velocity
 class InitialVX : virtual public Nextsim::InitialBase {
-  
+
   double _time;
 
 public:
 
   void settime(double t)
   {_time = t;}
-  
+
   double operator()(double x, double y) const {
     return (0.5*M_PI*sin(0.5*_time))
       * ((y-0.5)); }
 };
 class InitialVY : virtual public Nextsim::InitialBase {
-  
+
   double _time;
 
 public:
 
   void settime(double t)
   {_time = t;}
-  
+
   double operator()(double x, double y) const { return (0.5*M_PI*sin(0.5*_time)) *
       (-(x-0.5)); }
 };
@@ -91,11 +94,11 @@ class Test
 {
 
   size_t N; //!< size of mesh N x N
-  
+
 
   Nextsim::Mesh mesh;   //!< space mesh
   Nextsim::TimeMesh timemesh; //!< time mesh
-  
+
   //! Velocity vectors and density
   Nextsim::CellVector<DGdegree> vx, vy, phi, finalphi;
 
@@ -110,7 +113,7 @@ class Test
   std::vector<double> errors; //!< for storing error w.r.t. exact solution
 
   size_t writestep; //! write out n step in total (for debugging only)
-  
+
  public:
 
   Test(size_t n) : N(n), dgtransport(vx,vy), writestep(40)
@@ -122,7 +125,7 @@ class Test
       dgtransport.settimesteppingscheme("rk3");
   }
 
-  Test() 
+  Test()
   {
     std::cout << "call Test(N). N is number of mesh elements per row" << std::endl;
   }
@@ -138,7 +141,7 @@ class Test
     double tmax = 2.0*M_PI;
     int NT = (static_cast<int>((tmax / k + 1) /100 + 1) * 100); // No time steps dividable by 100
     timemesh.BasicInit(tmax, NT, 1);
-    
+
     //! Init Transport Scheme
     dgtransport.setmesh(mesh);
     dgtransport.settimemesh(timemesh);
@@ -148,81 +151,85 @@ class Test
     vy.resize_by_mesh(mesh);
     phi.resize_by_mesh(mesh);
   }
-  
+
   void run()
   {
     values.clear();
 
     size_t NITER=3;
-    
+
     //! Iteration with successive mesh refinement
     for (size_t iter=0;iter<NITER;++iter)
       {
-	Nextsim::GlobalTimer.reset();
-	Nextsim::GlobalTimer.start("run");
+	//Nextsim::GlobalTimer.reset();
+  Nextsim::Timer::main.reset();
+  //Nextsim::Timer::main.tick("run");
+  Nextsim::Timer::main.tick("run");
 
-	Nextsim::GlobalTimer.start("run -- init");
+	//Nextsim::Timer::main.tick("run -- init");
+	Nextsim::Timer::main.tick("run -- init");
+
 	init();
-	
+
 	// initial density
 	Nextsim::L2ProjectInitial(mesh, phi, InitialPhi());
 
 	if (WRITE_VTK)
 	  {
-	    Nextsim::GlobalTimer.start("run -- init -- vtk");
+	    Nextsim::Timer::main.tick("run -- init -- vtk");
 	    Nextsim::VTK::write_dg<DGdegree>("Results/dg",0,phi,mesh);
-	    Nextsim::GlobalTimer.stop("run -- init -- vtk");
+	    Nextsim::Timer::main.tock("run -- init -- vtk");
 	  }
-	
+
 	//! Save initial solution for error control
 	finalphi = phi;
-	Nextsim::GlobalTimer.stop("run -- init");
-	
+	Nextsim::Timer::main.tock("run -- init");
+
 
 	//! time loop
-	Nextsim::GlobalTimer.start("run -- loop");
+	Nextsim::Timer::main.tick("run -- loop");
 	for (size_t iter = 1; iter <= timemesh.N; ++iter)
 	  {
-	    Nextsim::GlobalTimer.start("run -- loop -- vel");
+	    Nextsim::Timer::main.tick("run -- loop -- vel");
 	    VX.settime(timemesh.dt * iter);
 	    VY.settime(timemesh.dt * iter);
-	    Nextsim::GlobalTimer.start("run -- loop -- vel -- l2");
+	    Nextsim::Timer::main.tick("run -- loop -- vel -- l2");
 	    Nextsim::L2ProjectInitial(mesh, vx, VX);
 	    Nextsim::L2ProjectInitial(mesh, vy, VY);
-	    Nextsim::GlobalTimer.stop("run -- loop -- vel -- l2");
+	    Nextsim::Timer::main.tock("run -- loop -- vel -- l2");
 	    dgtransport.reinitvelocity();
-	    Nextsim::GlobalTimer.stop("run -- loop -- vel");
-	    
+	    Nextsim::Timer::main.tock("run -- loop -- vel");
+
 	    dgtransport.step(phi); // performs one time step with the 2nd Order Heun scheme
 	    if (WRITE_VTK)
 	      if (iter % (timemesh.N/writestep)==0)
 		{
-		  Nextsim::GlobalTimer.start("run -- loop -- vtk");
+		  Nextsim::Timer::main.tick("run -- loop -- vtk");
 		  Nextsim::VTK::write_dg<DGdegree>("Results/dg",iter/(timemesh.N/writestep),phi,mesh);
-		  Nextsim::GlobalTimer.stop("run -- loop -- vtk");
+		  Nextsim::Timer::main.tock("run -- loop -- vtk");
 		}
-	    
+
 	  }
-	Nextsim::GlobalTimer.stop("run -- loop");
+	Nextsim::Timer::main.tock("run -- loop");
 
 
-	Nextsim::GlobalTimer.start("run -- error");
+	Nextsim::Timer::main.tick("run -- error");
 	Nextsim::CellVector<DGdegree> errorphi = phi;
 	errorphi += -finalphi;
 	if (WRITE_VTK)
 	  {
-	    Nextsim::GlobalTimer.start("run -- error- vtk");
+	    Nextsim::Timer::main.tick("run -- error- vtk");
 	    Nextsim::VTK::write_dg<DGdegree>("Results/error",N,errorphi,mesh);
-	    Nextsim::GlobalTimer.stop("run -- error- vtk");
+	    Nextsim::Timer::main.tock("run -- error- vtk");
 	  }
 	errors.push_back(errorphi.norm() * sqrt(mesh.hx*mesh.hy));
 	values.push_back(phi.col(0).sum() * mesh.hx * mesh.hy);
-	Nextsim::GlobalTimer.stop("run -- error");
-	
+	Nextsim::Timer::main.tock("run -- error");
 
-	Nextsim::GlobalTimer.stop("run");
 
-	//	Nextsim::GlobalTimer.print(); 
+	Nextsim::Timer::main.tock("run");
+
+	//	Nextsim::GlobalTimer.print();
 
 	if (iter<NITER-1)
 	  N*=2; //!< double the problem size
@@ -243,7 +250,7 @@ class Test
     std::cerr << "dG(" << DGdegree << ") FAILED: " << message << std::endl;
 
     assert(values.size()>=3);
-    
+
     for (size_t i=0;i<3;++i)
       std::cerr << v[i] << " = " << values[i+values.size()-3] << "\t" << e[i] << " = " << errors[i+values.size()-3] << std::endl;
   }
@@ -251,7 +258,7 @@ class Test
   bool check_references(const std::array<double,3>& v, const std::array<double,3>& e) const
   {
     bool passed = true;
-    
+
     for (size_t i=0;i<3;++i)
       {
 	if (fabs(v[i]-values[i])>TOL)
@@ -268,7 +275,7 @@ class Test
 
     return passed;
   }
-  
+
 
   bool check() const
   {
@@ -301,7 +308,7 @@ class Test
 
     return check_references(val_ref,err_ref);
   }
-  
+
 
 };
 
@@ -331,4 +338,8 @@ int main()
     std::cout << "dG(2) TEST FAILED" << std::endl;
   else
     std::cout << "dG(2) TEST PASSED" << std::endl;
+
+  std::cout << Nextsim::Timer::main << std::endl;
+
+
 }
