6abec1daf9fd758a68d927858cb712941c57b89a
Tim Spain <timothy.spain@nersc.no>
2021-12-02 14:31:45 +0100

    Configure CCSMIceAlbedo, since the albedo values are not constants.

--- src/CCSMIceAlbedo.cpp
+++ src/CCSMIceAlbedo.cpp
@@ -19,17 +19,25 @@
 
 namespace Nextsim {
 
-const double ICE_ALBEDO = 0.538;
-const double SNOW_ALBEDO = 0.8256;
+const double ICE_ALBEDO0 = 0.538;
+const double SNOW_ALBEDO0 = 0.8256;
+
+double CCSMIceAlbedo::iceAlbedo = ICE_ALBEDO0;
+double CCSMIceAlbedo::snowAlbedo = SNOW_ALBEDO0;
 
 double CCSMIceAlbedo::albedo(double temperature, double snowThickness)
 {
     const double tLimit = -1.;
-    double iceAlbedo = ICE_ALBEDO - std::fmax(0., 0.075 * (temperature - tLimit));
-    double snowAlbedo = SNOW_ALBEDO - std::fmax(0., 0.124 * (temperature - tLimit));
-
+    double iceAlbedoT = iceAlbedo - std::fmax(0., 0.075 * (temperature - tLimit));
+    double snowAlbedoT = snowAlbedo - std::fmax(0., 0.124 * (temperature - tLimit));
     double snowCoverFraction = snowThickness / (snowThickness + 0.02);
 
-    return snowCoverFraction * snowAlbedo + (1 - snowCoverFraction) * iceAlbedo;
+    return snowCoverFraction * snowAlbedoT + (1 - snowCoverFraction) * iceAlbedoT;
+}
+
+void CCSMIceAlbedo::configure()
+{
+    iceAlbedo = Configured::getConfiguration("CCSMIceAlbedo.iceAlbedo", ICE_ALBEDO0);
+    snowAlbedo = Configured::getConfiguration("CCSMIceAlbedo.snowAlbedo", SNOW_ALBEDO0);
 }
 }
--- src/include/CCSMIceAlbedo.hpp
+++ src/include/CCSMIceAlbedo.hpp
@@ -8,12 +8,19 @@
 #ifndef SRC_INCLUDE_CCSMICEALBEDO_HPP
 #define SRC_INCLUDE_CCSMICEALBEDO_HPP
 
+#include "Configured.hpp"
 #include "IIceAlbedo.hpp"
 
 namespace Nextsim {
 
-class CCSMIceAlbedo : public IIceAlbedo {
-    double albedo(double temperature, double snowThickness);
+class CCSMIceAlbedo : public IIceAlbedo, public Configured<CCSMIceAlbedo> {
+public:
+    double albedo(double temperature, double snowThickness) override;
+
+    void configure() override;
+private:
+    static double iceAlbedo;
+    static double snowAlbedo;
 };
 
 }
