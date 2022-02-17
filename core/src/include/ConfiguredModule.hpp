2de8402095d4b4de3a19c08bed7e0c947330242f
Tim Spain <timothy.spain@nersc.no>
2022-02-17 17:47:03 +0100

    Port ConfiguredModule (& test) over to the new module system.

--- core/src/include/ConfiguredModule.hpp
+++ core/src/include/ConfiguredModule.hpp
@@ -8,6 +8,9 @@
 #ifndef SRC_INCLUDE_CONFIGUREDMODULE_HPP
 #define SRC_INCLUDE_CONFIGUREDMODULE_HPP
 
+#include <functional>
+#include <list>
+#include <map>
 #include <string>
 
 namespace Nextsim {
@@ -18,11 +21,27 @@ namespace Nextsim {
  */
 class ConfiguredModule {
 public:
+
+    typedef std::function<void(const std::string&)> fn;
+    typedef std::map<const std::string, fn> map;
     ConfiguredModule() = default;
     virtual ~ConfiguredModule() = default;
 
     //! Parse the configuration for all of the modules defined in ModuleLoader.
     static void parseConfigurator();
+
+    /*!
+     * Sets the names of all the modules to be configured
+     * @param nameList a std::list of std::strings with the names of the
+     *                  interfaces to be configured.
+     */
+    static void setConfiguredModules(const map& ls);
+
+    /*!
+     * Adds the name of a module to the list to be configured
+     * @param name the name of the class to be configured.
+     */
+    static void configureModule(const std::string& mod, fn&);
 
     /*!
      * @brief Transforms a module name into a configuration option name.
@@ -33,6 +52,8 @@ public:
 
     //! The configuration options section name for modules.
     static const std::string MODULE_PREFIX;
+private:
+    static map configuredModules;
 };
 
 } /* namespace Nextsim */
