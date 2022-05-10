68c63a2578841faf15a1cc06532a7ebb2a45ba31
Tim Spain <timothy.spain@nersc.no>
2022-05-10 08:27:11 +0200

    Create the Atmosphere- and OceanState base classes.

---
+++ physics/src/modules/include/AtmosphereState.hpp
@@ -0,0 +1,45 @@
+/*!
+ * @file AtmosphereState.hpp
+ *
+ * @date May 9, 2022
+ * @author Tim Spain <timothy.spain@nersc.no>
+ */
+
+#ifndef ATMOSPHERESTATE_HPP
+#define ATMOSPHERESTATE_HPP
+
+#include "include/ModelComponent.hpp"
+#include "include/Time.hpp"
+
+namespace Nextsim {
+
+class AtmosphereState : public ModelComponent {
+public:
+    AtmosphereState();
+    virtual ~AtmosphereState();
+
+    void setData(const ModelState&) override;
+    ModelState getState() const override;
+    ModelState getState(const OutputLevel&) const override;
+    std::string getName() const override;
+    std::set<std::string> hFields() const override;
+
+    void update(const TimestepTime&);
+
+protected:
+    HField tair;
+    HField tdew;
+    HField pair;
+    HField rmix;
+    HField sw_in;
+    HField lw_in;
+    HField snowfall;
+
+    HField windSpeed;
+
+    virtual void updateSpecial(const TimestepTime&) = 0;
+};
+
+} /* namespace Nextsim */
+
+#endif /* ATMOSPHERESTATE_HPP */
---
+++ physics/src/modules/include/OceanState.hpp
@@ -0,0 +1,49 @@
+/*!
+ * @file OceanState.hpp
+ *
+ * @date May 9, 2022
+ * @author Tim Spain <timothy.spain@nersc.no>
+ */
+
+#ifndef OCEANSTATE_HPP
+#define OCEANSTATE_HPP
+
+#include "include/Configured.hpp"
+#include "include/IFreezingPointModule.hpp"
+#include "include/ModelComponent.hpp"
+
+
+namespace Nextsim {
+
+class OceanState : public ModelComponent, public Configured<OceanState> {
+public:
+    OceanState();
+    virtual ~OceanState();
+
+    void setData(const ModelState&) override;
+    ModelState getState() const override;
+    ModelState getState(const OutputLevel&) const override;
+    std::string getName() const override;
+    std::set<std::string> hFields() const override;
+
+    void configure() override;
+    void update(const TimestepTime&);
+
+protected:
+    HField sst;
+    HField sss;
+    HField mld;
+
+    HField tf;
+    HField cpml;
+
+    IFreezingPoint* tfImpl;
+
+    virtual void updateSpecial(const TimestepTime&) = 0;
+private:
+    void updateFreezingPoint(size_t i, const TimestepTime&);
+};
+
+} /* namespace Nextsim */
+
+#endif /* OCEANSTATE_HPP */
