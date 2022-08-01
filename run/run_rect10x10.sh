a1ab95ce6b2c177c0e4add7e88cf2318b99ed8c5
Aurélie Albert <auraoupa@gmail.com>
2022-08-01 15:50:43 +0200

    new nomenclature for examples

--- run/run_rect10x10.sh
+++ run/run_rect10x10.sh
@@ -2,4 +2,4 @@
 if [ ! -f nextsim ]; then
     echo "Copy or link the nextsim executable here from the build directory"
 fi
-./nextsim --config-file dev1.cfg
+./nextsim --config-file config_rect10x10.cfg
--- run/run_simple_example.sh
+++ run/run_simple_example.sh
@@ -2,4 +2,4 @@
 if [ ! -f nextsim ]; then
     echo "Copy or link the nextsim executable here from the build directory"
 fi
-./nextsim --config-file rect2030.cfg
+./nextsim --config-file config_simple_example.cfg
