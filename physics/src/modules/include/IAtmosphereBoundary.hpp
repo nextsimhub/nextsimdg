20eb58f4680db2544d3fc7c3f839bdbfdf835abd
Tim Spain <timothy.spain@nersc.no>
2022-09-13 18:56:10 +0200

    Add the prescribed fields to the boundary condition interfaces.

--- physics/src/modules/include/IAtmosphereBoundary.hpp
+++ physics/src/modules/include/IAtmosphereBoundary.hpp
@@ -20,9 +20,18 @@ class IAtmosphereBoundary : public ModelComponent {
     ModelState getState(const OutputLevel&) const override;
     ModelState getStateRecursive(const OutputSpec& os) const override;
 
-    std::string getName() const override;
+    std::string getName() const override { return "IAtmosphereBoundary"; }
     std::unordered_set<std::string> hFields() const override;
 
+protected:
+    HField qia; // Ice-atmosphere heat flux, W m⁻²
+    HField dqia_dt; // Derivative of qia w.r.t. ice surface temperature
+    HField qsw; // Solar shortwave radiation, W m⁻²
+    HField qlw; // Non-Solar radiative heat flux, W m⁻²
+    HField subl; // Ice sublimative mass flux, kg m⁻²
+    HField precip; // total precipitation flux, kg m⁻²
+    UField u; // x(east)-ward wind, m s⁻¹
+    VField v; // y(north)-ward wind, m s⁻¹
 };
 
 } /* namespace Nextsim */
--- physics/src/modules/include/IOceanBoundary.hpp
+++ physics/src/modules/include/IOceanBoundary.hpp
@@ -20,9 +20,15 @@ class IOceanBoundary : public ModelComponent {
     ModelState getState(const OutputLevel&) const override;
     ModelState getStateRecursive(const OutputSpec& os) const override;
 
-    std::string getName() const override;
+    std::string getName() const override { return "IOceanBoundary"; }
     std::unordered_set<std::string> hFields() const override;
 
+protected:
+    HField qio; // Ice-ocean heat flux, W m⁻²
+    HField sst; // Coupled or slab ocean sea surface temperature, ˚C
+    HField sss; // Coupled or slab ocean sea surface salinity, PSU
+    UField u; // x(east)-ward ocean current, m s⁻¹
+    VField v; // y(north)-ward ocean current, m s⁻¹
 };
 } /* namespace Nextsim */
 
