f20cf3656ad6a069afe0b0aaa651c9468df8e863
Tim Spain <timothy.spain@nersc.no>
2022-10-27 16:52:54 +0200

    Write the ParaGridIO data to the output file.

--- core/src/ParaGridIO.cpp
+++ core/src/ParaGridIO.cpp
@@ -7,6 +7,8 @@
 
 #include "include/ParaGridIO.hpp"
 
+#include "include/CommonRestartMetadata.hpp"
+#include "include/MissingData.hpp"
 #include "include/gridNames.hpp"
 
 #include <ncFile.h>
@@ -19,25 +21,25 @@
 namespace Nextsim {
 
 const std::map<std::string, ModelArray::Type> ParaGridIO::dimensionKeys = {
-        { "xy", ModelArray::Type::H },
-        { "xyz", ModelArray::Type::Z },
+    { "xy", ModelArray::Type::H },
+    { "xyz", ModelArray::Type::Z },
 
 };
 
 // Which dimensions are DG dimension, which could be legitimately missing
 const std::map<ModelArray::Dimension, bool> ParaGridIO::isDG = {
-        { ModelArray::Dimension::X, false },
-        { ModelArray::Dimension::Y, false },
-        { ModelArray::Dimension::Z, false },
-        { ModelArray::Dimension::XCG, true },
-        { ModelArray::Dimension::YCG, true },
-        { ModelArray::Dimension::DG, true },
-        { ModelArray::Dimension::DGSTRESS, true },
+    { ModelArray::Dimension::X, false },
+    { ModelArray::Dimension::Y, false },
+    { ModelArray::Dimension::Z, false },
+    { ModelArray::Dimension::XCG, true },
+    { ModelArray::Dimension::YCG, true },
+    { ModelArray::Dimension::DG, true },
+    { ModelArray::Dimension::DGSTRESS, true },
 };
 
 const std::map<ModelArray::Dimension, ModelArray::Type> ParaGridIO::dimCompMap = {
-        { ModelArray::Dimension::DG, ModelArray::Type::DG },
-        { ModelArray::Dimension::DGSTRESS, ModelArray::Type::DGSTRESS },
+    { ModelArray::Dimension::DG, ModelArray::Type::DG },
+    { ModelArray::Dimension::DGSTRESS, ModelArray::Type::DGSTRESS },
 };
 ModelState ParaGridIO::getModelState(const std::string& filePath)
 {
@@ -53,7 +55,8 @@ ModelState ParaGridIO::getModelState(const std::string& filePath)
         if (dim.isNull()) {
             // Only throw if a finite volume dimension (X, Y, Z) is missing
             if (!isDG.at(entry.first)) {
-                throw std::out_of_range(std::string("No dimension named ") + entry.second.name + " found in " + filePath);
+                throw std::out_of_range(std::string("No dimension named ") + entry.second.name
+                    + " found in " + filePath);
             }
         } else {
             if (dimCompMap.count(entry.first)) {
@@ -77,7 +80,8 @@ ModelState ParaGridIO::getModelState(const std::string& filePath)
             dimKey += dim.getName();
         }
         if (!dimensionKeys.count(dimKey)) {
-            throw std::out_of_range(std::string("No ModelArray::Type corresponds to the dimensional key ") + dimKey);
+            throw std::out_of_range(
+                std::string("No ModelArray::Type corresponds to the dimensional key ") + dimKey);
         }
         state.data[varName](dimensionKeys.at(dimKey));
         ModelArray& data = state.data.at(varName);
@@ -87,5 +91,54 @@ ModelState ParaGridIO::getModelState(const std::string& filePath)
 
     ncFile.close();
     return state;
+}
+
+void ParaGridIO::dumpModelState(const ModelState& state, const ModelMetadata& metadata,
+    const std::string& filePath, bool isRestart) const
+{
+
+    netCDF::NcFile ncFile(filePath, netCDF::NcFile::replace);
+
+    CommonRestartMetadata::writeStructureType(ncFile, metadata);
+    netCDF::NcGroup metaGroup = ncFile.addGroup(IStructure::metadataNodeName());
+    netCDF::NcGroup dataGroup = ncFile.addGroup(IStructure::dataNodeName());
+
+    CommonRestartMetadata::writeRestartMetadata(metaGroup, metadata);
+
+    // TODO dump grid data
+    // Dump the dimensions and number of components
+    std::map<ModelArray::Dimension, netCDF::NcDim> ncFromMAMap;
+    for (auto entry : ModelArray::definedDimensions) {
+        ModelArray::Dimension dim = entry.first;
+        size_t dimSz = (dimCompMap.count(dim)) ? ModelArray::nComponents(dimCompMap.at(dim)) : dimSz = entry.second.length;
+        ncFromMAMap[dim] = dataGroup.addDim(entry.second.name, dimSz);
+        // TODO Do I need to add data, even if it is just integers 0...n-1?
+    }
+    // Also create the sets of dimensions to be connected to the data fields
+    std::map<ModelArray::Type, std::vector<netCDF::NcDim>> dimMap;
+    for (auto entry : ModelArray::typeDimensions) {
+        ModelArray::Type type = entry.first;
+        std::vector<netCDF::NcDim> ncDims;
+        for (ModelArray::Dimension& maDim : entry.second) {
+            ncDims.push_back(ncFromMAMap.at(maDim));
+        }
+        dimMap[type] = ncDims;
+    }
+
+    std::set<std::string> restartFields
+        = { hiceName, ciceName, hsnowName, ticeName }; // TODO and others
+    // Loop through either the above list (isRestart) or all provided fields(!isRestart)
+    for (auto entry : state.data) {
+        if (!isRestart || restartFields.count(entry.first)) {
+            // Get the type, then relevant vector of NetCDF dimensions
+            ModelArray::Type type = entry.second.getType();
+            std::vector<netCDF::NcDim>& ncDims = dimMap.at(type);
+            netCDF::NcVar var(dataGroup.addVar(entry.first, netCDF::ncDouble, ncDims));
+            var.putAtt(mdiName, netCDF::ncDouble, MissingData::value());
+            var.putVar(entry.second.getData());
+        }
+    }
+
+    ncFile.close();
 }
 } /* namespace Nextsim */
