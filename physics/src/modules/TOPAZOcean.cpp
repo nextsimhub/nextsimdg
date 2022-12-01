4443bffd52dcb05c64b3c91d210314123664c6d4
Tim Spain <timothy.spain@nersc.no>
2022-12-01 10:22:26 +0100

    TOPAZOcean forcings adapted from ERA5Atmosphere.

---
+++ physics/src/modules/TOPAZOcean.cpp
@@ -0,0 +1,67 @@
+/*!
+ * @file TOPAZOcean.cpp
+ *
+ * @date Nov 25, 2022
+ * @author Tim Spain <timothy.spain@nersc.no>
+ */
+
+#include "include/TOPAZOcean.hpp"
+
+#include "include/Module.hpp"
+#include "include/ParaGridIO.hpp"
+
+#include <iostream> // FIXME remove me
+namespace Nextsim {
+
+std::string TOPAZOcean::filePath;
+
+static const std::string pfx = "TOPAZOcean";
+static const std::string fileKey = pfx + ".file";
+
+template <>
+const std::map<int, std::string> Configured<TOPAZOcean>::keyMap = {
+    { TOPAZOcean::FILEPATH_KEY, fileKey },
+};
+
+TOPAZOcean::TOPAZOcean()
+{
+    registerProtectedArray(ProtectedArray::SST, &sst);
+    registerProtectedArray(ProtectedArray::SSS, &sss);
+    registerProtectedArray(ProtectedArray::MLD, &mld);
+    registerProtectedArray(ProtectedArray::OCEAN_U, &u);
+    registerProtectedArray(ProtectedArray::OCEAN_V, &v);
+}
+
+ConfigurationHelp::HelpMap& TOPAZOcean::getHelpRecursive(HelpMap& map, bool getAll)
+{
+    map[pfx] = {
+        { fileKey, ConfigType::STRING, {}, "", "",
+            "Path to the processed NetCDF file providing the TOPAZ forcings." },
+    };
+
+    return map;
+}
+
+void TOPAZOcean::configure()
+{
+    filePath = Configured::getConfiguration(keyMap.at(FILEPATH_KEY), std::string());
+}
+
+void TOPAZOcean::updateBefore(const TimestepTime& tst)
+{
+    // TODO: Get more authoritative names for the forcings
+    std::set<std::string> forcings = { "sst", "sss", "mld", "u", "v" };
+
+    ModelState state = ParaGridIO::readForcingTimeStatic(forcings, tst.start, filePath);
+    sst = state.data.at("tair");
+    sss = state.data.at("dew2m");
+    mld = state.data.at("pair");
+    u = state.data.at("sw_in");
+    v = state.data.at("lw_in");
+}
+
+void TOPAZOcean::setFilePath(const std::string& filePathIn) { filePath = filePathIn; }
+
+void TOPAZOcean::setData(const ModelState::DataMap&) { }
+
+} /* namespace Nextsim */
---
+++ physics/src/modules/include/TOPAZOcean.hpp
@@ -0,0 +1,56 @@
+/*!
+ * @file TOPAZOcean.hpp
+ *
+ * @date Nov 25, 2022
+ * @author Tim Spain <timothy.spain@nersc.no>
+ */
+
+#ifndef TOPAZOCEAN_HPP
+#define TOPAZOCEAN_HPP
+
+#include "IOceanBoundary.hpp"
+
+#include "include/Configured.hpp"
+
+namespace Nextsim {
+
+/*!
+ * A class to provided forcings from pre-processed forcings files based on ERA5
+ * data.
+ */
+class TOPAZOcean : public IOceanBoundary, public Configured<TOPAZOcean> {
+public:
+    TOPAZOcean();
+    ~TOPAZOcean() = default;
+
+    enum {
+        FILEPATH_KEY,
+    };
+
+    void setData(const ModelState::DataMap&) override;
+    std::string getName() const override { return "TOPAZOcean"; }
+
+    static HelpMap& getHelpRecursive(HelpMap& map, bool getAll);
+
+    void configure() override;
+
+    void updateBefore(const TimestepTime&) override;
+    void updateAfter(const TimestepTime&) override { }
+
+    void setFilePath(const std::string& filePathIn);
+
+private:
+    // Since the configuration is global, it makes sense for the file path to
+    // be static.
+    static std::string filePath;
+
+    HField sst;
+    HField sss;
+    HField mld;
+    HField u;
+    HField v;
+};
+
+} /* namespace Nextsim */
+
+#endif /* TOPAZOCEAN_HPP_ */
