acfcd85923f883d0a4ede9176f0e468aca690bd4
Kacper Kornet <kk562@cam.ac.uk>
2023-02-13 08:49:38 +0000

    MPI: pass partitionFile argument to RectGridIO::getModelState

--- core/src/include/RectGridIO.hpp
+++ core/src/include/RectGridIO.hpp
@@ -3,6 +3,7 @@
  *
  * @date Feb 8, 2022
  * @author Tim Spain <timothy.spain@nersc.no>
+ * @author Kacper Kornet <kk562@cam.ac.uk>
  */
 
 #ifndef RECTGRIDIO_HPP
@@ -22,7 +23,12 @@ public:
 
     typedef RectangularGrid::GridDimensions GridDimensions;
 
+#ifdef USE_MPI
+    ModelState getModelState(
+        const std::string& filePath, const std::string& partitionFile) override;
+#else
     ModelState getModelState(const std::string& filePath) override;
+#endif
 
     void dumpModelState(const ModelState& state, const ModelMetadata& metadata,
         const std::string& filePath, bool isRestart) const override;
