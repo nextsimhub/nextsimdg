6f1d4a03874dfae099f02b4198a9663872c918f8
Tim Spain <timothy.spain@nersc.no>
2023-02-16 15:17:59 +0100

    Copy only the DG0 component between HField ModelArrays and DGVectors.

--- dynamics/tests/DGModelArray_test.cpp
+++ dynamics/tests/DGModelArray_test.cpp
@@ -10,6 +10,7 @@
 
 #include "include/DGModelArray.hpp"
 
+#include "include/ModelArrayRef.hpp"
 #include "include/ParametricMesh.hpp"
 
 namespace Nextsim {
@@ -164,4 +165,73 @@ TEST_CASE("Test with DG = 1", "[DGModelArray]") // (It would be a silly case to 
     REQUIRE(source(targetPoint) == dest(11, 13));
 
 }
+
+TEST_CASE("DGVector and HField", "[DGModelArray]")
+{
+    static const int DG = 3;
+    const size_t nx = 29;
+    const size_t ny = 27;
+    const size_t mx = 100;
+    const size_t my = 100;
+
+    ModelArray::setDimensions(ModelArray::Type::H, { nx, ny });
+    ModelArray::setDimensions(ModelArray::Type::DG, { nx, ny });
+    ModelArray::setNComponents(ModelArray::Type::DG, DG);
+
+    // Create the ParametricMesh object
+    ParametricMesh smash;
+    smash.nx = nx;
+    smash.ny = ny;
+    smash.nnodes = nx * ny;
+    smash.nelements = nx * ny;
+    smash.vertices.resize(smash.nelements, Eigen::NoChange);
+    for (size_t i = 0; i < nx; ++i) {
+        for (size_t j = 0; j < ny; ++j) {
+            smash.vertices(i * ny + j, 0) = i;
+            smash.vertices(i * ny + j, 1) = j;
+        }
+    }
+
+    // THe data comes from the restart file as a Type::DG ModelArray
+    ModelArray restart(ModelArray::Type::DG);
+    restart.resize();
+    // Fill with data
+    for (size_t i = 0; i < nx; ++i) {
+        for (size_t j = 0; j < ny; j++) {
+            for (size_t c = 0; c < DG; ++c) {
+                restart.components({i, j})[c] = c + my * (j + mx * (i));
+            }
+        }
+    }
+
+    // This is written to a DGVector stored in the dynamics
+    DGVector<DG> dyn(smash);
+    DGModelArray::ma2dg<DG>(restart, dyn);
+
+    size_t targetPoint = ModelArray::indexFromLocation(ModelArray::Type::DG, {14, 12});
+    REQUIRE(dyn(targetPoint, 2) == restart.components({14, 12})[2]);
+
+    // In a given timestep, the dynamics happens first, changing the DGVector in all components
+    dyn(targetPoint, 0) += 0.2983;
+    dyn(targetPoint, 1) -= 0.34596;
+    dyn(targetPoint, 2) += 0.53244;
+
+    double preservedComponent = dyn(targetPoint, 1);
+
+    // This is the copied out to the thermodynamics array, an HField ModelArray
+    ModelArray thermo(ModelArray::Type::H);
+    thermo.resize();
+    DGModelArray::dg2ma<DG>(dyn, thermo);
+    REQUIRE(thermo(14, 12) == dyn(targetPoint, 0));
+
+    // The thermodynamics happens, changing the thermo array
+    thermo(14, 12) -= 0.62748;
+
+    // This is then copied back into the dynamics array, only changing the 0 component
+    DGModelArray::ma2dg<DG>(thermo, dyn);
+    REQUIRE(dyn(targetPoint, 0) == thermo(14, 12));
+    REQUIRE(dyn(targetPoint, 1) == preservedComponent);
+
+}
+
 }
