da0dbaa2eb552dce65a40bedcab03885aa1c56da
Tim Spain <timothy.spain@nersc.no>
2023-02-20 16:43:24 +0100

    Code to get the full DG data.

--- dynamics/src/include/DummyDynamicsKernel.hpp
+++ dynamics/src/include/DummyDynamicsKernel.hpp
@@ -8,72 +8,96 @@
 #ifndef DUMMYDYNAMICSKERNEL_HPP
 #define DUMMYDYNAMICSKERNEL_HPP
 
-#include "include/dgVector.hpp"
 #include "include/cgVector.hpp"
+#include "include/dgVector.hpp"
 
-#include "include/gridNames.hpp"
-#include "dgModelArray.hpp"
 #include "cgModelArray.hpp"
+#include "dgModelArray.hpp"
+#include "include/gridNames.hpp"
 
 #include <string>
 #include <unordered_map>
 
 namespace Nextsim {
-template <int DG, int CGdegree>
-class DummyDynamicsKernel {
+template <int DG, int CGdegree> class DummyDynamicsKernel {
 public:
-
-/*!
- * @brief Sets the data from a provided ModelArray.
- *
- * @details Given a name and a ModelArray, sets the data associated with that
- * name. In some special cases (hice, cice…) this is a special array used in
- * the dynamics calculations. In all other cases, these are added to the
- * container of name data fields to be advected. The provided ModelArray can be
- * of DG or DGSTRESS type, in which case all components of the DGVector are
- * filled, or any other type which only fills the DG0 finite volume element of
- * the dgVector. The behaviour is exactly that of the ma2dg() function defined
- * in the DGModelArray class.
- *
- * @param name The name of the data field to set.
- * @param data The ModelArray containing the data to be set.
- *
- */
-void setData(const std::string& name, const ModelArray& data)
-{
-    // Special cases: hice, cice, (damage, stress) <- not yet implemented
-    if (name == hiceName) {
-        DGModelArray::ma2dg(data, hice);
-    } else if (name == ciceName) {
-        DGModelArray::ma2dg(data, cice);
-    } else if (name == "u"){
-        CGModelArray::ma2cg(data, u);
-    } else if (name == "v"){
-        CGModelArray::ma2cg(data, v);
-    } else {
-        // All other fields get shoved in a (labelled) bucket
-        DGModelArray::ma2dg(data, advectedFields[name]);
-    }
-}
-
-/*!
- * @brief Returns an HField ModelArray containing the DG0 finite volume
- * component of the named dynamics field.
- *
- */
-ModelArray getDG0Data(const std::string& name) {
-    HField data(ModelArray::Type::H);
-    if (name == hiceName) {
-        return DGModelArray::ma2dg(data, hice);
-    } else if (name == ciceName) {
-        return DGModelArray::ma2dg(data, cice);
-    } else {
-        // Any other named field must exist
-        return DGModelArray::ma2dg(data, advectedFields.at(name));
+    /*!
+     * @brief Sets the data from a provided ModelArray.
+     *
+     * @details Given a name and a ModelArray, sets the data associated with that
+     * name. In some special cases (hice, cice…) this is a special array used in
+     * the dynamics calculations. In all other cases, these are added to the
+     * container of name data fields to be advected. The provided ModelArray can be
+     * of DG or DGSTRESS type, in which case all components of the DGVector are
+     * filled, or any other type which only fills the DG0 finite volume element of
+     * the dgVector. The behaviour is exactly that of the ma2dg() function defined
+     * in the DGModelArray class.
+     *
+     * @param name The name of the data field to set.
+     * @param data The ModelArray containing the data to be set.
+     *
+     */
+    void setData(const std::string& name, const ModelArray& data)
+    {
+        // Special cases: hice, cice, (damage, stress) <- not yet implemented
+        if (name == hiceName) {
+            DGModelArray::ma2dg(data, hice);
+        } else if (name == ciceName) {
+            DGModelArray::ma2dg(data, cice);
+        } else if (name == "u") {
+            CGModelArray::ma2cg(data, u);
+        } else if (name == "v") {
+            CGModelArray::ma2cg(data, v);
+        } else {
+            // All other fields get shoved in a (labelled) bucket
+            DGModelArray::ma2dg(data, advectedFields[name]);
+        }
     }
 
-}
+    /*!
+     * @brief Returns an HField ModelArray containing the DG0 finite volume
+     * component of the named dynamics field.
+     *
+     * @param name the name of the requested field.
+     *
+     */
+    ModelArray getDG0Data(const std::string& name)
+    {
+        HField data(ModelArray::Type::H);
+        if (name == hiceName) {
+            return DGModelArray::dg2ma(hice, data);
+        } else if (name == ciceName) {
+            return DGModelArray::dg2ma(cice, data);
+        } else {
+            // Any other named field must exist
+            return DGModelArray::dg2ma(advectedFields.at(name), data);
+        }
+    }
 
+    /*!
+     * @brief Returns a DG or DGSTRESS ModelArray containing the full DG data for
+     * the named dynamics field.
+     *
+     * @param name the name of the requested field.
+     */
+    ModelArray getDGData(const std::string& name)
+    {
+
+        if (name == hiceName) {
+            DGField data(ModelArray::Type::DG);
+            data.resize();
+            return DGModelArray::dg2ma(hice, data);
+        } else if (name == ciceName) {
+            DGField data(ModelArray::Type::DG);
+            data.resize();
+            return DGModelArray::dg2ma(cice, data);
+        } else {
+            ModelArray::Type type = fieldType.at(name);
+            ModelArray data(type);
+            data.resize();
+            return DGModelArray::dg2ma(advectedFields.at(name), data);
+        }
+    }
 private:
     DGVector<DG> hice;
     DGVector<DG> cice;
