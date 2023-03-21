6c74baedae564fcf5ada98e9616c0bdee77fe269
Piotr Minakowski <piotr.minakowski@ovgu.de>
2023-03-21 09:06:11 +0100

    Adding compression testcase

---
+++ dynamics/applications/benchmark_compression_sasip_meb.cpp
@@ -0,0 +1,288 @@
+/*!
+ * @file benchmark_compression_meb.cpp
+ * @date 06 March 2023
+ * @author Piotr Minakowski <piotr.minakowski@ovgu.de>
+ */
+
+#include "Interpolations.hpp"
+#include "ParametricMesh.hpp"
+#include "ParametricTools.hpp"
+#include "ParametricTransport.hpp"
+
+#include "Tools.hpp"
+#include "cgParametricMomentum.hpp"
+#include "cgVector.hpp"
+#include "dgInitial.hpp"
+#include "dgLimit.hpp"
+#include "dgVisu.hpp"
+#include "stopwatch.hpp"
+
+#include <cassert>
+#include <chrono>
+#include <filesystem>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+bool WRITE_VTK = true;
+
+namespace Nextsim {
+extern Timer GlobalTimer;
+}
+
+inline constexpr double SQR(double x) { return x * x; }
+
+
+// Benchmark LKF testcase from [Mehlmann et al., 2022]
+namespace ReferenceScale {
+    constexpr double T = 2.0 * 24 * 60. * 60.; //!< Time horizon 2 days
+    constexpr double L = 512000.0; //!< Size of domain !!!
+    constexpr double vmax_ocean = 0.01; //!< Maximum velocity of ocean
+    double vmax_atm = 30.0 / exp(1.0); //!< Max. vel. of wind
+}
+
+// Benchmark compression testcase from [Plante Tremblay, 2021]
+namespace RefScaleCompression {
+    using namespace ReferenceScale;
+
+    constexpr double T = 4. * 60. * 60.; //!< Time horizon 2 hours
+    constexpr double L = 60000.0; //!< Size of domain
+    constexpr double vmax_ocean = 0.0; //!< Maximum velocity of ocean
+    constexpr double vmax_atm = 20; //!< Max. vel. of wind
+
+}
+
+
+class OceanX : public Nextsim::Interpolations::Function {
+public:
+    double operator()(double x, double y) const { return 0.0; }
+};
+class OceanY : public Nextsim::Interpolations::Function {
+public:
+    double operator()(double x, double y) const { return 0.0; }
+};
+struct AtmX : public Nextsim::Interpolations::Function {
+    double time;
+
+public:
+    void settime(double t) { time = t; }
+    double operator()(double x, double y) const { return 0.0; }
+};
+struct AtmY : public Nextsim::Interpolations::Function {
+    double time;
+
+public:
+    void settime(double t) { time = t; }
+    double operator()(double x, double y) const 
+    {
+        constexpr double twohours = 2. * 60.0 * 60.0;
+        if (time < twohours) {
+            return -20. * 0.5 * (1. - cos(M_PI * time / twohours));
+        }
+        //! maximum is 20 m/s corresponds to 0.625 Pa forcing
+        else {
+            return -20.;
+        }
+    }
+};
+class InitialH : public Nextsim::Interpolations::Function {
+public:
+    double operator()(double x, double y) const { return 1.0; }
+};
+
+class InitialA : public Nextsim::Interpolations::Function {
+public:
+    double operator()(double x, double y) const { return 1.0; }
+};
+
+class InitialD : public Nextsim::Interpolations::Function {
+public:
+    double operator()(double x, double y) const { return 1.0; }
+};
+
+//////////////////////////////////////////////////
+
+template <int CG, int DGadvection, int DGstress>
+void run_benchmark(const std::string meshfile)
+{
+    //! Define the spatial mesh
+    Nextsim::ParametricMesh smesh;
+    smesh.readmesh(meshfile);
+    size_t NX = smesh.nx;
+
+    //! Compose name of output directory and create it
+    std::string resultsdir = "Compression_" + std::to_string(CG) + "_" + std::to_string(DGadvection) + "_" + std::to_string(DGstress)
+        + "__" + std::to_string(NX);
+    std::filesystem::create_directory(resultsdir);
+
+    //! Main class to handle the momentum equation. This class also stores the CG velocity vector
+    Nextsim::CGParametricMomentum<CG, DGstress> momentum(smesh);
+
+    //! define the time mesh
+    double dt_adv = 30. ; //60.0; //!< Time step of advection problem
+    //double dt_adv = ReferenceScale::L / smesh.nx / 1000. * 5;  //!< Stable time step
+    size_t NT = RefScaleCompression::T / dt_adv + 1.e-4; //!< Number of Advections steps
+
+    constexpr size_t NT_meb = 100; //!< Momentum substeps
+    double dt_mom = dt_adv / NT_meb; //!< Time step of momentum problem
+
+    //! Rheology-Parameters
+    Nextsim::MEBParameters Params;
+
+    std::cout << "Time step size (advection) " << dt_adv << "\t" << NT << " time steps" << std::endl
+              << "Momentum subcycling " << NT_meb << "\t dt_momentum " << dt_mom
+              << std::endl;
+
+    //! VTK output
+    double T_vtk = .05 * 60.0 * 60.0; // every 4 hours
+    size_t NT_vtk = T_vtk / dt_adv + 1.e-4;
+    //! LOG message
+    double T_log = 10.0 * 60.0; // every 30 minute
+    size_t NT_log = T_log / dt_adv + 1.e-4;
+
+    ////////////////////////////////////////////////// Forcing
+    Nextsim::Interpolations::Function2CG(smesh, momentum.GetOceanx(), OceanX());
+    Nextsim::Interpolations::Function2CG(smesh, momentum.GetOceany(), OceanY());
+    AtmX AtmForcingX;
+    AtmY AtmForcingY;
+    AtmForcingX.settime(0.0);
+    AtmForcingY.settime(0.0);
+    Nextsim::Interpolations::Function2CG(smesh, momentum.GetAtmx(), AtmForcingX);
+    Nextsim::Interpolations::Function2CG(smesh, momentum.GetAtmy(), AtmForcingY);
+
+    ////////////////////////////////////////////////// Variables and Initial Values
+    Nextsim::DGVector<DGadvection> H(smesh), A(smesh), D(smesh); //!< ice height and concentration
+    Nextsim::Interpolations::Function2DG(smesh, H, InitialH());
+    Nextsim::Interpolations::Function2DG(smesh, A, InitialA());
+    Nextsim::Interpolations::Function2DG(smesh, D, InitialD());
+
+    ////////////////////////////////////////////////// i/o of initial condition
+    Nextsim::GlobalTimer.start("time loop - i/o");
+    Nextsim::VTK::write_cg_velocity(resultsdir + "/vel", 0, momentum.GetVx(), momentum.GetVy(), smesh);
+    Nextsim::VTK::write_dg(resultsdir + "/A", 0, A, smesh);
+    Nextsim::VTK::write_dg(resultsdir + "/H", 0, H, smesh);
+    Nextsim::VTK::write_dg(resultsdir + "/D", 0, D, smesh);
+    //Nextsim::VTK::write_dg(resultsdir + "/S11", 0, momentum.GetS11(), smesh);
+    //Nextsim::VTK::write_dg(resultsdir + "/S12", 0, momentum.GetS12(), smesh);
+    //Nextsim::VTK::write_dg(resultsdir + "/S22", 0, momentum.GetS22(), smesh);
+    Nextsim::VTK::write_dg(resultsdir + "/Div", 0,
+        Nextsim::Tools::TensorInvI(smesh, momentum.GetE11(), momentum.GetE12(), momentum.GetE22()), smesh);
+    Nextsim::VTK::write_dg(resultsdir + "/Shear", 0,
+        Nextsim::Tools::Shear(smesh, momentum.GetE11(), momentum.GetE12(), momentum.GetE22()), smesh);
+    Nextsim::VTK::write_dg(resultsdir + "/sigma_n", 0,
+        Nextsim::Tools::TensorInvI(smesh, momentum.GetS11(), momentum.GetS12(), momentum.GetS22()), smesh);
+    Nextsim::VTK::write_dg(resultsdir + "/tau", 0,
+        Nextsim::Tools::TensorInvII(smesh, momentum.GetS11(), momentum.GetS12(), momentum.GetS22()), smesh);
+    Nextsim::GlobalTimer.stop("time loop - i/o");
+
+    ////////////////////////////////////////////////// Initialize transport
+    Nextsim::ParametricTransport<DGadvection> dgtransport(smesh);
+    dgtransport.settimesteppingscheme("rk2");
+
+    ////////////////////////////////////////////////// Main Loop
+    Nextsim::GlobalTimer.start("time loop");
+    for (size_t timestep = 1; timestep <= NT; ++timestep) {
+        double time = dt_adv * timestep;
+        double timeInMinutes = time / 60.0;
+        double timeInHours = time / 60.0 / 60.0;
+        double timeInDays = time / 60.0 / 60.0 / 24.;
+
+        if (timestep % NT_log == 0)
+            std::cout << "\rAdvection step " << timestep << "\t " << std::setprecision(2)
+                      << std::fixed << std::setw(10) << std::right << time << "s\t" << std::setw(8)
+                      << std::right << timeInMinutes << "m\t" << std::setw(6) << std::right
+                      << timeInHours << "h\t" << std::setw(6) << std::right << timeInDays << "d\t\t"
+                      << std::flush;
+
+        //////////////////////////////////////////////////
+        //! Initialize time-dependent forcing
+        Nextsim::GlobalTimer.start("time loop - forcing");
+        AtmForcingX.settime(time);
+        AtmForcingY.settime(time);
+        Nextsim::Interpolations::Function2CG(smesh, momentum.GetAtmx(), AtmForcingX);
+        Nextsim::Interpolations::Function2CG(smesh, momentum.GetAtmy(), AtmForcingY);
+        Nextsim::GlobalTimer.stop("time loop - forcing");
+
+        //////////////////////////////////////////////////
+        //! Advection step
+        Nextsim::GlobalTimer.start("time loop - advection");
+
+        // interpolates CG velocity to DG and reinits normal velocity
+        dgtransport.prepareAdvection(momentum.GetVx(), momentum.GetVy());
+        //dgtransport.prepareAdvection(momentum.GetAvgSubiterVx(), momentum.GetAvgSubiterVy());
+
+        //dgtransport.step(dt_adv, A);
+        
+        //dgtransport.step(dt_adv, H);
+        //dgtransport.step(dt_adv, D);
+
+        //! Gauss-point limiting
+        Nextsim::LimitMax(A, 1.0);
+        Nextsim::LimitMin(A, 1.0);
+        Nextsim::LimitMin(H, 0.0);
+        Nextsim::LimitMax(D, 1.0);
+        Nextsim::LimitMin(D, 1e-12);
+        Nextsim::GlobalTimer.stop("time loop - advection");
+
+        //////////////////////////////////////////////////
+        Nextsim::GlobalTimer.start("time loop - meb");
+        momentum.prepareIteration(H, A, D);
+        // MEB momentum subcycling
+        
+        
+        Nextsim::VPParameters VP;
+        constexpr double alpha = 1500.0;
+        constexpr double beta = 1500.0;
+        constexpr size_t NT_evp = 100;
+
+        for (size_t mebstep = 0; mebstep < NT_meb; ++mebstep) {
+            momentum.MEBStep(Params, NT_meb, dt_adv, H, A, D);
+            //momentum.BBMStep(Params, NT_meb, dt_adv, H, A, D);
+            //momentum.mEVPStep(VP, NT_evp, alpha, beta, dt_adv, H, A);
+
+            // <- MPI
+        }
+
+        Nextsim::GlobalTimer.stop("time loop - meb");
+
+        //////////////////////////////////////////////////
+        if (WRITE_VTK) // Output
+            if ((timestep % NT_vtk == 0)) {
+                std::cout << "VTK output at day " << time / 24. / 60. / 60. << std::endl;
+
+                int printstep = timestep / NT_vtk + 1.e-4;
+                Nextsim::GlobalTimer.start("time loop - i/o");
+                Nextsim::VTK::write_cg_velocity(resultsdir + "/vel", printstep, momentum.GetVx(), momentum.GetVy(), smesh);
+                Nextsim::VTK::write_dg(resultsdir + "/A", printstep, A, smesh);
+                Nextsim::VTK::write_dg(resultsdir + "/H", printstep, H, smesh);
+                Nextsim::VTK::write_dg(resultsdir + "/D", printstep, D, smesh);
+                //Nextsim::VTK::write_dg(resultsdir + "/S11", printstep, momentum.GetS11(), smesh);
+                //Nextsim::VTK::write_dg(resultsdir + "/S12", printstep, momentum.GetS12(), smesh);
+                //Nextsim::VTK::write_dg(resultsdir + "/S22", printstep, momentum.GetS22(), smesh);
+                Nextsim::VTK::write_dg(resultsdir + "/Div", printstep, Nextsim::Tools::TensorInvI(smesh, momentum.GetE11(), momentum.GetE12(), momentum.GetE22()), smesh);
+                Nextsim::VTK::write_dg(resultsdir + "/Shear", printstep, Nextsim::Tools::Shear(smesh, momentum.GetE11(), momentum.GetE12(), momentum.GetE22()), smesh);
+                Nextsim::VTK::write_dg(resultsdir + "/sigma_n", printstep,
+                    Nextsim::Tools::TensorInvI(smesh, momentum.GetS11(), momentum.GetS12(), momentum.GetS22()), smesh);
+                Nextsim::VTK::write_dg(resultsdir + "/tau", printstep,
+                    Nextsim::Tools::TensorInvII(smesh, momentum.GetS11(), momentum.GetS12(), momentum.GetS22()), smesh);
+                Nextsim::GlobalTimer.stop("time loop - i/o");
+            }
+    }
+    Nextsim::GlobalTimer.stop("time loop");
+
+    std::cout << std::endl;
+    Nextsim::GlobalTimer.print();
+}
+
+int main()
+{
+    
+    std::vector<std::string> meshes;
+    meshes.push_back("../ParametricMesh/compression_30x60.smesh");// 2km mesh
+    //meshes.push_back("../ParametricMesh/compression_60x120.smesh");// 1km mesh
+
+     for (const auto& it : meshes) {
+         run_benchmark<1, 3, 3>(it);
+         //run_benchmark<2, 6, 8>(it);
+     }
+}
