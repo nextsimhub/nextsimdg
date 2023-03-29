128fac88401472d22b29931dbca4255490bcd206
Piotr Minakowski <piotr.minakowski@ovgu.de>
2023-03-29 14:44:53 +0200

    .

--- dynamics/src/include/DynamicsKernel.hpp
+++ dynamics/src/include/DynamicsKernel.hpp
@@ -163,8 +163,18 @@ public:
         dgtransport->prepareAdvection(u, v);
 
         //! Perform transport step
+        //std::cout << "Before" << cice << std::endl;
+
+        
+        std::cout << "V" << v.row(10) << std::endl;
+        std::cout << "U" << u.row(10) << std::endl;
+
+        std::exit(0);
         dgtransport->step(dt_adv, cice);
-	    dgtransport->step(dt_adv, hice);
+	    
+        dgtransport->step(dt_adv, hice);
+        
+        //std::cout << "After" << cice << std::endl;
 
 
     };
