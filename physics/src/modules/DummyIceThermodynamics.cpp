1258d56135c78446ca989b6ed7410cbbdd2ac329
Tim Spain <timothy.spain@nersc.no>
2023-04-18 13:28:19 +0200

    Add a do-nothing vertical ice thermodynamics module.

---
+++ physics/src/modules/DummyIceThermodynamics.cpp
@@ -0,0 +1,12 @@
+/*!
+ * @file DummyIceThermodynamics.cpp
+ *
+ * @date 18 Apr 2023
+ * @author Tim Spain <timothy.spain@nersc.no>
+ */
+
+#include "include/DummyIceThermodynamics.hpp"
+
+namespace Nextsim {
+
+} /* namespace Nextsim */
