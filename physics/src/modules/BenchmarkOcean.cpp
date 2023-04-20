8ee8d37b99e55c8a86c9c8048acb7ae553ab4063
Tim Spain <timothy.spain@nersc.no>
2023-04-20 14:42:40 +0200

    Set the values other than velocity.

--- physics/src/modules/BenchmarkOcean.cpp
+++ physics/src/modules/BenchmarkOcean.cpp
@@ -7,11 +7,20 @@
 
 #include "include/BenchmarkOcean.hpp"
 
+#include "include/constants.hpp"
+
 namespace Nextsim {
 
 void BenchmarkOcean::setData(const ModelState::DataMap&)
 {
-
+    // The material parameters of the ocean are fixed to ensure no
+    // thermodynamics occurs
+    sst = -1.;
+    sss = 32;
+    tf = -1.8;
+    mld = 10.;
+    cpml = Water::rho * Water::cp * mld[0];
+    qio = 0;
 }
 
 void BenchmarkOcean::updateBefore(const TimestepTime& tst)
