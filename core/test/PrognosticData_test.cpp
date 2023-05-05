232488831700b3429900ac5ce878ba3c5f1aea0a
Tim Spain <timothy.spain@nersc.no>
2023-05-05 15:47:52 +0200

    Ignore the dynamics in the PrognosticData test.

--- core/test/PrognosticData_test.cpp
+++ core/test/PrognosticData_test.cpp
@@ -6,7 +6,6 @@
  */
 
 #include "include/PrognosticData.hpp"
-#include "include/DynamicsModule.hpp"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
@@ -19,66 +18,10 @@
 #include "include/Module.hpp"
 #include "include/UnescoFreezing.hpp"
 #include "include/constants.hpp"
-#include "include/DummyDynamics.hpp"
 
 #include <sstream>
 
-namespace Module {
-const std::string DUMMYDYNAMICS = "Nextsim::DummyDynamics";
-
-template <>
-Module<Nextsim::IDynamics>::map Module<Nextsim::IDynamics>::functionMap = {
-    { DUMMYDYNAMICS, newImpl<Nextsim::IDynamics, Nextsim::DummyDynamics> },
-};
-
-
-
-
-template <>
-Module<Nextsim::IDynamics>::fn Module<Nextsim::IDynamics>::spf = functionMap.at(DUMMYDYNAMICS);
-template <>
-std::unique_ptr<Nextsim::IDynamics> Module<Nextsim::IDynamics>::staticInstance
-    = std::move(newImpl<Nextsim::IDynamics, Nextsim::DummyDynamics>());
-
-template <>
-std::string Module<Nextsim::IDynamics>::moduleName(){ return "Nextsim::IDynamics"; }
-
-template<> HelpMap& getHelpRecursive<Nextsim::IDynamics>(HelpMap& map, bool getAll)
-{
-    const std::string& pfx = Nextsim::ConfiguredModule::MODULE_PREFIX;
-    map[pfx].push_back({ pfx + "." + Module<Nextsim::IDynamics>::moduleName(), ConfigType::MODULE,
-                         { DUMMYDYNAMICS }, DUMMYDYNAMICS, "",
-                         "MODULE DESCRIPTION HERE" });
-    return map;
-}
-template <>
-Nextsim::IDynamics& getImplementation<Nextsim::IDynamics>()
-{
-    return getImplTemplate<Nextsim::IDynamics, DynamicsModule>();
-}
-template <>
-void setImplementation<Nextsim::IDynamics>(const std::string& implName)
-{
-    setImplTemplate<DynamicsModule>(implName);
-}
-template <>
-std::unique_ptr<Nextsim::IDynamics> getInstance()
-{
-    return getInstTemplate<Nextsim::IDynamics, DynamicsModule>();
-}
-DynamicsModule::Constructor DynamicsModule::ctor;
-    DynamicsModule::Constructor::Constructor()
-    {
-        addToConfiguredModules<Nextsim::IDynamics, DynamicsModule>();
-    }
-
-} /* namespace Module */
-
 namespace Nextsim {
-
-class Dynamics : public DummyDynamics {
-    Dynamics () : DummyDynamics() {};
-};
 
 TEST_SUITE_BEGIN("PrognosticData");
 TEST_CASE("PrognosticData call order test")
