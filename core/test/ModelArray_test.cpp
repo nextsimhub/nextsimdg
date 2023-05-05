8d201aff5be8ecec1704dc32150774a0cdf9c52d
Tim Spain <timothy.spain@nersc.no>
2023-05-05 11:44:42 +0200

    Add a test of zIndexAndLayer.

--- core/test/ModelArray_test.cpp
+++ core/test/ModelArray_test.cpp
@@ -255,6 +255,36 @@ TEST_CASE("Location from index")
     REQUIRE(loc[1] == y);
     REQUIRE(loc[2] == z);
 }
+
+// Test the zIndexAndLayer function
+TEST_CASE("zIndexAndLayer")
+{
+    const size_t nx = 29;
+    const size_t ny = 23;
+    const size_t nz = 11;
+
+    ModelArray::setDimensions(ModelArray::Type::THREED, {nx, ny, nz});
+
+    ThreeDField threeD(ModelArray::Type::THREED);
+    threeD.resize();
+
+    size_t mul = 100;
+    // Fill the array fastest last
+    for (size_t i = 0; i < nx; ++i) {
+        for (size_t j = 0; j < ny; ++j) {
+            for (size_t k = 0; k < nz; ++k) {
+                threeD(i, j, k) = k + mul * (j + mul * (i));
+            }
+        }
+    }
+
+    size_t x = 19;
+    size_t y = 17;
+    size_t z = 7;
+    size_t ind = ModelArray::indexFromLocation(ModelArray::Type::TWOD, {x, y});
+    REQUIRE(threeD.zIndexAndLayer(ind, z) == threeD(x, y, z));
+
+}
 TEST_SUITE_END();
 
 } /* namespace Nextsim */
