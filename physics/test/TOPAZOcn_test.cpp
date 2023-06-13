59cb030754b8e0afa68aa6774cc0a3ee67272355
Tim Spain <timothy.spain@nersc.no>
2023-06-13 15:55:01 +0200

    Switch TOPAZ test values to the new dimension ordering.

--- physics/test/TOPAZOcn_test.cpp
+++ physics/test/TOPAZOcn_test.cpp
@@ -73,26 +73,29 @@ TEST_CASE("TOPAZOcean test")
 
     double mdi = -2.03703597633448608e90;
 
+    // Use this, rather than the literal 0.035045, as the two are not equal at double precision
+    double targetFrac = 35 * 0.001 + 45 * 0.000001;
+
     REQUIRE(sst(0, 0) == mdi);
     REQUIRE(sst(32, 32) == -0.032032);
-    REQUIRE(sst(45, 35) == -0.045035);
-    REQUIRE(mld(45, 35) == 10.045035);
+    REQUIRE(sst(45, 35) == -(0+targetFrac));
+    REQUIRE(mld(45, 35) == (10+targetFrac));
 
     TimePoint t2("2000-02-01T00:00:00Z");
     topaz.updateBefore({ t2, Duration(600) });
 
     REQUIRE(sst(0, 0) == mdi);
     REQUIRE(sst(32, 32) == -0.032032 - 1);
-    REQUIRE(sst(45, 35) == -0.045035 - 1);
-    REQUIRE(mld(45, 35) == 10.045035 + 1);
+    REQUIRE(sst(45, 35) == -(0+targetFrac) - 1);
+    REQUIRE(mld(45, 35) == (10+targetFrac) + 1);
 
     TimePoint t12("2000-12-01T00:00:00Z");
     topaz.updateBefore({ t12, Duration(600) });
 
     REQUIRE(sst(0, 0) == mdi);
     REQUIRE(sst(32, 32) == -0.032032 - 11);
-    REQUIRE(sst(45, 35) == -0.045035 - 11);
-    REQUIRE(mld(45, 35) == 10.045035 + 11);
+    REQUIRE(sst(45, 35) == -(0 + targetFrac) - 11);
+    REQUIRE(mld(45, 35) == (10+targetFrac) + 11);
 
     // All times after the last time sample should use the last sample's data
     TimePoint t120("2010-01-01T00:00:00Z");
@@ -100,8 +103,8 @@ TEST_CASE("TOPAZOcean test")
 
     REQUIRE(sst(0, 0) == mdi);
     REQUIRE(sst(32, 32) == -0.032032 - 11);
-    REQUIRE(sst(45, 35) == -0.045035 - 11);
-    REQUIRE(mld(45, 35) == 10.045035 + 11);
+    REQUIRE(sst(45, 35) == -(0 + targetFrac) - 11);
+    REQUIRE(mld(45, 35) == (10 + targetFrac) + 11);
 
     std::filesystem::remove(filePath);
 }
