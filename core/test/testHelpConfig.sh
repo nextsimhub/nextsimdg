865cc1345d59c8314cb71ee4e1489d822ce4c761
Tim Spain <timothy.spain@nersc.no>
2023-08-09 09:15:15 +0200

    Better messages for the help config test.

--- core/test/testHelpConfig.sh
+++ core/test/testHelpConfig.sh
@@ -11,10 +11,10 @@ HELP_TEXT=`./nextsim --help 2>&1`
 HELPCFG_TEXT=`./nextsim --help-config 2>&1`
 
 if [ "$HELP_TEXT" == "$HELPCFG_TEXT" ]; then
-    echo "Test 1 failed :("
+    echo "Test failed: --help-config option not recognized."
     exit 1
 else
-    echo "Test 1 passed :)"
+    echo "Test passed: --help-config option recognized."
 fi
 
 # Check the text produced by an implicit argument and by an argument of 'all'
@@ -23,9 +23,9 @@ fi
 HELPALL_TEXT=`./nextsim --help-config all 2>&1`
 
 if [ "$HELPALL_TEXT" == "$HELPCFG_TEXT" ]; then
-    echo "Test 2 passed :)"
+    echo "Test passed: implicit value accepted."
 else
-    echo "Test 2 failed :("
+    echo "Test failed: implicit value not accepted."
     exit 1
 fi
 
@@ -34,9 +34,9 @@ fi
 HELPAVAIL_TEXT=`./nextsim --help-config avail 2>&1`
 
 if [[ "$HELPAVAIL_TEXT" == *"CCSMIceAlbedo"* ]]; then
-    echo "Test 3 passed :)"
+    echo "Test passed: CCSMIceAlbedo has configuration help."
 else
-    echo "Test 3 failed :("
+    echo "Test failed: CCSMIceAlbedo is not listed as having configuration help."
     exit 1
 fi
 
@@ -46,8 +46,8 @@ HELPCCSM_TEXT=`./nextsim --help-config CCSMIceAlbedo 2>&1`
 
 if [[ "$HELPCCSM_TEXT" == *"CCSMIceAlbedo.iceAlbedo"* ]] &&
     [[ "$HELPCCSM_TEXT" == *"CCSMIceAlbedo.snowAlbedo"* ]]; then
-    echo "Test 4 passed :)"
+    echo "Test passed: CCSMIceAlbedo configuration help contains the expected fields."
 else
-    echo "Test 4 failed :("
+    echo "Test failed: CCSMIceAlbedo configuration help does not contain the expected fields."
     exit 1
 fi
