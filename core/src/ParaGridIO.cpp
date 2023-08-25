d95119f22a2f629f39a92f05039bd0dd19965601
Tim Spain <timothy.spain@nersc.no>
2023-08-25 17:41:18 +0200

    Make ParaGridIO compatible with reversed array index ordering.

--- core/src/ParaGridIO.cpp
+++ core/src/ParaGridIO.cpp
@@ -17,6 +17,7 @@
 #include <ncGroup.h>
 #include <ncVar.h>
 
+#include <algorithm>
 #include <cstdlib>
 #include <map>
 #include <string>
@@ -24,12 +25,12 @@
 namespace Nextsim {
 
 const std::map<std::string, ModelArray::Type> ParaGridIO::dimensionKeys = {
-    { "xy", ModelArray::Type::H },
-    { "xyz", ModelArray::Type::Z },
-    { "xydg_comp", ModelArray::Type::DG },
-    { "xydgstress_comp", ModelArray::Type::DGSTRESS },
-    { "xcgycg", ModelArray::Type::CG },
-    { "xvertexyvertexncoords", ModelArray::Type::VERTEX },
+    { "yx", ModelArray::Type::H },
+    { "zyx", ModelArray::Type::Z },
+    { "yxdg_comp", ModelArray::Type::DG },
+    { "yxdgstress_comp", ModelArray::Type::DGSTRESS },
+    { "ycgxcg", ModelArray::Type::CG },
+    { "yvertexxvertexncoords", ModelArray::Type::VERTEX },
 };
 
 // Which dimensions are DG dimension, which could be legitimately missing
@@ -115,6 +116,7 @@ ModelState ParaGridIO::getModelState(const std::string& filePath)
         if (newType == ModelArray::Type::Z) {
             std::vector<size_t> startVector(ModelArray::nDimensions(newType), 0);
             std::vector<size_t> extentVector = ModelArray::dimensions(newType);
+            std::reverse(extentVector.begin(), extentVector.end());
             var.getVar(startVector, extentVector, &data[0]);
         } else {
             var.getVar(&data[0]);
@@ -198,12 +200,19 @@ void ParaGridIO::dumpModelState(
     for (auto entry : ModelArray::typeDimensions) {
         ModelArray::Type type = entry.first;
         std::vector<netCDF::NcDim> ncDims;
-        for (ModelArray::Dimension& maDim : entry.second) {
+        for (auto iter = entry.second.rbegin(); iter != entry.second.rend(); ++iter) {
+            ModelArray::Dimension& maDim = *iter;
             ncDims.push_back(ncFromMAMap.at(maDim));
         }
         dimMap[type] = ncDims;
     }
-    // Everything that has components needs that dimension, too
+    // Reverse the order of the dimensions to translate between column-major ModelArray and
+    // row-major netCDF
+    for (auto& [type, v] : dimMap) {
+        std::reverse(v.begin(), v.end());
+    }
+    // Everything that has components needs that dimension, too. This always varies fastest, and so
+    // is last in the vector of dimensions.
     for (auto entry : dimCompMap) {
         dimMap.at(entry.second).push_back(ncFromMAMap.at(entry.first));
     }
@@ -285,7 +294,8 @@ void ParaGridIO::writeDiagnosticTime(
             // For VERTEX in an existing file, there is nothing more to be done
             continue;
         }
-        for (ModelArray::Dimension& maDim : entry.second) {
+        for (auto iter = entry.second.rbegin(); iter != entry.second.rend(); ++iter) {
+            ModelArray::Dimension& maDim = *iter;
             ncDims.push_back(ncFromMAMap.at(maDim));
             indexArray.push_back(0);
             extentArray.push_back(ModelArray::definedDimensions.at(maDim).length);
