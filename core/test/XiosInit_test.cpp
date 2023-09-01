b198cdb9d74baa8583b540777eb170f7e5de3919
Alexander Smith <as3402@cam.ac.uk>
2023-09-01 16:29:13 +0100

    Upgrade XiosInitTest To Doctest

--- core/test/XiosInit_test.cpp
+++ core/test/XiosInit_test.cpp
@@ -9,8 +9,12 @@
  * file serves as a set of unit tests, the system tests are elsewhere.
  */
 
-#define CATCH_CONFIG_RUNNER
-#include <catch2/catch.hpp>
+// #define CATCH_CONFIG_RUNNER
+// #include <catch2/catch.hpp>
+// #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest/doctest.h>
+
 #include <mpi.h>
 #include "include/Xios.hpp"
 #include <iostream>
@@ -20,6 +24,21 @@
 Nextsim::Xios *xios_handler;
 
 int main( int argc, char* argv[] ) {
+
+    doctest::Context context;
+    // // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
+
+    // // defaults
+    // context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
+    // context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
+    // context.setOption("order-by", "name");            // sort the test cases by their name
+
+    // context.applyCommandLine(argc, argv);
+
+    // // overrides
+    // context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
+
+
     // Enable xios in the 'config'
     Nextsim::Configurator::clearStreams();
     std::stringstream config;
@@ -36,7 +55,11 @@ int main( int argc, char* argv[] ) {
     // TODO: Investigate and find workaround
     xios_handler = new Nextsim::Xios::Xios(NULL, NULL);
 
-    int result = Catch::Session().run( argc, argv );
+    //int result = Catch::Session().run( argc, argv );
+    int result = context.run();// argc, argv );
+
+    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+        return result;          // propagate the result of the tests
 
     // global clean-up...
     delete xios_handler;
