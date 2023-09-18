07a1f5502af77074db0a5f60319e65ae8ced1fcd
Tim Spain <timothy.spain@nersc.no>
2023-09-18 17:32:08 +0200

    Explicitly correct the restart file land mask.

--- run/make_init25kmNH.py
+++ run/make_init25kmNH.py
@@ -162,7 +162,8 @@ if __name__ == "__main__":
     mask = datagrp.createVariable("mask", "f8", field_dims)
     sst_data = topaz4_interpolate(element_lon, element_lat, source_file["temperature"][0, :, :].squeeze(), lat_array)
     mask[:, :] = 1 - ma.getmask(sst_data)
-
+    mask[100, 95] = 0.0 # Ad-hoc fix for the westernmost point of the New Siberian Islands
+    
     # Ice concentration and thickness
     cice_data = topaz4_interpolate(element_lon, element_lat, source_file["fice"][0, :, :].squeeze(), lat_array)
     hice_data = topaz4_interpolate(element_lon, element_lat, source_file["hice"][0, :, :].squeeze(), lat_array)
