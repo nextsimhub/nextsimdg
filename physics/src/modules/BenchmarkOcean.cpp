dce4ce501858e15b1abd86c13c74b74377e3a8a9
Tim Spain <timothy.spain@nersc.no>
2023-09-26 11:14:46 +0200

    Calculate the ocean currents in BenchmarkOcean.

--- physics/src/modules/BenchmarkOcean.cpp
+++ physics/src/modules/BenchmarkOcean.cpp
@@ -7,12 +7,14 @@
 
 #include "include/BenchmarkOcean.hpp"
 
+#include "include/BenchmarkCoordinates.hpp"
 #include "include/constants.hpp"
 
 namespace Nextsim {
 
 void BenchmarkOcean::setData(const ModelState::DataMap&)
 {
+    BenchmarkCoordinates::setData();
     // The material parameters of the ocean are fixed to ensure no
     // thermodynamics occurs
     sst = -1.;
@@ -21,15 +23,16 @@ void BenchmarkOcean::setData(const ModelState::DataMap&)
     mld = 10.;
     cpml = Water::rho * Water::cp * mld[0];
     qio = 0;
-}
 
-void BenchmarkOcean::updateBefore(const TimestepTime& tst)
-{
     // The time and length scales of the current generation function
     constexpr double L = 512000.; // Size of the domain in km
     constexpr double vMaxOcean = 0.01; // 1 cm/s in m/s
     constexpr double T = 8 * 86400; // 8 days in seconds
 
-
+    // The currents are constant wrt time and space
+    u = vMaxOcean * (2 * BenchmarkCoordinates::fy() - 1);
+    v = vMaxOcean * (1 - 2 * BenchmarkCoordinates::fx());
 }
+
+void BenchmarkOcean::updateBefore(const TimestepTime& tst) { }
 } /* namespace Nextsim */
