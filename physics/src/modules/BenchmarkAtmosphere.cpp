592f25973e2f7dfc6f57c570e8ddd4819ba9077c
Tim Spain <timothy.spain@nersc.no>
2023-09-28 12:04:26 +0200

    Clean up BenchmarkAtmosphere.

--- physics/src/modules/BenchmarkAtmosphere.cpp
+++ physics/src/modules/BenchmarkAtmosphere.cpp
@@ -46,23 +46,23 @@ void BenchmarkAtmosphere::update(const TimestepTime& tst)
     double cycloneDuration = 5.; // days
 
     // cyclone parameters
-    constexpr double A = 1e-5;
-    constexpr double k = 1e-5; // m⁻¹
+    const double A = 1e-5;
+    const double k = 1e-5; // m⁻¹
     const double alpha = 72. / 180. * M_PI;
-    constexpr double cosalpha = cos(alpha);
-    constexpr double sinalpha = sin(alpha);
+    const double cosalpha = cos(alpha);
+    const double sinalpha = sin(alpha);
 
     // centre of the cyclone (metres)
-    double x0 = BenchmarkCoordinates::nx * BenchmarkCoordinates::dx * 0.5 * (1 + timeFraction / cycloneDuration);
-    double y0 = BenchmarkCoordinates::ny * BenchmarkCoordinates::dy * 0.5 * (1 + timeFraction / cycloneDuration);
+    double x0 = BenchmarkCoordinates::nx() * BenchmarkCoordinates::dx * 0.5 * (1 + timeFraction / cycloneDuration);
+    double y0 = BenchmarkCoordinates::ny() * BenchmarkCoordinates::dy * 0.5 * (1 + timeFraction / cycloneDuration);
 
     // distance from the centre of the cyclone
     const ModelArray& xPrime = BenchmarkCoordinates::x() - x0;
     const ModelArray& yPrime = BenchmarkCoordinates::y() - y0;
 
     // Perform the rest of the calculation per element
-    for (size_t j = 0; j < BenchmarkCoordinates::ny; ++j) {
-        for (size_t i = 0; j < BenchmarkCoordinates::nx; ++i) {
+    for (size_t j = 0; j < BenchmarkCoordinates::ny(); ++j) {
+        for (size_t i = 0; i < BenchmarkCoordinates::nx(); ++i) {
             // Expression taken from the original implementation:
             // double scale = exp(1.0) / 100.0 * exp(-0.01e-3 * sqrt(SQR(x - cMx) + SQR(y - cMy))) * 1.e-3;
             double scale = A * exp(-k * hypot(xPrime(i, j), yPrime(i, j)));
