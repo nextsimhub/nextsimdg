2a770c07adec6c13a326fddc2d0b7dc33dfcc8bb
Tim Spain <timothy.spain@nersc.no>
2023-11-20 07:16:40 +0100

    Break the module_builder strings into a separate python module.

--- scripts/module_builder.py
+++ scripts/module_builder.py
@@ -6,25 +6,7 @@
 
 import configparser
 import os
-
-default_str = "DEFAULT"
-module_file_str = "module.cfg"
-module_section_str = "module"
-module_file_prefix = "module"
-header_suffix = "hpp"
-source_suffix = "cpp"
-name_str = "name"
-file_prefix_str = "file_prefix"
-interface_prefix_str = "interface_prefix"
-description_str = "description"
-has_help_str = "has_help"
-is_default_str = "is_default"
-file_encoding = "utf-8"
-true_str = "true"
-
-# Variable names for the dict of common strings
-class_name = "class_name"
-module_class_name = "module_class_name"
+from module_strings import *
 
 # A valid implementation section has an entry "file_prefix = "
 def is_impl_section_valid(section):
