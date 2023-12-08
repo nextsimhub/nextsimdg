f7451eec18591d09a0cbdcd6b6f044f042835996
Tim Spain <timothy.spain@nersc.no>
2023-12-08 14:36:43 +0100

    Further minimize the differences in the albedo source files.

--- physics/src/modules/IceAlbedoModule/include/CCSMIceAlbedo.hpp
+++ physics/src/modules/IceAlbedoModule/include/CCSMIceAlbedo.hpp
@@ -17,10 +17,12 @@ namespace Nextsim {
 class CCSMIceAlbedo : public IIceAlbedo, public Configured<CCSMIceAlbedo> {
 public:
     /*!
-     * @brief Calculates the CCSM ice surface short wave albedo.
-     *
+     * @brief Calculates the CCSM3 ice surface short wave albedo and fraction of penetrating
+     * short-wave radiation.
      * @param temperature The temperature of the ice surface.
      * @param snowThickness The true snow thickness on top of the ice.
+     * @param i0 The fraction of short-wave radiation that can penetrate bare ice (not taking snow
+     * cover into account).
      */
     std::tuple<double, double> surfaceShortWaveBalance(double temperature, double snowThickness, double i0) override;
 
