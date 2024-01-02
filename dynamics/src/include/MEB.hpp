d41ccb43419d2e2179a9c667c46982eb8ae8748d
Tim Spain <timothy.spain@nersc.no>
2024-01-02 10:15:25 +0100

    Typedefs and constexprs for nicer type names (NGaussPoints).

--- dynamics/src/include/MEB.hpp
+++ dynamics/src/include/MEB.hpp
@@ -54,43 +54,44 @@ namespace MEB {
         const DGVector<DGa>& A, DGVector<DGa>& D,
         const double dt_mom)
     {
-//#define NGP (DGs == 8 ? 3 : (DGs == 3 ? 2 : -1))
-#define NGP 3
+        constexpr size_t NGaussPoints = (DGs == 8 ? 3 : (DGs == 3 ? 2 : -1));
+        typedef Eigen::Matrix<double, 1, NGaussPoints * NGaussPoints> GaussPointVector;
+
 
         //! Stress and Damage Update
 #pragma omp parallel for
         for (size_t i = 0; i < smesh.nelements; ++i) {
 
             //! Evaluate values in Gauss points (3 point Gauss rule in 2d => 9 points)
-            const Eigen::Matrix<double, 1, NGP* NGP> h_gauss = (H.row(i) * PSI<DGa, NGP>).array().max(0.0).matrix();
-            const Eigen::Matrix<double, 1, NGP* NGP> a_gauss = (A.row(i) * PSI<DGa, NGP>).array().max(0.0).min(1.0).matrix();
-            Eigen::Matrix<double, 1, NGP* NGP> d_gauss = (D.row(i) * PSI<DGa, NGP>).array().max(1e-12).min(1.0).matrix();
+            const GaussPointVector h_gauss = (H.row(i) * PSI<DGa, NGaussPoints>).array().max(0.0).matrix();
+            const GaussPointVector a_gauss = (A.row(i) * PSI<DGa, NGaussPoints>).array().max(0.0).min(1.0).matrix();
+            GaussPointVector d_gauss = (D.row(i) * PSI<DGa, NGaussPoints>).array().max(1e-12).min(1.0).matrix();
 
-            const Eigen::Matrix<double, 1, NGP* NGP> e11_gauss = E11.row(i) * PSI<DGs, NGP>;
-            const Eigen::Matrix<double, 1, NGP* NGP> e12_gauss = E12.row(i) * PSI<DGs, NGP>;
-            const Eigen::Matrix<double, 1, NGP* NGP> e22_gauss = E22.row(i) * PSI<DGs, NGP>;
+            const GaussPointVector e11_gauss = E11.row(i) * PSI<DGs, NGaussPoints>;
+            const GaussPointVector e12_gauss = E12.row(i) * PSI<DGs, NGaussPoints>;
+            const GaussPointVector e22_gauss = E22.row(i) * PSI<DGs, NGaussPoints>;
 
-            Eigen::Matrix<double, 1, NGP* NGP> s11_gauss = S11.row(i) * PSI<DGs, NGP>;
-            Eigen::Matrix<double, 1, NGP* NGP> s12_gauss = S12.row(i) * PSI<DGs, NGP>;
-            Eigen::Matrix<double, 1, NGP* NGP> s22_gauss = S22.row(i) * PSI<DGs, NGP>;
+            GaussPointVector s11_gauss = S11.row(i) * PSI<DGs, NGaussPoints>;
+            GaussPointVector s12_gauss = S12.row(i) * PSI<DGs, NGaussPoints>;
+            GaussPointVector s22_gauss = S22.row(i) * PSI<DGs, NGaussPoints>;
 
             //! exp(-C(1-A))
-            const Eigen::Matrix<double, 1, NGP* NGP> expC = (params.compaction_param * (1.0 - a_gauss.array())).exp().array();
+            const GaussPointVector expC = (params.compaction_param * (1.0 - a_gauss.array())).exp().array();
 
             // Eqn. 20
-            Eigen::Matrix<double, 1, NGP* NGP> powalpha = (d_gauss.array()).pow(params.exponent_relaxation_sigma - 1.);
-            const Eigen::Matrix<double, 1, NGP* NGP> time_viscous = (params.undamaged_time_relaxation_sigma * powalpha.array() ).matrix();
+            GaussPointVector powalpha = (d_gauss.array()).pow(params.exponent_relaxation_sigma - 1.);
+            const GaussPointVector time_viscous = (params.undamaged_time_relaxation_sigma * powalpha.array() ).matrix();
 
             // Eqn. 4: first factor on RHS
             const double Dunit_factor = 1. / (1. - (params.nu0 * params.nu0));
 
             //! MEB
             // 1. / (1. + dt / lambda) 
-            Eigen::Matrix<double, 1, NGP* NGP> multiplicator = (1. / (1. + dt_mom / time_viscous.array())).matrix();
+            GaussPointVector multiplicator = (1. / (1. + dt_mom / time_viscous.array())).matrix();
 
             
             //! Eqn. 24
-            const Eigen::Matrix<double, 1, NGP* NGP> elasticity = h_gauss.array() * params.young * d_gauss.array() * expC.array();
+            const GaussPointVector elasticity = h_gauss.array() * params.young * d_gauss.array() * expC.array();
 
             // Eqn. 4: first factor on RHS
             /* Stiffness matrix
@@ -113,13 +114,13 @@ namespace MEB {
             s22_gauss.array() *= multiplicator.array();
 
             //! Current normal and tangent stress for the evaluation Mohr-Couloumb (Eqn 13)
-            const Eigen::Matrix<double, 1, NGP* NGP> sigma_n = 0.5 * (s11_gauss.array() + s22_gauss.array());
-            const Eigen::Matrix<double, 1, NGP* NGP> tau = (0.25 * (s11_gauss.array() - s22_gauss.array()).square() + s12_gauss.array().square()).sqrt();
+            const GaussPointVector sigma_n = 0.5 * (s11_gauss.array() + s22_gauss.array());
+            const GaussPointVector tau = (0.25 * (s11_gauss.array() - s22_gauss.array()).square() + s12_gauss.array().square()).sqrt();
 
-            Eigen::Matrix<double, 1, NGP* NGP> dcrit = Eigen::Matrix<double, 1, NGP* NGP>::Ones();
+            GaussPointVector dcrit = GaussPointVector::Ones();
 
             // Fixed Cohesion
-            const Eigen::Matrix<double, 1, NGP* NGP> cohesion = params.c0 * h_gauss.array() ;
+            const GaussPointVector cohesion = params.c0 * h_gauss.array() ;
             
             
             //! This is not part of Dansereau et al. 2016
@@ -151,24 +152,22 @@ namespace MEB {
 
 
             // INTEGRATION OF STRESS AND DAMAGE
-            const Eigen::Matrix<Nextsim::FloatType, 1, NGP* NGP> J = ParametricTools::J<3>(smesh, i);
+            const Eigen::Matrix<Nextsim::FloatType, 1, NGaussPoints * NGaussPoints> J = ParametricTools::J<3>(smesh, i);
             // get the inverse of the mass matrix scaled with the test-functions in the gauss points,
             // with the gauss weights and with J. This is a 8 x 9 matrix
-            const Eigen::Matrix<Nextsim::FloatType, DGs, NGP* NGP> imass_psi = ParametricTools::massMatrix<DGs>(smesh, i).inverse()
-                * (PSI<DGs, NGP>.array().rowwise() * (GAUSSWEIGHTS<NGP>.array() * J.array())).matrix();
+            const Eigen::Matrix<Nextsim::FloatType, DGs, NGaussPoints * NGaussPoints> imass_psi = ParametricTools::massMatrix<DGs>(smesh, i).inverse()
+                * (PSI<DGs, NGaussPoints>.array().rowwise() * (GAUSSWEIGHTS<NGaussPoints>.array() * J.array())).matrix();
 
             S11.row(i) = imass_psi * s11_gauss.matrix().transpose();
             S12.row(i) = imass_psi * s12_gauss.matrix().transpose();
             S22.row(i) = imass_psi * s22_gauss.matrix().transpose();
 
-            const Eigen::Matrix<Nextsim::FloatType, DGa, NGP* NGP> imass_psi2 = ParametricTools::massMatrix<DGa>(smesh, i).inverse()
-                * (PSI<DGa, NGP>.array().rowwise() * (GAUSSWEIGHTS<NGP>.array() * J.array())).matrix();
+            const Eigen::Matrix<Nextsim::FloatType, DGa, NGaussPoints * NGaussPoints> imass_psi2 = ParametricTools::massMatrix<DGa>(smesh, i).inverse()
+                * (PSI<DGa, NGaussPoints>.array().rowwise() * (GAUSSWEIGHTS<NGaussPoints>.array() * J.array())).matrix();
 
             D.row(i) = imass_psi2 * d_gauss.matrix().transpose();
         }
     }
-
-#undef NGP
 
 } /* namespace MEB */
 
