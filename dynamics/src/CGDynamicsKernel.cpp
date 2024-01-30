6bbb1cd44b79afceb56a2352d5ca9e655c78a50d
Tim Spain <timothy.spain@nersc.no>
2024-01-30 16:44:43 +0100

    Fully separate the DG and CG components of DynamicsKernel.

--- dynamics/src/CGDynamicsKernel.cpp
+++ dynamics/src/CGDynamicsKernel.cpp
@@ -10,53 +10,86 @@
  */
 
 #include "include/DynamicsKernel.hpp"
+#include "include/ModelArray.hpp"
 
 // Import this from the build system *somehow*
 static const int CGdegree = 2;
+static const int DGstressDegree = CG2DGSTRESS(CGdegree);
+
 
 namespace Nextsim {
 
 template <int DGadvection, int DGstress>
-DynamicsKernel<DGadvection, DGstress>::DynamicsKernel() = default;
-
-template <int DGadvection, int DGstress>
-DynamicsKernel<DGadvection, DGstress>::~DynamicsKernel() = default;
-
-template <int DGadvection, int DGstress>
 void DynamicsKernel<DGadvection, DGstress>::initialise(const ModelArray& coords, bool isSpherical, const ModelArray& mask)
 {
-    if (isSpherical)
-        throw std::runtime_error("DG dynamics do not yet handle spherical coordinates.");
-        // TODO handle spherical coordinates
-
-    //! Define the spatial mesh
-    smesh = new ParametricMesh(Nextsim::CARTESIAN);
-
-    smesh->coordinatesFromModelArray(coords);
-    smesh->landmaskFromModelArray(mask);
-    smesh->dirichletFromMask();
-    // TODO: handle periodic and open edges
-    for (ParametricMesh::Edge edge : ParametricMesh::edges) {
-        smesh->dirichletFromEdge(edge);
-    }
-
-    //! Initialize transport
-    dgtransport = new Nextsim::DGTransport<DGadvection>(*smesh);
-    dgtransport->settimesteppingscheme("rk2");
-
-    //! Initialize stress transport
-    stresstransport = new Nextsim::DGTransport<CG2DGSTRESS(CGdegree)>(*smesh);
-    stresstransport->settimesteppingscheme("rk2");
+    initialiseDG(coords, isSpherical, mask);
 
     //! Initialize momentum
     internals.momentum = new Nextsim::CGParametricMomentum<CGdegree>(*smesh);
 
-    // resize CG and DG vectors
-    hice.resize_by_mesh(*smesh);
-    cice.resize_by_mesh(*smesh);
-
     internals.u.resize_by_mesh(*smesh);
     internals.v.resize_by_mesh(*smesh);
+}
+
+template <int DGadvection, int DGstress>
+void DynamicsKernel<DGadvection, DGstress>::setVelocityData(const std::string& name, const ModelArray& data)
+{
+    if (name == uName) {
+        // FIXME take into account possibility to restart form CG
+        // CGModelArray::ma2cg(data, u);
+        DGVector<DGadvection> utmp(*smesh);
+        DGModelArray::ma2dg(data, utmp);
+        Nextsim::Interpolations::DG2CG(*smesh, internals.u, utmp);
+    } else if (name == vName) {
+        // CGModelArray::ma2cg(data, v);
+        DGVector<DGadvection> vtmp(*smesh);
+        DGModelArray::ma2dg(data, vtmp);
+        Nextsim::Interpolations::DG2CG(*smesh, internals.v, vtmp);
+    } else if (name == uWindName) {
+        DGVector<DGadvection> utmp(*smesh);
+        DGModelArray::ma2dg(data, utmp);
+        Nextsim::Interpolations::DG2CG(*smesh, internals.momentum->GetAtmx(), utmp);
+    } else if (name == vWindName) {
+        DGVector<DGadvection> vtmp(*smesh);
+        DGModelArray::ma2dg(data, vtmp);
+        Nextsim::Interpolations::DG2CG(*smesh, internals.momentum->GetAtmy(), vtmp);
+    } else if (name == uOceanName) {
+        DGVector<DGadvection> utmp(*smesh);
+        DGModelArray::ma2dg(data, utmp);
+        Nextsim::Interpolations::DG2CG(*smesh, internals.momentum->GetOceanx(), utmp);
+    } else if (name == vOceanName) {
+        DGVector<DGadvection> vtmp(*smesh);
+        DGModelArray::ma2dg(data, vtmp);
+        Nextsim::Interpolations::DG2CG(*smesh, internals.momentum->GetOceany(), vtmp);
+    }
+}
+
+template <int DGadvection, int DGstress>
+ModelArray DynamicsKernel<DGadvection, DGstress>::getVelocityDG0Data(const std::string& name)
+{
+    if (name == uName) {
+        ModelArray data(ModelArray::Type::U);
+        DGVector<DGadvection> utmp(*smesh);
+        Nextsim::Interpolations::CG2DG(*smesh, utmp, internals.momentum->u);
+        return DGModelArray::dg2ma(utmp, data);
+    } else if (name == vName) {
+        ModelArray data(ModelArray::Type::V);
+        DGVector<DGadvection> vtmp(*smesh);
+        Nextsim::Interpolations::CG2DG(*smesh, vtmp, internals.momentum->v);
+        return DGModelArray::dg2ma(vtmp, data);
+    } else {
+        ModelArray noData(ModelArray::Type::H);
+        noData.resize();
+        noData = 0;
+        return noData;
+    }
+}
+
+template <int DGadvection, int DGstress>
+void DynamicsKernel<DGadvection, DGstress>::prepareAdvection()
+{
+    dgtransport->prepareTransport(internals.momentum->u, internals.momentum->v);
+    stresstransport->prepareTransport(internals.momentum->u, internals.momentum->v);
 }
 
 template <int DGdegree>
@@ -69,4 +102,21 @@ class DynamicsInternals {
 
     friend DynamicsKernel<DGdegree, CG2DGSTRESS(CGdegree)>;
 };
+
+// Instantiate the templates for all (1, 2) degrees of DGadvection
+
+template class DynamicsKernel<1, DGstressDegree>;
+template void DynamicsKernel<1, DGstressDegree>::initialise(const ModelArray& coords, bool isSpherical, const ModelArray& mask);
+template void DynamicsKernel<1, DGstressDegree>::setVelocityData(const std::string& name, const ModelArray& data);
+template ModelArray DynamicsKernel<1, DGstressDegree>::getVelocityDG0Data(const std::string& name);
+template void DynamicsKernel<1, DGstressDegree>::prepareAdvection();
+template class DynamicsInternals<1>;
+
+template class DynamicsKernel<2, DGstressDegree>;
+template void DynamicsKernel<2, DGstressDegree>::initialise(const ModelArray& coords, bool isSpherical, const ModelArray& mask);
+template void DynamicsKernel<2, DGstressDegree>::setVelocityData(const std::string& name, const ModelArray& data);
+template ModelArray DynamicsKernel<2, DGstressDegree>::getVelocityDG0Data(const std::string& name);
+template void DynamicsKernel<2, DGstressDegree>::prepareAdvection();
+template class DynamicsInternals<2>;
+
 }
--- dynamics/src/include/DynamicsKernel.hpp
+++ dynamics/src/include/DynamicsKernel.hpp
@@ -20,7 +20,6 @@
 #include "dgVector.hpp"
 #include "dgVisu.hpp"
 
-#include "CGModelArray.hpp"
 #include "DGModelArray.hpp"
 #include "include/ModelArray.hpp"
 #include "include/Time.hpp"
@@ -42,7 +41,7 @@ public:
     typedef std::pair<const std::string, const DGVector<DGadvection>&> DataMapping;
     typedef std::map<DataMapping::first_type, DataMapping::second_type> DataMap;
 
-    DynamicsKernel();
+    DynamicsKernel() = default;
     virtual ~DynamicsKernel() = default;
     void initialise(const ModelArray& coords, bool isSpherical, const ModelArray& mask);
     /*!
@@ -69,33 +68,8 @@ public:
             DGModelArray::ma2dg(data, hice);
         } else if (name == ciceName) {
             DGModelArray::ma2dg(data, cice);
-        } else if (name == "u") {
-            // FIXME take into account possibility to restart form CG
-            // CGModelArray::ma2cg(data, u);
-            DGVector<DGadvection> utmp(*smesh);
-            DGModelArray::ma2dg(data, utmp);
-            Nextsim::Interpolations::DG2CG(*smesh, u, utmp);
-        } else if (name == "v") {
-            // CGModelArray::ma2cg(data, v);
-            DGVector<DGadvection> vtmp(*smesh);
-            DGModelArray::ma2dg(data, vtmp);
-            Nextsim::Interpolations::DG2CG(*smesh, v, vtmp);
-        } else if (name == uWindName) {
-            DGVector<DGadvection> utmp(*smesh);
-            DGModelArray::ma2dg(data, utmp);
-            Nextsim::Interpolations::DG2CG(*smesh, momentum->GetAtmx(), utmp);
-        } else if (name == vWindName) {
-            DGVector<DGadvection> vtmp(*smesh);
-            DGModelArray::ma2dg(data, vtmp);
-            Nextsim::Interpolations::DG2CG(*smesh, momentum->GetAtmy(), vtmp);
-        } else if (name == uOceanName) {
-            DGVector<DGadvection> utmp(*smesh);
-            DGModelArray::ma2dg(data, utmp);
-            Nextsim::Interpolations::DG2CG(*smesh, momentum->GetOceanx(), utmp);
-        } else if (name == vOceanName) {
-            DGVector<DGadvection> vtmp(*smesh);
-            DGModelArray::ma2dg(data, vtmp);
-            Nextsim::Interpolations::DG2CG(*smesh, momentum->GetOceany(), vtmp);
+        } else if (velocityFields.count(name)) {
+            setVelocityData(name, data);
         } else {
             // All other fields get shoved in a (labelled) bucket
             DGModelArray::ma2dg(data, advectedFields[name]);
@@ -116,14 +90,8 @@ public:
             return DGModelArray::dg2ma(hice, data);
         } else if (name == ciceName) {
             return DGModelArray::dg2ma(cice, data);
-        } else if (name == uName) {
-            DGVector<DGadvection> utmp(*smesh);
-            Nextsim::Interpolations::CG2DG(*smesh, utmp, momentum->GetVx());
-            return DGModelArray::dg2ma(utmp, data);
-        } else if (name == vName) {
-            DGVector<DGadvection> vtmp(*smesh);
-            Nextsim::Interpolations::CG2DG(*smesh, vtmp, momentum->GetVy());
-            return DGModelArray::dg2ma(vtmp, data);
+        } else if (name == uName || name == vName) {
+            return getVelocityDG0Data(name);
         } else {
             // Any other named field must exist
             return DGModelArray::dg2ma(advectedFields.at(name), data);
@@ -159,15 +127,13 @@ public:
     {
         static int stepNumber = 0;
 
-        //! Perform transport step
-        dgtransport->prepareAdvection(momentum->GetVx(), momentum->GetVy());
+        prepareAdvection();
 
+        //! Perform transport step
         dgtransport->step(tst.step.seconds(), cice);
         dgtransport->step(tst.step.seconds(), hice);
 
         //! Perform transport step for stress
-        stresstransport->prepareAdvection(momentum->GetVx(), momentum->GetVy());
-
         // stresstransport->step(tst.step.seconds(), momentum->GetS11());
         // stresstransport->step(tst.step.seconds(), momentum->GetS12());
         // stresstransport->step(tst.step.seconds(), momentum->GetS22());
@@ -177,8 +143,6 @@ public:
         Nextsim::LimitMin(cice, 0.0);
         Nextsim::LimitMin(hice, 0.0);
 
-//        momentum->prepareIteration(hice, cice);
-
         updateMomentum(tst);
 
         ++stepNumber;
@@ -186,12 +150,14 @@ public:
 
 protected:
     Nextsim::DGTransport<DGadvection>* dgtransport;
-//    Nextsim::DGTransport<CG2DGSTRESS(CGdegree)>* stresstransport;
     Nextsim::DGTransport<DGstress>* stresstransport;
-//    Nextsim::CGParametricMomentum<CGdegree>* momentum;
 
     DGVector<DGadvection> hice;
     DGVector<DGadvection> cice;
+
+    //! Vectors storing strain and stress components
+    DGVector<DGstress> e11, e12, e22;
+    DGVector<DGstress> s11, s12, s22;
 
     size_t NT_evp = 100;
 
@@ -219,11 +185,20 @@ protected:
      */
     void applyBoundaries();
 
+    /*!
+     * Prepares the transport objects to perform the advection step.
+     */
+    void prepareAdvection();
+
+    //! Sets the data for a velocity component
+    void setVelocityData(const std::string& velocityFieldName, const ModelArray& data);
+
+    //! Gets the DG0/finite volume data for velocity fields
+    ModelArray getVelocityDG0Data(const std::string& name);
+
 private:
 
     DynamicsInternals<DGadvection>& internals;
-//    CGVector<CGdegree> u;
-//    CGVector<CGdegree> v;
 
     Nextsim::ParametricMesh* smesh;
 
@@ -232,6 +207,39 @@ private:
     // A map from field name to the type of
     const std::unordered_map<std::string, ModelArray::Type> fieldType;
 
+    // Fields that are specially treated on set and get
+    inline static const std::set<std::string> velocityFields = { uName, vName, uWindName, vWindName, uOceanName, vOceanName };
+
+    // Initialises the common DG parts that do not depend on the velocity numerics implementation
+    void initialiseDG(const ModelArray& coords, bool isSpherical, const ModelArray& mask)
+    {
+        if (isSpherical)
+            throw std::runtime_error("DG dynamics do not yet handle spherical coordinates.");
+            // TODO handle spherical coordinates
+
+        //! Define the spatial mesh
+        smesh = new ParametricMesh(Nextsim::CARTESIAN);
+
+        smesh->coordinatesFromModelArray(coords);
+        smesh->landmaskFromModelArray(mask);
+        smesh->dirichletFromMask();
+        // TODO: handle periodic and open edges
+        for (ParametricMesh::Edge edge : ParametricMesh::edges) {
+            smesh->dirichletFromEdge(edge);
+        }
+
+        //! Initialize transport
+        dgtransport = new Nextsim::DGTransport<DGadvection>(*smesh);
+        dgtransport->settimesteppingscheme("rk2");
+
+        //! Initialize stress transport
+        stresstransport = new Nextsim::DGTransport<DGstress>(*smesh);
+        stresstransport->settimesteppingscheme("rk2");
+
+        // resize DG vectors
+        hice.resize_by_mesh(*smesh);
+        cice.resize_by_mesh(*smesh);
+    }
 };
 
 }
