ff49fe2c599e65613e82220d0d61fbaed107ed6e
Einar Örn Ólason <einar.olason@nersc.no>
2024-06-05 05:53:09 +0200

    Don't use the missing_value attribute
    For some reason, the missing_value attribute in restart files is now
    causing problems. This commit removes it from the initMaker class, so
    now the idealised tests work again.

--- run/make_init_base.py
+++ run/make_init_base.py
@@ -183,6 +183,7 @@ class initMaker:
         sss[:, :] = self.sss
 
         # mask data
+        """ TODO: Figure out why this masking doesn't work
         mdi = -3.282346e38  # Minus float max
         cice[:, :] = cice[:, :] * mask[:, :] + antimask * mdi
         cice.missing_value = mdi
@@ -202,5 +203,6 @@ class initMaker:
         sss.missing_value = mdi
         sst[:, :] = sst[:, :] * mask[:, :] + antimask * mdi
         sst.missing_value = mdi
+        """
 
         root.close()
