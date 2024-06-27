cbd66b26ed3eb2ba87005ea5286a8d04cb4df836
Joe Wallwork <jw2423@cam.ac.uk>
2024-06-27 17:32:38 +0100

    Update file dates

--- core/test/XiosDomain_test.cpp
+++ core/test/XiosDomain_test.cpp
@@ -1,7 +1,7 @@
 /*!
  * @file    XiosDomain_test.cpp
  * @author  Joe Wallwork <jw2423@cam.ac.uk
- * @date    21 June 2024
+ * @date    27 June 2024
  * @brief   Tests for XIOS domains
  * @details
  * This test is designed to test domain functionality of the C++ interface
