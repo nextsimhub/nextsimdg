bd73c30294887d04fd61a6cc1a9e209ffacaec25
Thomas Richter <thomas.richter@ovgu.de>
2024-07-10 11:16:32 +0200

    Fixed bug in dgLimit.hpp
    This changed reference values for the AdvectionPeriodicBC test case.

--- dynamics/test/AdvectionPeriodicBC_test.cpp
+++ dynamics/test/AdvectionPeriodicBC_test.cpp
@@ -49,7 +49,7 @@ const size_t NT0 = 50;
 size_t NT = NT0;
 }
 
-bool WRITE_VTK = false; //!< set to true for vtk output
+bool WRITE_VTK = true; //!< set to true for vtk output
 
 double TOL = 1.e-7; //!< tolerance for checking test results
 
@@ -168,8 +168,8 @@ public:
 
         // initial density
         Nextsim::Interpolations::Function2DG(smesh, phi, Packman());
-        Nextsim::LimitMax(phi, 1.0);
-        Nextsim::LimitMin(phi, 0.0);
+	Nextsim::LimitMax(phi, 1.0);
+	//	Nextsim::LimitMin(phi, 0.0);
 
         double initialmass = Nextsim::Tools::MeanValue(smesh, phi);
         // velocity field
@@ -295,12 +295,11 @@ TEST_CASE("Advection Periodic Boundary Conditions")
     std::cout << std::setprecision(4) << std::scientific;
 
     std::array<std::array<double, 6>, 3> exact = // Exact values taken 26/06/2024
-      { std::array<double,6>({1.0338503986019776e+00,1.1451366598186576e+00,1.0681593193338463e+00,9.5252231195653514e-01,8.1458581892610948e-01,6.8950068528265862e-01}),
-	std::array<double,6>({1.0618882222552408e+00,7.7511490432906593e-01,5.2182522412941823e-01,3.5902410728004258e-01,2.6109655891908934e-01,1.9431647351962186e-01}),
-	std::array<double,6>({6.1221633288436406e-01,4.1515677607536206e-01,3.0256496304997554e-01,2.2095605759224748e-01,1.6356635995171517e-01,1.2110580882455481e-01})};
-    
+      {std::array<double,6>({1.0338503986019776e+00,1.1451366598186583e+00,1.0681593193338459e+00,9.5252231195653603e-01,8.1458581892610615e-01,6.8950068528265651e-01}),
+       std::array<double,6>({1.0949680727313791e+00,8.3851748134278226e-01,5.8501741891364523e-01,4.0006092999256893e-01,2.8493698219799068e-01,2.0801424342840474e-01}),
+       std::array<double,6>({7.1704413076190610e-01,4.6135258391583606e-01,3.2770311091970727e-01,2.3424633076579465e-01,1.7038068017215552e-01,1.2486932724366147e-01})};
     REQUIRE(run<1>(exact) == true);
     REQUIRE(run<3>(exact) == true);
     REQUIRE(run<6>(exact) == true);
-
+     
 }
