8c04be85f981b5c63f407d3715122bdf52cd29d4
Joe Wallwork <jw2423@cam.ac.uk>
2024-08-02 15:47:49 +0100

    Use domain in XiosWrite_test

--- core/test/XiosWrite_test.cpp
+++ core/test/XiosWrite_test.cpp
@@ -1,7 +1,7 @@
 /*!
  * @file    XiosWrite_test.cpp
  * @author  Joe Wallwork <jw2423@cam.ac.uk>
- * @date    5 August 2024
+ * @date    21 August 2024
  * @brief   Tests for XIOS write method
  * @details
  * This test is designed to test the write method of the C++ interface
@@ -52,37 +52,36 @@ MPI_TEST_CASE("TestXiosWrite", 2)
     xios_handler.setCalendarStart(TimePoint("2023-03-17T17:11:00Z"));
     xios_handler.setCalendarTimestep(Duration("P0-0T01:30:00"));
 
+    // Domain setup
+    xios_handler.createDomain("xy_domain");
+    xios_handler.setDomainType("xy_domain", "rectilinear");
+    const size_t nx_glo { 4 };
+    xios_handler.setDomainGlobalXSize("xy_domain", nx_glo);
+    const size_t ny_glo { 2 };
+    xios_handler.setDomainGlobalYSize("xy_domain", ny_glo);
+    const size_t nx { nx_glo / size };
+    xios_handler.setDomainLocalXSize("xy_domain", nx);
+    const size_t ny { ny_glo };
+    xios_handler.setDomainLocalYSize("xy_domain", ny);
+    xios_handler.setDomainLocalXStart("xy_domain", nx * rank);
+    xios_handler.setDomainLocalYStart("xy_domain", 0);
+    std::vector<double> vx { -1.0 + rank, -0.5 + rank };
+    xios_handler.setDomainLocalXValues("xy_domain", vx);
+    std::vector<double> vy { -1.0, 1.0 };
+    xios_handler.setDomainLocalYValues("xy_domain", vy);
+
     // Axis setup
-    const int n1 = 2;
-    const int n2 = 3;
-    const int n3 = 4;
-    const int n4 = 5;
-    xios_handler.createAxis("axis_A");
-    xios_handler.setAxisSize("axis_A", n1);
-    xios_handler.setAxisValues("axis_A", { 0, 1 });
-    xios_handler.createAxis("axis_B");
-    xios_handler.setAxisSize("axis_B", n2);
-    xios_handler.setAxisValues("axis_B", { 0, 1, 2 });
-    xios_handler.createAxis("axis_C");
-    xios_handler.setAxisSize("axis_C", n3);
-    xios_handler.setAxisValues("axis_C", { 0, 1, 2, 3 });
-    xios_handler.createAxis("axis_D");
-    xios_handler.setAxisSize("axis_D", n4);
-    xios_handler.setAxisValues("axis_D", { 0, 1, 2, 3, 4 });
+    const int nz = 2;
+    xios_handler.createAxis("z_axis");
+    xios_handler.setAxisSize("z_axis", nz);
+    xios_handler.setAxisValues("z_axis", { 0.0, 1.0 });
 
     // Grid setup
     xios_handler.createGrid("grid_2D");
-    xios_handler.gridAddAxis("grid_2D", "axis_A");
-    xios_handler.gridAddAxis("grid_2D", "axis_B");
+    xios_handler.gridAddDomain("grid_2D", "xy_domain");
     xios_handler.createGrid("grid_3D");
-    xios_handler.gridAddAxis("grid_3D", "axis_A");
-    xios_handler.gridAddAxis("grid_3D", "axis_B");
-    xios_handler.gridAddAxis("grid_3D", "axis_C");
-    xios_handler.createGrid("grid_4D");
-    xios_handler.gridAddAxis("grid_4D", "axis_A");
-    xios_handler.gridAddAxis("grid_4D", "axis_B");
-    xios_handler.gridAddAxis("grid_4D", "axis_C");
-    xios_handler.gridAddAxis("grid_4D", "axis_D");
+    xios_handler.gridAddDomain("grid_3D", "xy_domain");
+    xios_handler.gridAddAxis("grid_3D", "z_axis");
 
     // Field setup
     xios_handler.createField("field_2D");
@@ -93,10 +92,6 @@ MPI_TEST_CASE("TestXiosWrite", 2)
     xios_handler.setFieldOperation("field_3D", "instant");
     xios_handler.setFieldGridRef("field_3D", "grid_3D");
     xios_handler.setFieldReadAccess("field_3D", false);
-    xios_handler.createField("field_4D");
-    xios_handler.setFieldOperation("field_4D", "instant");
-    xios_handler.setFieldGridRef("field_4D", "grid_4D");
-    xios_handler.setFieldReadAccess("field_4D", false);
 
     // File setup
     xios_handler.createFile("xios_test_output");
@@ -106,33 +101,31 @@ MPI_TEST_CASE("TestXiosWrite", 2)
     xios_handler.setFileMode("xios_test_output", "write");
     xios_handler.fileAddField("xios_test_output", "field_2D");
     xios_handler.fileAddField("xios_test_output", "field_3D");
-    xios_handler.fileAddField("xios_test_output", "field_4D");
 
     xios_handler.close_context_definition();
 
     // --- Tests for writing to file
     Module::setImplementation<IStructure>("Nextsim::ParametricGrid");
-    ModelArray::setDimension(ModelArray::Dimension::X, n1, n1, 0);
-    ModelArray::setDimension(ModelArray::Dimension::Y, n2, n2, 0);
-    ModelArray::setDimension(ModelArray::Dimension::Z, n3, n3, 0);
+    ModelArray::setDimension(ModelArray::Dimension::X, nx, nx, 0);
+    ModelArray::setDimension(ModelArray::Dimension::Y, ny, ny, 0);
+    ModelArray::setDimension(ModelArray::Dimension::Z, nz, nz, 0);
     // Create some fake data to test writing methods
     HField field_2D(ModelArray::Type::H);
     field_2D.resize();
-    for (size_t j = 0; j < n2; ++j) {
-        for (size_t i = 0; i < n1; ++i) {
-            field_2D(i, j) = 1.0 * (i + n1 * j);
+    for (size_t j = 0; j < ny; ++j) {
+        for (size_t i = 0; i < nx; ++i) {
+            field_2D(i, j) = 1.0 * (i + nx * j);
         }
     }
     HField field_3D(ModelArray::Type::Z);
     field_3D.resize();
-    for (size_t k = 0; k < n3; ++k) {
-        for (size_t j = 0; j < n2; ++j) {
-            for (size_t i = 0; i < n1; ++i) {
-                field_3D(i, j, k) = 1.0 * (i + n1 * (j + n2 * k));
+    for (size_t k = 0; k < nz; ++k) {
+        for (size_t j = 0; j < ny; ++j) {
+            for (size_t i = 0; i < nx; ++i) {
+                field_3D(i, j, k) = 1.0 * (i + nx * (j + ny * k));
             }
         }
     }
-    // TODO: Implement 4D case
     // Verify calendar step is starting from zero
     REQUIRE(xios_handler.getCalendarStep() == 0);
     // Check a file with the expected name doesn't exist yet
@@ -144,7 +137,6 @@ MPI_TEST_CASE("TestXiosWrite", 2)
         // Send data to XIOS to be written to disk
         xios_handler.write("field_2D", field_2D);
         xios_handler.write("field_3D", field_3D);
-        // TODO: Implement 4D case
         // Verify timestep
         REQUIRE(xios_handler.getCalendarStep() == ts);
     }
@@ -156,5 +148,7 @@ MPI_TEST_CASE("TestXiosWrite", 2)
         std::filesystem::remove("xios_test_output_20230317201100-20230317231059.nc");
     }
     xios_handler.context_finalize();
+
+    // TODO: Consider adding a 4D test case
 }
 }
