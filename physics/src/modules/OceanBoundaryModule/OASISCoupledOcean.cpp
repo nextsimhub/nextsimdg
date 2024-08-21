e8d363381a9d34b52fbf7d141e3da60563bfe5b0
Einar Örn Ólason <einar.olason@nersc.no>
2024-08-21 16:09:50 +0200

    Options for the couplingId code
    There are now a lot of options to set, so that it's flexible what you
    put in the namcouple file.

--- physics/src/modules/OceanBoundaryModule/OASISCoupledOcean.cpp
+++ physics/src/modules/OceanBoundaryModule/OASISCoupledOcean.cpp
@@ -54,28 +54,29 @@ void OASISCoupledOcean::updateBefore(const TimestepTime& tst)
     const int dimension0 = ModelArray::dimensions(ModelArray::Type::H)[0];
     const int dimension1 = ModelArray::dimensions(ModelArray::Type::H)[1];
 
-    OASIS_CHECK_ERR(oasis_c_get(couplingId.at(SST), OASISTime, dimension0, dimension1, bundleSize,
-        OASIS_DOUBLE, OASIS_COL_MAJOR, &sst[0], &kinfo));
+    OASIS_CHECK_ERR(oasis_c_get(couplingId.at(SSTKey), OASISTime, dimension0, dimension1,
+        bundleSize, OASIS_DOUBLE, OASIS_COL_MAJOR, &sst[0], &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_get(couplingId.at(SSS), OASISTime, dimension0, dimension1, bundleSize,
-        OASIS_DOUBLE, OASIS_COL_MAJOR, &sss[0], &kinfo));
+    OASIS_CHECK_ERR(oasis_c_get(couplingId.at(SSSKey), OASISTime, dimension0, dimension1,
+        bundleSize, OASIS_DOUBLE, OASIS_COL_MAJOR, &sss[0], &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_get(couplingId.at(UOCEAN), OASISTime, dimension0, dimension1,
+    OASIS_CHECK_ERR(oasis_c_get(couplingId.at(UOceanKey), OASISTime, dimension0, dimension1,
         bundleSize, OASIS_DOUBLE, OASIS_COL_MAJOR, &u[0], &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_get(couplingId.at(VOCEAN), OASISTime, dimension0, dimension1,
+    OASIS_CHECK_ERR(oasis_c_get(couplingId.at(VOceanKey), OASISTime, dimension0, dimension1,
         bundleSize, OASIS_DOUBLE, OASIS_COL_MAJOR, &v[0], &kinfo));
 
     // TODO: Implement ssh reading and passing to dynamics!
-    //    OASIS_CHECK_ERR(oasis_c_get(cplIdIn[couplingIdIn::SSH], OASISTime, dimension0, dimension1,
+    //    OASIS_CHECK_ERR(oasis_c_get(cplIdIn[couplingIdIn::SSHKey], OASISTime, dimension0,
+    //    dimension1,
     //                                bundleSize, OASIS_DOUBLE, OASIS_COL_MAJOR, &ssh[0], &kinfo));
 
-    // TODO: Handle mld being an optional field with a command line option for the value
-    if (couplingId.find(SSH) != couplingId.end())
-        OASIS_CHECK_ERR(oasis_c_get(couplingId.at(VOCEAN), OASISTime, dimension0, dimension1,
+    if (couplingId.find(SSHKey) != couplingId.end()) {
+        OASIS_CHECK_ERR(oasis_c_get(couplingId.at(VOceanKey), OASISTime, dimension0, dimension1,
             bundleSize, OASIS_DOUBLE, OASIS_COL_MAJOR, &mld[0], &kinfo));
-    else
-        mld = 10.;
+    } else {
+        mld = firstLayerDepth;
+    }
 
     cpml = Water::cp * Water::rho * mld;
 
@@ -94,37 +95,36 @@ void OASISCoupledOcean::updateBefore(const TimestepTime& tst)
 void OASISCoupledOcean::updateAfter(const TimestepTime& tst)
 {
 #ifdef USE_OASIS
-    // TODO: Add OASIS send-calls here
     int kinfo;
     const int dimension0 = ModelArray::dimensions(ModelArray::Type::H)[0];
     const int dimension1 = ModelArray::dimensions(ModelArray::Type::H)[1];
 
-    // We still need the the outputs
+    // TODO We still need the the actual data
     HField dummy;
     dummy.resize();
     dummy.setData(0.);
-    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(TAUX), OASISTime, dimension0, dimension1, 1,
+    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(TauXKey), OASISTime, dimension0, dimension1, 1,
         OASIS_DOUBLE, OASIS_COL_MAJOR, &dummy[0], OASIS_No_Restart, &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(TAUY), OASISTime, dimension0, dimension1, 1,
+    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(TauYKey), OASISTime, dimension0, dimension1, 1,
         OASIS_DOUBLE, OASIS_COL_MAJOR, &dummy[0], OASIS_No_Restart, &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(EMP), OASISTime, dimension0, dimension1, 1,
+    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(EMPKey), OASISTime, dimension0, dimension1, 1,
         OASIS_DOUBLE, OASIS_COL_MAJOR, &dummy[0], OASIS_No_Restart, &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(QSW), OASISTime, dimension0, dimension1, 1,
+    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(QSWKey), OASISTime, dimension0, dimension1, 1,
         OASIS_DOUBLE, OASIS_COL_MAJOR, &dummy[0], OASIS_No_Restart, &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(QNOSUN), OASISTime, dimension0, dimension1, 1,
+    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(QNoSunKey), OASISTime, dimension0, dimension1, 1,
         OASIS_DOUBLE, OASIS_COL_MAJOR, &dummy[0], OASIS_No_Restart, &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(SFLX), OASISTime, dimension0, dimension1, 1,
+    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(SFluxKey), OASISTime, dimension0, dimension1, 1,
         OASIS_DOUBLE, OASIS_COL_MAJOR, &dummy[0], OASIS_No_Restart, &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(TAUMOD), OASISTime, dimension0, dimension1, 1,
+    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(TauModKey), OASISTime, dimension0, dimension1, 1,
         OASIS_DOUBLE, OASIS_COL_MAJOR, &dummy[0], OASIS_No_Restart, &kinfo));
 
-    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(CICE), OASISTime, dimension0, dimension1, 1,
+    OASIS_CHECK_ERR(oasis_c_put(couplingId.at(CIceKey), OASISTime, dimension0, dimension1, 1,
         OASIS_DOUBLE, OASIS_COL_MAJOR, &dummy[0], OASIS_No_Restart, &kinfo));
 
     // Increment the "OASIS" time by the number of seconds in the time step
@@ -133,6 +133,75 @@ void OASISCoupledOcean::updateAfter(const TimestepTime& tst)
     std::string message = __func__ + std::string(": OASIS support not compiled in.\n");
     throw std::runtime_error(message);
 #endif
+}
+
+void OASISCoupledOcean::configure()
+{
+    firstLayerDepth = Configured::getConfiguration(layerDepthConfigKey, FIRST_LAYER_DEPTH);
+    if (Configured::getConfiguration(exchangeFirstLayerConfigKey, EXCHANGE_FIRST_LAYER)) {
+        cplStringsIn.push_back(MLDKey);
+    }
+
+    SSTKey = Configured::getConfiguration(SSTConfigKey, SSTKeyDefault);
+    SSSKey = Configured::getConfiguration(SSSConfigKey, SSSKeyDefault);
+    UOceanKey = Configured::getConfiguration(UOceanConfigKey, UOceanKeyDefault);
+    VOceanKey = Configured::getConfiguration(VOceanConfigKey, VOceanKeyDefault);
+    SSHKey = Configured::getConfiguration(SSHConfigKey, SSHKeyDefault);
+    MLDKey = Configured::getConfiguration(MLDConfigKey, MLDKeyDefault);
+    TauXKey = Configured::getConfiguration(TauXConfigKey, TauXKeyDefault);
+    TauYKey = Configured::getConfiguration(TauYConfigKey, TauYKeyDefault);
+    TauModKey = Configured::getConfiguration(TauModConfigKey, TauModKeyDefault);
+    EMPKey = Configured::getConfiguration(EMPConfigKey, EMPKeyDefault);
+    QNoSunKey = Configured::getConfiguration(QNoSunConfigKey, QNoSunKeyDefault);
+    QSWKey = Configured::getConfiguration(QSWConfigKey, QSWKeyDefault);
+    SFluxKey = Configured::getConfiguration(SFluxConfigKey, SFluxKeyDefault);
+    CIceKey = Configured::getConfiguration(CIceConfigKey, CIceKeyDefault);
+}
+
+OASISCoupledOcean::HelpMap& OASISCoupledOcean::getHelpText(HelpMap& map, bool getAll)
+{
+    map[moduleName] = {
+        { layerDepthConfigKey, ConfigType::NUMERIC, { "0", "∞" }, std::to_string(FIRST_LAYER_DEPTH),
+            "m", "Depth of the first ocean model layer (if this is fixed)." },
+        { exchangeFirstLayerConfigKey, ConfigType::BOOLEAN, { "true", "false" },
+            std::to_string(EXCHANGE_FIRST_LAYER), "",
+            "Use the thickness of the first ocean layer provided through the coupler" },
+        { SSTConfigKey, ConfigType::STRING, {}, SSTKeyDefault, "",
+            "The field name for sea surface temperature used in namcouple" },
+        { SSSConfigKey, ConfigType::STRING, {}, SSSKeyDefault, "",
+            "The field name for sea surface salinity used in namcouple" },
+        { UOceanConfigKey, ConfigType::STRING, {}, UOceanKeyDefault, "",
+            "The field name for ocean u-velocity used in namcouple" },
+        { VOceanConfigKey, ConfigType::STRING, {}, VOceanKeyDefault, "",
+            "The field name for ocean v-velocity used in namcouple" },
+        { SSHConfigKey, ConfigType::STRING, {}, SSHKeyDefault, "",
+            "The field name for sea surface height used in namcouple" },
+        { MLDConfigKey, ConfigType::STRING, {}, MLDKeyDefault, "",
+            "The field name for the thickness of the first ocean layer in namcouple (if that's defined)." },
+        { TauXConfigKey, ConfigType::STRING, {}, TauXKeyDefault, "",
+            "The field name for the x-component of ice-ocean stress in namcouple." },
+        { TauYConfigKey, ConfigType::STRING, {}, TauYKeyDefault, "",
+            "The field name for the y-component of ice-ocean stress in namcouple." },
+        { TauModConfigKey, ConfigType::STRING, {}, TauModKeyDefault, "",
+            "The field name for the modulus of ice-ocean stress in namcouple." },
+        { EMPConfigKey, ConfigType::STRING, {}, EMPKeyDefault, "",
+            "The field name for freshwater flux used in namcouple" },
+        { QNoSunConfigKey, ConfigType::STRING, {}, QNoSunKeyDefault, "",
+            "The field name for non-solar flux used in namcouple" },
+        { QSWConfigKey, ConfigType::STRING, {}, QSWKeyDefault, "",
+            "The field name for showrt-wave flux used in namcouple" },
+        { QSWConfigKey, ConfigType::STRING, {}, QSWKeyDefault, "",
+            "The field name for showrt-wave flux used in namcouple" },
+        { SFluxConfigKey, ConfigType::STRING, {}, SFluxKeyDefault, "",
+            "The field name for salt flux used in namcouple" },
+        { CIceConfigKey, ConfigType::STRING, {}, CIceKeyDefault, "",
+            "The field name for sea-ice concentration used in namcouple" },
+    };
+    return map;
+}
+OASISCoupledOcean::HelpMap& OASISCoupledOcean::getHelpRecursive(HelpMap& map, bool getAll)
+{
+    return getHelpText(map, getAll);
 }
 
 } /* namespace Nextsim */
--- physics/src/modules/OceanBoundaryModule/include/OASISCoupledOcean.hpp
+++ physics/src/modules/OceanBoundaryModule/include/OASISCoupledOcean.hpp
@@ -17,8 +17,48 @@
 
 namespace Nextsim {
 
+static const std::string moduleName = "OASISCoupledOcean";
+
+static const std::string layerDepthConfigKey = moduleName + ".layer_depth";
+static const std::string exchangeFirstLayerConfigKey = moduleName + ".exchange_first_layer";
+
+static const double FIRST_LAYER_DEPTH = 1.; // There really is no sensible default here(!)
+static const bool EXCHANGE_FIRST_LAYER = false;
+
+static const std::string SSTKeyDefault = "I_SST";
+static const std::string SSSKeyDefault = "I_SSS";
+static const std::string UOceanKeyDefault = "I_Uocn";
+static const std::string VOceanKeyDefault = "I_Vocn";
+static const std::string SSHKeyDefault = "I_SSH";
+static const std::string MLDKeyDefault = "I_MLD"; // This one is optional
+static const std::string TauXKeyDefault = "I_taux";
+static const std::string TauYKeyDefault = "I_tauy";
+static const std::string TauModKeyDefault = "I_taumod";
+static const std::string EMPKeyDefault = "I_fwflux";
+static const std::string QNoSunKeyDefault = "I_rsnos";
+static const std::string QSWKeyDefault = "I_rsso";
+static const std::string SFluxKeyDefault = "I_sfi";
+static const std::string CIceKeyDefault = "I_conc";
+
+static const std::string SSTConfigKey = ".sea_surface_temperature";
+static const std::string SSSConfigKey = ".sea_surface_salinity";
+static const std::string UOceanConfigKey = ".ocean_u_velocity";
+static const std::string VOceanConfigKey = ".ocean_v_velocity";
+static const std::string SSHConfigKey = ".sea_surface_height";
+static const std::string MLDConfigKey = ".first_ocean_layer_depth"; // This one is optional
+static const std::string TauXConfigKey = ".ice_ocean_stress_x";
+static const std::string TauYConfigKey = ".ice_ocean_stress_y";
+static const std::string TauModConfigKey = ".ice_ocean_stress_modulo";
+static const std::string EMPConfigKey = ".fresh_water_flux";
+static const std::string QNoSunConfigKey = ".non_solar_heatflux";
+static const std::string QSWConfigKey = ".short_wave_flux";
+static const std::string SFluxConfigKey = ".salt_flux";
+static const std::string CIceConfigKey = ".sea_ice_concentration";
+
 //* Ocean boundary data values that are hardcoded.
-class OASISCoupledOcean : public IOceanBoundary, OASISCoupled {
+class OASISCoupledOcean : public IOceanBoundary,
+                          public OASISCoupled,
+                          public Configured<OASISCoupledOcean> {
 public:
     OASISCoupledOcean()
         : IOceanBoundary()
@@ -26,13 +66,19 @@ public:
     }
     ~OASISCoupledOcean() { OASISCoupled::~OASISCoupled(); }
 
-    std::string getName() const override { return "OASISCoupledOcean"; }
+    std::string getName() const override { return moduleName; }
     void updateBefore(const TimestepTime& tst) override;
     void updateAfter(const TimestepTime& tst) override;
     void setMetadata(const ModelMetadata& metadata) override;
 
+    void configure() override;
+
+    static HelpMap& getHelpText(HelpMap& map, bool getAll);
+    static HelpMap& getHelpRecursive(HelpMap& map, bool getAll);
+
 private:
-    int bundleSize = 1;
+    int bundleSize = 1; // Always "unbundled", as per the OASIS manual
+    double firstLayerDepth = FIRST_LAYER_DEPTH;
 
     SlabOcean slabOcean;
 
@@ -43,26 +89,13 @@ private:
 
     // A map to relate the strings in the namcouple file to the numbers def_var spits out
     std::map<std::string, int> couplingId;
-    std::string SST = "I_SST";
-    std::string SSS = "I_SSS";
-    std::string UOCEAN = "I_Uocn";
-    std::string VOCEAN = "I_Vocn";
-    std::string SSH = "I_SSH";
-    std::string MLD = "I_MLD"; // This one is optional
-    std::string TAUX = "I_taux";
-    std::string TAUY = "I_tauy";
-    std::string TAUMOD = "I_taumod";
-    std::string EMP = "I_fwflux";
-    std::string QNOSUN = "I_rsnos";
-    std::string QSW = "I_rsso";
-    std::string SFLX = "I_sfi";
-    std::string CICE = "I_conc";
+    std::string SSTKey, SSSKey, UOceanKey, VOceanKey, SSHKey, MLDKey, TauXKey, TauYKey, TauModKey,
+        EMPKey, QNoSunKey, QSWKey, SFluxKey, CIceKey;
 
-    const std::vector<std::string> cplStringsIn
-        = { SST, SSS, UOCEAN, VOCEAN, SSH  }; // MLD can be added to this one
-    const std::vector<std::string> cplStringsOut
-        = { TAUX, TAUY, TAUMOD, EMP, QNOSUN, QSW, SFLX, CICE };
-
+    std::vector<std::string> cplStringsIn
+        = { SSTKey, SSSKey, UOceanKey, VOceanKey, SSHKey }; // MLDKey can be added to this one
+    std::vector<std::string> cplStringsOut
+        = { TauXKey, TauYKey, TauModKey, EMPKey, QNoSunKey, QSWKey, SFluxKey, CIceKey };
 };
 
 } /* namespace Nextsim */
