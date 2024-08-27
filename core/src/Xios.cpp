98824282f2864d802619cd593a78207d21d5052c
Joe Wallwork <jw2423@cam.ac.uk>
2024-08-27 09:37:46 +0100

    Introduce ParaGridIO_Xios and move read and write methods onto it

--- core/src/Xios.cpp
+++ core/src/Xios.cpp
@@ -1561,54 +1561,6 @@ void Xios::fileAddField(const std::string fileId, const std::string fieldId)
     xios::CField* field = getField(fieldId);
     cxios_xml_tree_add_fieldtofile(getFile(fileId), &field, fieldId.c_str(), fieldId.length());
 }
-
-/*!
- * Send a field to the XIOS server to be written to file
- *
- * @param field name
- * @param reference to the ModelArray containing the data to be written
- */
-void Xios::write(const std::string fieldId, ModelArray& modelarray)
-{
-    auto ndim = modelarray.nDimensions();
-    auto dims = modelarray.dimensions();
-    if (ndim == 2) {
-        cxios_write_data_k82(
-            fieldId.c_str(), fieldId.length(), modelarray.getData(), dims[0], dims[1], -1);
-    } else if (ndim == 3) {
-        cxios_write_data_k83(
-            fieldId.c_str(), fieldId.length(), modelarray.getData(), dims[0], dims[1], dims[2], -1);
-    } else if (ndim == 4) {
-        cxios_write_data_k84(fieldId.c_str(), fieldId.length(), modelarray.getData(), dims[0],
-            dims[1], dims[2], dims[3], -1);
-    } else {
-        throw std::invalid_argument("Only ModelArrays of dimension 2, 3, or 4 are supported");
-    }
-}
-
-/*!
- * Receive field from XIOS server that has been read from file.
- *
- * @param field name
- * @param reference to the ModelArray containing the data to be written
- */
-void Xios::read(const std::string fieldId, ModelArray& modelarray)
-{
-    auto ndim = modelarray.nDimensions();
-    auto dims = modelarray.dimensions();
-    if (ndim == 2) {
-        cxios_read_data_k82(
-            fieldId.c_str(), fieldId.length(), modelarray.getData(), dims[0], dims[1]);
-    } else if (ndim == 3) {
-        cxios_read_data_k83(
-            fieldId.c_str(), fieldId.length(), modelarray.getData(), dims[0], dims[1], dims[2]);
-    } else if (ndim == 4) {
-        cxios_read_data_k84(fieldId.c_str(), fieldId.length(), modelarray.getData(), dims[0],
-            dims[1], dims[2], dims[3]);
-    } else {
-        throw std::invalid_argument("Only ModelArrays of dimension 2, 3, or 4 are supported");
-    }
-}
 }
 
 #endif
---
+++ core/src/include/ParaGridIO_Xios.hpp
@@ -0,0 +1,77 @@
+/*!
+ * @file   ParaGridIO_Xios.hpp
+ *
+ * @date   27 Aug 2024
+ * @author Joe Wallwork <jw2423@cam.ac.uk>
+ */
+#ifndef PARAGRIDIO_XIOS_HPP
+#define PARAGRIDIO_XIOS_HPP
+
+#include "ModelArray.hpp"
+#include "StructureModule/include/ParametricGrid.hpp"
+
+namespace Nextsim {
+
+class ParaGridIO_Xios : public ParametricGrid::IParaGridIO {
+
+public:
+    ParaGridIO_Xios(ParametricGrid& grid)
+        : IParaGridIO(grid)
+    {
+        // TODO: Implement this method
+    }
+    virtual ~ParaGridIO_Xios();
+
+    // TODO: Align the API with ParaGridIO
+    void read(const std::string fileId, ModelArray& modelarray);
+    void write(const std::string fileId, ModelArray& modelarray);
+
+    // TODO: Align the API with ParaGridIO
+    /*!
+     * Retrieves the ModelState from a restart file of the parametric_grid type.
+     * @param filePath The file path containing the file to be read.
+     */
+#ifdef USE_MPI
+    ModelState getModelState(const std::string& filePath, ModelMetadata& metadata) override;
+#else
+    ModelState getModelState(const std::string& filePath) override;
+#endif
+
+    // TODO: Align the API with ParaGridIO
+    /*!
+     * @brief Writes the ModelState to a given file location from the provided
+     * model data and metadata.
+     *
+     * @params state The model state and configuration object.
+     * @params metadata The model metadata (principally the initial file
+     * creation model time).
+     * @params filePath The path for the restart file.
+     */
+    void dumpModelState(
+        const ModelState& state, const ModelMetadata& meta, const std::string& filePath) override;
+
+    // TODO: Align the API with ParaGridIO
+    /*!
+     * @brief Reads forcings from a ParameticGrid flavoured file.
+     *
+     * @param forcings The names of the forcings required.
+     * @param time The time for which to get the forcings.
+     * @param filePath Path to the file to read.
+     */
+    ModelState readForcingTime(const std::set<std::string>& forcings, const TimePoint& time,
+        const std::string& filePath) override;
+
+    // TODO: Align the API with ParaGridIO
+    /*!
+     * @brief Writes diagnostic data to a file.
+     *
+     * @param state The state to write to the file.
+     * @param time The time of the passed data.
+     * @param filePath Path of the file to write to.
+     */
+    void writeDiagnosticTime(
+        const ModelState& state, const ModelMetadata& meta, const std::string& filePath) override;
+};
+} /* namespace Nextsim */
+
+#endif /* PARAGRIDIO_XIOS_HPP */
