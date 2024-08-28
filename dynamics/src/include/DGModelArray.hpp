700be0b5f84d805c26bcbd4da1748a379c8a8b2e
Tim Spain <timothy.spain@nersc.no>
2024-08-28 08:34:08 +0200

    Remove the higher dimensional DGModelArray features.

--- dynamics/src/include/DGModelArray.hpp
+++ dynamics/src/include/DGModelArray.hpp
@@ -19,6 +19,7 @@ class DGModelArray {
 public:
     template <int N> static DGVector<N>& ma2dg(const ModelArray& ma, DGVector<N>& dg)
     {
+        dg.zero();
         if (N == ma.components(0).size()) {
             //            assert(N == ma.components(0).size());
             dg = ma.data().matrix();
@@ -28,48 +29,6 @@ public:
             dg.col(0) = ma.data().matrix();
         }
         return dg;
-    }
-
-    /*!
-     * Copies the contents of a 2D slice from a ModelArray to a DGVector, where the Model Array
-     * has more than 2 dimensions.
-     *
-     * @param ma The source ModelArray, with more than 2 dimensions
-     * @param dg The target DGVector
-     * @param kField The selected z-level of ModelArray ma.
-     */
-    template <int DG> static DGVector<DG>& ma2dg(const ModelArray& ma, DGVector<DG>& dg, size_t kField)
-    {
-        // Use the 2D optimized version if possible
-        if (ma.nDimensions() == 2) return ma2dg(ma, dg);
-
-        // Extract the desired level in the first dimension
-        // The size of a 2D domain is taken to be the product of the first two dimensions of ma
-        ModelArray::MultiDim maDims = ma.dimensions();
-        size_t size2D = maDims[0] * maDims[1];
-        // With DG components
-        if (DG == ma.components(0).size()) {
-            dg = ma.data().matrix()(Eigen::seqN(kField * size2D, size2D), Eigen::all);
-        } else {
-            dg.col(0) = ma.data().matrix()(Eigen::seqN(kField * size2D, size2D), 0);
-        }
-        return dg;
-    }
-
-    /*!
-     * Copies the contents of 2D slice from a ModelArray to a DGVector, where the Model Array
-     * has more than 2 dimensions.
-     *
-     * @param ma The source ModelArray, with more than 2 dimensions
-     * @param dg The target DGVector
-     * @param sliceIndices The selected z-level of ModelArray ma.
-     */
-    template <int DG> static DGVector<DG>& ma2dg(const ModelArray& ma, DGVector<DG>& dg, const ModelArray::MultiDim& sliceIndices)
-    {
-        // Use the 2D optimized version if possible
-        if (ma.nDimensions() == 2) return ma2dg(ma, dg);
-
-        return ma2dg(ma, dg, kFromLocation(sliceIndices, ma));
     }
 
     template <int N> static ModelArray& dg2ma(const DGVector<N>& dg, ModelArray& ma)
@@ -88,31 +47,6 @@ public:
         return ma;
     }
 
-    template <int DG> static ModelArray& dg2ma(const DGVector<DG>& dg, ModelArray& ma, size_t kIndex)
-    {
-        // Use the 2D optimized version if possible
-        if (ma.nDimensions() == 2) return dg2ma(dg, ma);
-
-        // Extract the desired level in the first dimension
-        // The size of a 2D domain is taken to be the product of the first two dimensions of ma
-        ModelArray::MultiDim maDims = ma.dimensions();
-        size_t size2D = maDims[0] * maDims[1];
-        if (DG == ma.components(0).size()) {
-            ma.setData(dg.array(), kIndex* size2D, size2D);
-        } else {
-            ma.setData(dg.array().col(0), kIndex* size2D, size2D);
-        }
-        return ma;
-    }
-
-    template <int DG> static ModelArray& dg2ma(const DGVector<DG>& dg, ModelArray& ma, const ModelArray::MultiDim& sliceIndices)
-    {
-        // Use the 2D optimized version if possible
-        if (ma.nDimensions() == 2) return dg2ma(dg, ma);
-
-        return dg2ma(dg, ma, kFromLocation(sliceIndices, ma));
-    }
-
     template <int N> static DGVector<N>& hField2dg(const HField& h, DGVector<N>& dg)
     {
         dg.col(0) = h.data();
@@ -124,25 +58,6 @@ public:
         h.setData(dg.col(0));
         return h;
     }
-private:
-    static size_t kFromLocation(const ModelArray::MultiDim& loc, const ModelArray& ma)
-    {
-        const size_t twoD = 2;
-
-        // Copy the slice index and prefix with two zeroes
-        ModelArray::MultiDim fullDims(loc);
-        for (size_t dim = 0; dim < twoD; ++dim) {
-            fullDims.insert(fullDims.begin(), 0);
-        }
-        // Get the index of the first element of the slice
-        size_t kIndex = ma.indexFromLocation(fullDims);
-        // Divide by the size of the 2D slice (first two dimensions)
-        for (size_t dim = 0; dim < twoD; ++dim) {
-            kIndex /= ma.dimensions()[dim];
-        }
-        return kIndex;
-    }
-
 };
 
 template <> inline DGVector<1>& DGModelArray::hField2dg(const HField& h, DGVector<1>& dg)
--- dynamics/test/DGModelArray_test.cpp
+++ dynamics/test/DGModelArray_test.cpp
@@ -15,8 +15,6 @@
 
 #include "include/ParametricMesh.hpp"
 
-#include <list>
-
 Nextsim::COORDINATES CoordinateSystem = Nextsim::CARTESIAN;
 
 namespace Nextsim {
@@ -102,7 +100,6 @@ TEST_CASE("DGVector from ModelArray::Type::H")
         }
     }
     DGVector<DG> dest(smesh);
-    dest.zero(); // Zero the higher components, ma2dg does not touch them.
     DGModelArray::ma2dg<DG>(source, dest);
 
     // Did it work?
@@ -281,124 +278,6 @@ TEST_CASE("Test the HField/DG0 transfer") // (It would be a silly case to get wr
     REQUIRE(source(targetPoint, 1) == retained);
     REQUIRE(source(targetPoint, 0) != retained);
 }
-
-static const double dx = 100;
-static const double dy = 100;
-size_t indexr(size_t i, size_t j, size_t k)
-{
-    return k + dy * (j + dx * i) + 1000000;
-}
-
-TEST_CASE("Test 3D arrays")
-{
-    static const int DG = 3;
-    const size_t nx = 13;
-    const size_t ny = 17;
-    const size_t nz = 3;
-
-
-    ModelArray::setDimension(ModelArray::Dimension::X, nx);
-    ModelArray::setDimension(ModelArray::Dimension::Y, ny);
-    ModelArray::setDimension(ModelArray::Dimension::Z, nz);
-    ModelArray::setNComponents(ModelArray::Type::DG, DG);
-
-    // Create the ParametricMesh object
-    ParametricMesh smesh(CoordinateSystem);
-    smesh.nx = nx;
-    smesh.ny = ny;
-    smesh.nnodes = nx * ny;
-    smesh.nelements = nx * ny;
-    smesh.vertices.resize(smesh.nelements, Eigen::NoChange);
-    for (size_t i = 0; i < nx; ++i) {
-        for (size_t j = 0; j < ny; ++j) {
-            smesh.vertices(i * ny + j, 0) = i;
-            smesh.vertices(i * ny + j, 1) = j;
-        }
-    }
-
-    ZField zin(ModelArray::Type::Z);
-    zin.resize();
-    ZField zout(ModelArray::Type::Z);
-    zout.resize();
-    zout = 0.;
-
-    for (size_t k = 0; k < nz; ++k) {
-        for (size_t j = 0; j < ny; ++j) {
-            for (size_t i = 0; i < nx; ++i) {
-                zin(i, j, k) = indexr(i, j, k);
-            }
-        }
-    }
-
-    for (size_t k = 0; k < nz; ++k) {
-        DGVector<DG> dgData;
-        dgData.resize_by_mesh(smesh);
-        dgData.zero();
-        DGModelArray::ma2dg<DG>(zin, dgData, k);
-
-        // Check the corners
-        REQUIRE(dgData(0, 0) == indexr(0, 0, k));
-        REQUIRE(dgData(nx - 1, 0) == indexr(nx - 1, 0, k));
-        REQUIRE(dgData((ny - 1) * nx, 0) == indexr(0, ny - 1, k));
-
-        // check all of dgData
-        bool isDGDataCorrect = true;
-        for (size_t idx = 0; idx < nx * ny; ++idx) {
-            auto loc = ModelArray::locationFromIndex(ModelArray::Type::H, idx);
-            if (dgData(idx, 0) != indexr(loc[0], loc[1], k)) {
-                isDGDataCorrect = false;
-                std::string locString = "(" + std::to_string(loc[0]) + "," + std::to_string(loc[1]) + ")";
-                REQUIRE_MESSAGE(dgData(idx, 0) != indexr(loc[0], loc[1], k), "At ", locString);
-            }
-        }
-        if (isDGDataCorrect) MESSAGE("k=", k, ": DGData seems correct, ma2dg(ma, dg, k) succeeded");
-
-        DGModelArray::dg2ma<DG>(dgData, zout, k);
-        // Check that all higher z-levels are still zero
-        double absSum = 0.;
-        std::list<size_t> badElements;
-        for (size_t l = k + 1; l < nz; ++l) {
-            for (size_t i = 0; i < nx * ny; ++i) {
-                size_t idx = i + zout.indexFromLocation({ 0UL, 0UL, l });
-                double absValue = std::fabs(zout[idx]);
-                if (absValue > 0) badElements.push_back(idx);
-                absSum += absValue;
-            }
-            if (badElements.size() > 0) {
-                for (auto badIdx : badElements) {
-                    auto badLoc = zout.locationFromIndex(badIdx);
-                    std::cout << "(";
-                    for (auto badLocDim : badLoc) {
-                        std::cout << badLocDim << ",";
-                    }
-                    std::cout << ") = " << zout(badLoc);
-                    bool foundCulprit = false;
-                    for (size_t idg = 0; idg < nx * ny; ++idg) {
-                        if (dgData(idg, 0) == zout(badLoc)) {
-                            foundCulprit = true;
-                            std::cout << " = dgData(";
-                            auto dgBadLoc = zout.locationFromIndex(badIdx);
-                            for (auto badLocDim : dgBadLoc) {
-                                std::cout << badLocDim << ",";
-                            }
-                            std::cout << ")" << std::endl;
-                        }
-                    }
-                    if (!foundCulprit) {
-                        std::cout << " which has no match found in dgData" << std::endl;
-                    }
-                }
-                std::cout << std::endl;
-            }
-            REQUIRE_MESSAGE(absSum == 0., "k=", std::to_string(l));
-        }
-
-        REQUIRE(zout(0UL, 0UL, k) == zin(0UL, 0UL, k));
-        REQUIRE(zout(nx-1, 0UL, k) == zin(nx-1, 0UL, k));
-        REQUIRE(zout(0UL, ny-1, k) == zin(0UL, ny-1, k));
-    }
-}
-
 TEST_SUITE_END();
 
 }
