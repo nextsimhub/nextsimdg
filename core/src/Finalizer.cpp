4beab2ee9ec329d0b3d7f408ea109c2935ded695
Tim Spain <timothy.spain@nersc.no>
2024-09-12 08:44:49 +0200

    clang formatting

--- core/src/Finalizer.cpp
+++ core/src/Finalizer.cpp
@@ -10,45 +10,45 @@
 namespace Nextsim {
 
 void Finalizer::registerFunc(const FinalFn& fn)
-    {
-        auto& fns = functions();
-        fns.push_back(fn);
-    }
+{
+    auto& fns = functions();
+    fns.push_back(fn);
+}
 
 bool Finalizer::registerUnique(const FinalFn& fn)
-    {
-        if (contains(fn))
-            return false;
-        registerFunc(fn);
-        return true;
-    }
+{
+    if (contains(fn))
+        return false;
+    registerFunc(fn);
+    return true;
+}
 
 bool Finalizer::contains(const FinalFn& fn)
-    {
-        auto& fns = functions();
-        for (const auto& stored : fns) {
-            if (stored == fn)
-                return true;
-        }
-        return false;
+{
+    auto& fns = functions();
+    for (const auto& stored : fns) {
+        if (stored == fn)
+            return true;
     }
+    return false;
+}
 
 void Finalizer::finalize()
-    {
-        while (!functions().empty()) {
-            auto fn = functions().back();
-            // Remove the function from the finalization list before executing it.
-            functions().pop_back();
-            fn();
-        }
+{
+    while (!functions().empty()) {
+        auto fn = functions().back();
+        // Remove the function from the finalization list before executing it.
+        functions().pop_back();
+        fn();
     }
+}
 
 size_t Finalizer::count() { return functions().size(); }
 
 Finalizer::FinalFnContainer& Finalizer::functions()
-    {
-        static FinalFnContainer set;
-        return set;
-    }
+{
+    static FinalFnContainer set;
+    return set;
+}
 
 }
