cacde72ac37d864f4d64a75b3ee6b646af1fe621
Tim Spain <timothy.spain@nersc.no>
2024-09-16 13:25:48 +0200

    clang formatting

--- core/src/ConfiguredModule.cpp
+++ core/src/ConfiguredModule.cpp
@@ -20,7 +20,6 @@ const std::string ConfiguredModule::MODULE_PREFIX = "Modules";
 // A default string that can never be a valid C++ class name
 static const std::string defaultStr = "+++DEFAULT+++";
 
-
 std::string ConfiguredModule::addPrefix(const std::string& moduleName)
 {
     return MODULE_PREFIX + "." + moduleName;
@@ -31,8 +30,8 @@ std::string ConfiguredModule::getImpl(const std::string& module)
     boost::program_options::options_description opt;
 
     opt.add_options()(addPrefix(module).c_str(),
-            boost::program_options::value<std::string>()->default_value(defaultStr),
-            ("Load an implementation of " + module).c_str());
+        boost::program_options::value<std::string>()->default_value(defaultStr),
+        ("Load an implementation of " + module).c_str());
 
     std::string implString = Configurator::parse(opt)[addPrefix(module)].as<std::string>();
     // Only do anything if the retrieved option is not the default value
