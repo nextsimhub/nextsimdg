90fe9b90f0ca59b08eaf4fe78eff0e9562105b6f
Tim Spain <timothy.spain@nersc.no>
2024-10-14 09:55:27 +0200

    Zero out the high DG components when setting forcing data.

--- dynamics/src/CGDynamicsKernel.cpp
+++ dynamics/src/CGDynamicsKernel.cpp
@@ -71,18 +71,22 @@ void CGDynamicsKernel<DGadvection>::setData(const std::string& name, const Model
         isAdvectionReady = false;
     } else if (name == uWindName) {
         DGVector<DGadvection> utmp(*smesh);
+        utmp.zero();
         DGModelArray::ma2dg(data, utmp);
         Nextsim::Interpolations::DG2CG(*smesh, uAtmos, utmp);
     } else if (name == vWindName) {
         DGVector<DGadvection> vtmp(*smesh);
+        vtmp.zero();
         DGModelArray::ma2dg(data, vtmp);
         Nextsim::Interpolations::DG2CG(*smesh, vAtmos, vtmp);
     } else if (name == uOceanName) {
         DGVector<DGadvection> utmp(*smesh);
+        utmp.zero();
         DGModelArray::ma2dg(data, utmp);
         Nextsim::Interpolations::DG2CG(*smesh, uOcean, utmp);
     } else if (name == vOceanName) {
         DGVector<DGadvection> vtmp(*smesh);
+        vtmp.zero();
         DGModelArray::ma2dg(data, vtmp);
         Nextsim::Interpolations::DG2CG(*smesh, vOcean, vtmp);
     } else {
