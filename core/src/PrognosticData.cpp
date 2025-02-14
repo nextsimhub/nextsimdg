7abdeccbe1b2e0d42c4060f558d79490e250ca21
Tim Spain <timothy.spain@nersc.no>
2025-02-14 09:03:24 +0100

    Set the DG0 component of damge to 0.

--- core/src/PrognosticData.cpp
+++ core/src/PrognosticData.cpp
@@ -99,7 +99,7 @@ void PrognosticData::setData(const ModelState::DataMap& ms)
         copyAllComponents(ms.at(damageName), damage);
     } else {
         damage.resize();
-        damage = 1.;
+        damage.component(0) = 1.;
     }
 
     pAtmBdy->setData(ms);
