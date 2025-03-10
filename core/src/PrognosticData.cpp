c5a032761a958f0f7dd292ef2eb84a6671cbc038
Tim Spain <timothy.spain@nersc.no>
2025-03-10 08:34:40 +0100

    Initialize the hsnow variable.

--- core/src/PrognosticData.cpp
+++ core/src/PrognosticData.cpp
@@ -91,6 +91,7 @@ void PrognosticData::setData(const ModelState::DataMap& ms)
     // Copy the full DG data
     hiceAdvection = 0;
     ciceAdvection = 0;
+    hsnow = 0;
     damage = 0;
     hsnow = 0;
     copyAllComponents(ms.at(hiceName), hiceAdvection);
