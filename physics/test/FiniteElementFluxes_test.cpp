671164a9c2461aa7c4c6dbf4b069eb0476066a80
Tim Spain <timothy.spain@nersc.no>
2025-04-29 14:23:07 +0200

    Fix the FiniteElementFluxes test.

--- physics/test/FiniteElementFluxes_test.cpp
+++ physics/test/FiniteElementFluxes_test.cpp
@@ -29,7 +29,6 @@ TEST_SUITE_BEGIN("FiniteElementFluxes");
 TEST_CASE("Melting conditions")
 {
     ModelArray::setDimensions(ModelArray::Type::H, { 1, 1 });
-    ModelArray::setDimensions(ModelArray::Type::Z, { 1, 1, 1 });
 
     Module::Module<IFreezingPoint>::setImplementation("Nextsim::UnescoFreezing");
     Module::Module<IIceAlbedo>::setImplementation("Nextsim::CCSMIceAlbedo");
@@ -191,7 +190,6 @@ TEST_CASE("Melting conditions")
 TEST_CASE("Freezing conditions")
 {
     ModelArray::setDimensions(ModelArray::Type::H, { 1, 1 });
-    ModelArray::setDimensions(ModelArray::Type::Z, { 1, 1, 1 });
 
     Module::Module<IFreezingPoint>::setImplementation("Nextsim::UnescoFreezing");
     Module::Module<IIceAlbedo>::setImplementation("Nextsim::CCSMIceAlbedo");
