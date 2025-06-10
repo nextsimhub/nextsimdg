a610411dc9032308f7faf5a19d6cfa739a22f27b
Joe Wallwork <jw2423@cam.ac.uk>
2025-06-10 13:24:28 +0100

    Avoid broken pipe issue

--- scripts/doxygen_prep.sh
+++ scripts/doxygen_prep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Bash script for preparing a file to be processed by Doxygen.
+#
+# In particular, it adds the @file and @date entries to the frontmatter of the file.
 
 # Check for valid input
 if [ -z "$1" ]; then
@@ -20,13 +23,17 @@ if ! git ls-files --error-unmatch "${FILE}" >/dev/null 2>&1; then
 fi
 
 # Only consider files containing '@author'
-if [ -n "$(cat ${FILE} | grep '@author')" ]; then
+if grep -q '@author' "${FILE}"; then
 
   # Get the base filename
   FILE_NO_PATH=$(basename "${FILE}")
 
   # Get the date of the most recent change using git blame
   LAST_CHANGE_DATE=$(git blame --line-porcelain "${FILE}" | grep "^author-time " | sort -nr | head -n 1 | awk '{print $2}' | xargs -I{} date -d @{} +"%d %b %Y")
+  if [ -z "${LAST_CHANGE_DATE}" ]; then
+    echo "Error: Unable to determine the last change date for '${FILE}'."
+    exit 1
+  fi
 
   # Insert frontmatter @file and @date entries required by Doxygen, separated by a blank line
   sed -i "1a \ * @file    ${FILE_NO_PATH}\n *\n * @date    ${LAST_CHANGE_DATE}" "${FILE}"
