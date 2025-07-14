8bd4f0c640431ed3f78026703fc7c4375542b333
Joe Wallwork <jw2423@cam.ac.uk>
2025-07-14 15:46:21 +0100

    Remove groups from NetcdfMetadataConfiguration

--- core/src/NetcdfMetadataConfiguration.cpp
+++ core/src/NetcdfMetadataConfiguration.cpp
@@ -9,7 +9,6 @@
 
 #include <map>
 #include <ncFile.h>
-#include <ncGroup.h>
 #include <ncVar.h>
 
 namespace Nextsim {
@@ -23,18 +22,10 @@ std::stringstream NetcdfMetadataConfiguration::read(const std::string& source)
     std::string fileName = Configurator::parse(opt)[source].as<std::string>();
 
     netCDF::NcFile ncFile(fileName, netCDF::NcFile::read);
-    // Get the configuration group, allowing for it to not exist. In which
-    // case, do nothing
-    // TODO: Remove nc groups
-    netCDF::NcGroup metadataGroup(ncFile.getGroup("metadata"));
-    if (metadataGroup.isNull()) {
-        return std::stringstream();
-    }
-    netCDF::NcGroup configurationGroup(metadataGroup.getGroup("configuration"));
 
     std::stringstream config;
 
-    std::multimap<std::string, netCDF::NcVar> configs = configurationGroup.getVars();
+    std::multimap<std::string, netCDF::NcVar> configs = ncFile.getVars();
     for (auto entry : configs) {
         config << entry.first << " = ";
         if (entry.second.getType() == netCDF::ncDouble) {
