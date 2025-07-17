eec80a892ff7f2a6852c7597ff1e0680e7a1091b
Joe Wallwork <jw2423@cam.ac.uk>
2025-07-17 16:29:01 +0100

    Remove groups from ThermoIntegration test

--- test/ThermoIntegration_test.py
+++ test/ThermoIntegration_test.py
@@ -5,6 +5,8 @@ import unittest
 import netCDF4
 import numpy as np
 
+# TODO: Convert to CDL
+
 
 class SingleColumnThermo(unittest.TestCase):
     # A few useful global variables for the class
@@ -39,11 +41,11 @@ class SingleColumnThermo(unittest.TestCase):
 
         # Load the basic variables
         root = netCDF4.Dataset(cls.diagnostics_file, "r", format="NETCDF4")
-        cls.hice = np.squeeze(np.array(root.groups["data"].variables["hice"][:].data))
-        cls.hsnow = np.squeeze(np.array(root.groups["data"].variables["hsnow"][:].data))
-        cls.tsurf = np.array(root.groups["data"].variables["tsurf"][:].data)
-        cls.tintr = np.array(root.groups["data"].variables["tinterior"][:].data)
-        cls.tbott = np.array(root.groups["data"].variables["tbottom"][:].data)
+        cls.hice = np.squeeze(np.array(root.variables["hice"][:].data))
+        cls.hsnow = np.squeeze(np.array(root.variables["hsnow"][:].data))
+        cls.tsurf = np.array(root.variables["tsurf"][:].data)
+        cls.tintr = np.array(root.variables["tinterior"][:].data)
+        cls.tbott = np.array(root.variables["tbottom"][:].data)
 
     @classmethod
     def __make_cfg_file(cls):
@@ -85,22 +87,6 @@ ks = 0.31
         import numpy as np
 
         root = netCDF4.Dataset(cls.init_file, "w", format="NETCDF4")
-        metagrp = root.createGroup("structure")
-        structure_name = "parametric_rectangular"
-        metagrp.type = structure_name
-
-        metagrp = root.createGroup("metadata")
-        metagrp.type = structure_name
-        confgrp = metagrp.createGroup("configuration")  # But add nothing to it
-        timegrp = metagrp.createGroup("time")
-        time_var = timegrp.createVariable("time", "i8")
-        data_time = 1263204000
-        time_var[:] = data_time
-        time.units = "seconds since 1970-01-01T00:00:00Z"
-        formatted = timegrp.createVariable("formatted", str)
-        formatted.format = "%Y-%m-%dT%H:%M:%SZ"
-        formatted[0] = "2010-01-01T00:00:00Z"
-        datagrp = root.createGroup("data")
 
         nfirst = 1
         nsecond = 1
@@ -109,15 +95,15 @@ ks = 0.31
         n_dgstress = 1
         n_coords = 2
 
-        yDim = datagrp.createDimension("ydim", nfirst)
-        xDim = datagrp.createDimension("xdim", nsecond)
-        yVertexDim = datagrp.createDimension("yvertex", nfirst + 1)
-        xVertexDim = datagrp.createDimension("xvertex", nsecond + 1)
-        ycg_dim = datagrp.createDimension("y_cg", nfirst * ncg + 1)
-        xcg_dim = datagrp.createDimension("x_cg", nsecond * ncg + 1)
-        dg_comp = datagrp.createDimension("dg_comp", n_dg)
-        dgs_comp = datagrp.createDimension("dgstress_comp", n_dgstress)
-        n_coords_comp = datagrp.createDimension("ncoords", n_coords)
+        yDim = root.createDimension("ydim", nfirst)
+        xDim = root.createDimension("xdim", nsecond)
+        yVertexDim = root.createDimension("yvertex", nfirst + 1)
+        xVertexDim = root.createDimension("xvertex", nsecond + 1)
+        ycg_dim = root.createDimension("y_cg", nfirst * ncg + 1)
+        xcg_dim = root.createDimension("x_cg", nsecond * ncg + 1)
+        dg_comp = root.createDimension("dg_comp", n_dg)
+        dgs_comp = root.createDimension("dgstress_comp", n_dgstress)
+        n_coords_comp = root.createDimension("ncoords", n_coords)
 
         field_dims = ("ydim", "xdim")
         coord_dims = ("yvertex", "xvertex", "ncoords")
@@ -135,16 +121,16 @@ ks = 0.31
 
         node_lat[:, :] = lat0
 
-        coords = datagrp.createVariable("coords", "f8", coord_dims)
+        coords = root.createVariable("coords", "f8", coord_dims)
         coords[:, :, 0] = node_lon
         coords[:, :, 1] = node_lat
 
-        elem_lon = datagrp.createVariable("longitude", "f8", field_dims)
+        elem_lon = root.createVariable("longitude", "f8", field_dims)
         elem_lon[:, :] = 0
-        elem_lat = datagrp.createVariable("latitude", "f8", field_dims)
+        elem_lat = root.createVariable("latitude", "f8", field_dims)
         elem_lat[:, :] = 90
 
-        grid_azimuth = datagrp.createVariable("grid_azimuth", "f8", field_dims)
+        grid_azimuth = root.createVariable("grid_azimuth", "f8", field_dims)
         grid_azimuth[:, :] = 0
 
         ice_salinity = 5  # should match Ice::s in constants.hpp
@@ -152,28 +138,28 @@ ks = 0.31
         ocean_temperature = -1.54
         ocean_salinity = ocean_temperature / mu
 
-        mask = datagrp.createVariable("mask", "f8", field_dims)
+        mask = root.createVariable("mask", "f8", field_dims)
         mask[:, :] = [[1]]
-        cice = datagrp.createVariable("cice", "f8", field_dims)
+        cice = root.createVariable("cice", "f8", field_dims)
         cice[:, :] = 1.
-        hice = datagrp.createVariable("hice", "f8", field_dims)
+        hice = root.createVariable("hice", "f8", field_dims)
         hice[:, :] = 3.00
-        hsnow = datagrp.createVariable("hsnow", "f8", field_dims)
+        hsnow = root.createVariable("hsnow", "f8", field_dims)
         hsnow[:, :] = 0.3
-        sss = datagrp.createVariable("sss", "f8", field_dims)
+        sss = root.createVariable("sss", "f8", field_dims)
         sss[:, :] = ocean_salinity
-        sst = datagrp.createVariable("sst", "f8", field_dims)
+        sst = root.createVariable("sst", "f8", field_dims)
         sst[:, :] = ocean_temperature
-        tsurf = datagrp.createVariable("tsurf", "f8", field_dims)
+        tsurf = root.createVariable("tsurf", "f8", field_dims)
         tsurf[:, :] = ice_salinity * mu
-        tintr = datagrp.createVariable("tinterior", "f8", field_dims)
+        tintr = root.createVariable("tinterior", "f8", field_dims)
         tintr[:, :] = ice_salinity * mu
-        tbott = datagrp.createVariable("tbottom", "f8", field_dims)
+        tbott = root.createVariable("tbottom", "f8", field_dims)
         tbott[:, :] = ice_salinity * mu
         # Ice is at rest
-        u = datagrp.createVariable("u", "f8", field_dims)
+        u = root.createVariable("u", "f8", field_dims)
         u[:, :] = 0
-        v = datagrp.createVariable("v", "f8", field_dims)
+        v = root.createVariable("v", "f8", field_dims)
         v[:, :] = 0
         root.close()
 
--- test/run_test_jan2010_integration_test.sh
+++ test/run_test_jan2010_integration_test.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 # Get the name of the python executable
-if [ $# -lt 1 ]
-then
-    PYTHON=python
+if [ $# -lt 1 ]; then
+  PYTHON=python
 else
-    PYTHON=$1
+  PYTHON=$1
 fi
-
 
 # Prepares, executes and test the nextSIM-DG integration test based on the
 # January 2010 Arctic simulation
@@ -17,8 +15,6 @@ era5_file=25km_NH.ERA5_2010-01-01T000000_test_data.nc
 topaz_file=25km_NH.TOPAZ4_2010-01-01T000000_test_data.nc
 out_file=out.integration_test.nc
 
-$PYTHON make_init25kmNH_test_data.py
-$PYTHON era5_topaz4_test_data.py
 echo run_integration_test.sh
 ./run_integration_test.sh
 $PYTHON test_integration_test.py
