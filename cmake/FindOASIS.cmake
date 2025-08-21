3b0dd20845b4608c36e7baf60642af8da9dbc744
Einar Örn Ólason <einar.olason@nersc.no>
2025-08-21 10:56:52 +0200

    Search explicitly for all the component libraries of OASIS
    This makes the CMakeLists.txt file look more standard and putts all
    essential functionality into FindOASIS.cmake. It would also catch the
    extremely unlikely case when some of the component libraries of OASIS
    are missing or in a different location from the rest. Also, a bit of
    reformatting.

--- cmake/FindOASIS.cmake
+++ cmake/FindOASIS.cmake
@@ -2,19 +2,27 @@
 #
 # Please pass the -DOASIS_DIR variable to cmake (location of the OASIS libraries).
 
-find_library (OASIS_LIBRARIES NAMES mct mpeu oasis.cbind psmile.MPI1 scrip HINTS ${OASIS_DIR}/lib ENV LD_LIBRARY_PATH)
+# Search for all library files individually and merge to a single OASIS_LIBRARIES variable for the parent CMakeLists.txt
+find_library(LIB_OASIS NAMES oasis.cbind HINTS ${OASIS_DIR}/lib ENV LD_LIBRARY_PATH)
+find_library(LIB_MCT NAMES mct HINTS ${OASIS_DIR}/lib ENV LD_LIBRARY_PATH)
+find_library(LIB_MPEU NAMES mpeu HINTS ${OASIS_DIR}/lib ENV LD_LIBRARY_PATH)
+find_library(LIB_PSMILE NAMES psmile.MPI1 HINTS ${OASIS_DIR}/lib ENV LD_LIBRARY_PATH)
+find_library(LIB_SCRIP NAMES scrip HINTS ${OASIS_DIR}/lib ENV LD_LIBRARY_PATH)
 
-get_filename_component (OASIS_LIBRARIES "${OASIS_LIBRARIES}" PATH)
+if (LIB_OASIS AND LIB_MCT AND LIB_MPEU AND LIB_PSMILE AND LIB_SCRIP)
+    set(OASIS_LIBRARIES ${LIB_OASIS} ${LIB_MCT} ${LIB_MPEU} ${LIB_PSMILE} ${LIB_SCRIP})
+endif ()
 
-set (OASIS_DIR "${OASIS_LIBRARIES}/../")
+get_filename_component(OASIS_LIB_DIR "${LIB_OASIS}" PATH)
+
+set(OASIS_DIR "${OASIS_LIB_DIR}/../")
 cmake_path(NORMAL_PATH OASIS_DIR)
 
-find_path (OASIS_INCLUDES NAMES oasis_c.h HINTS ${OASIS_DIR}/include)
+find_path(OASIS_INCLUDES NAMES oasis_c.h HINTS ${OASIS_DIR}/include)
 
 # handle the QUIETLY and REQUIRED arguments and set OASIS_FOUND to TRUE if all listed variables are TRUE
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OASIS DEFAULT_MSG
-        OASIS_LIBRARIES OASIS_INCLUDES)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OASIS DEFAULT_MSG OASIS_DIR OASIS_LIBRARIES OASIS_INCLUDES)
 
 # message (STATUS "OASIS_LIBRARIES: ${OASIS_LIBRARIES}")
 # message (STATUS "OASIS_INCLUDES: ${OASIS_INCLUDES}")
