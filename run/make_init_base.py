5d8bd08442b5c4e3612de5e951b0f148dfed4255
Einar Örn Ólason <einar.olason@nersc.no>
2025-09-12 14:18:18 +0200

    Make space for geographic grids in make_init_base.py
    Just rewrite it a bit so that you have to call either
    make_cartesian_grid or make_geographic_grid before setting your
    variables. Only make_cartesian_grid implemented so far.

--- run/make_init_base.py
+++ run/make_init_base.py
@@ -10,47 +10,32 @@ class initMaker:
     Usage:
      0. Import make_init_base
       >>> from make_init_base import initMaker
-     1. Create an initialiser object with given filename, dimensions, and
-        resolution, e.g.
-      >>> init = initMaker("test", 128, 128, 3e3)
-     2. Modify any variables needed, e.g.
+     1. Create an initialiser object with a given filename, e.g.
+      >>> init = initMaker("test")
+         2a. Create a cartesian grid from dimensions and resolution, e.g.
+          >>> init.make_cartesian_grid(128, 128, 3e3)
+         2b. Create a geographic grid from a file containing coordinates. We need to define type of grid:
+            * "p": only center points of the grid are given
+            * "ur": the centre and upper right corner of the grid is given
+            * "ll": the centre and lower left corner of the grid is given
+          >>> init.make_geographic_grid("coords.nc", "p", p_lat_name="latitude", p_lon_name="longitude")
+     3. Modify any variables needed, e.g.
       >>> init.cice = 1
       >>> init.hice = 3
       >>> init.hice[10:20, 10:20] = 4
-     3. The init file is written when the initialiser object goes out of scope
+     4. The init file is written when the initialiser object goes out of scope
         (e.g. when the program ends or at the end of a loop).
     """
 
-    def __init__(self, fname, nFirst, nSecond, res, nCg=1, nDg=1, nDgStress=3, nCoords=2, checkZeros=True):
+    def __init__(self, fname, nCg=1, nDg=1, nDgStress=3, nCoords=2, checkZeros=True):
         """
-        Initialise all internal variables, except __nfirst, __nsecond, __nLayers,
-        and __res to zero. All arrays are set to the right size as well.
-        
+        Initialise basic internal variables, but we must call make_cartesian_grid or make_geographic_grid before continuing.
+
         :param fname: Name of the file to write the output into
-        :param nFirst: Number of rows (first dimension)
-        :param nSecond: Number of columns (seond dimension)
-        :param res: Model resolution [m]
         """
 
         # Set the file name
         self.__fname = fname
-
-        # Set the array dimensions and resolution
-        self.__nFirst = nFirst
-        self.__nSecond = nSecond
-        self.__res = res
-
-        # Set all arrays to the correct size
-        self.mask = np.zeros((self.__nFirst, self.__nSecond))
-        self.cice = np.zeros((self.__nFirst, self.__nSecond))
-        self.hice = np.zeros((self.__nFirst, self.__nSecond))
-        self.hsnow = np.zeros((self.__nFirst, self.__nSecond))
-        self.damage = np.zeros((self.__nFirst, self.__nSecond))
-        self.uice = np.zeros((self.__nFirst, self.__nSecond))
-        self.vice = np.zeros((self.__nFirst, self.__nSecond))
-        self.azimuth = np.zeros((self.__nFirst, self.__nSecond))
-        self.sss = np.zeros((self.__nFirst, self.__nSecond))
-        self.sst = np.zeros((self.__nFirst, self.__nSecond))
 
         # Set basic coordinate sizes
         self.__nCg = nCg
@@ -61,30 +46,114 @@ class initMaker:
         # Do we check for zeros?
         self.__checkZeros = checkZeros
 
+    def __init_vars__(self, nFirst, nSecond):
+
+        # Set all arrays to the correct size
+        self.__nFirst = nFirst
+        self.__nSecond = nSecond
+
+        self.mask = np.zeros((nFirst, nSecond))
+        self.cice = np.zeros((nFirst, nSecond))
+        self.hice = np.zeros((nFirst, nSecond))
+        self.hsnow = np.zeros((nFirst, nSecond))
+        self.damage = np.zeros((self.__nFirst, nSecond))
+        self.uice = np.zeros((nFirst, nSecond))
+        self.vice = np.zeros((nFirst, nSecond))
+        self.azimuth = np.zeros((nFirst, nSecond))
+        self.sss = np.zeros((nFirst, nSecond))
+        self.sst = np.zeros((nFirst, nSecond))
+
+    def __init_file(self):
+        """
+        Open the netCDF file for writing and create the dimensions and basic structure.
+        """
+
+        self.__ncFile = netCDF4.Dataset(self.__fname, "w", format="NETCDF4")
+
+        structure_name = "parametric_rectangular"
+        self.__ncFile.structure_name = structure_name
+
+        self.__ncFile.createDimension("ydim", self.__nFirst)
+        self.__ncFile.createDimension("xdim", self.__nSecond)
+        self.__ncFile.createDimension("yvertex", self.__nFirst + 1)
+        self.__ncFile.createDimension("xvertex", self.__nSecond + 1)
+        self.__ncFile.createDimension("y_cg", self.__nFirst * self.__nCg + 1)
+        self.__ncFile.createDimension("x_cg", self.__nSecond * self.__nCg + 1)
+        self.__ncFile.createDimension("dg_comp", self.__nDg)
+        self.__ncFile.createDimension("dgstress_comp", self.__nDgStress)
+        self.__ncFile.createDimension("ncoords", self.__nCoords)
+
+        self.__field_dims = ("ydim", "xdim")
+        self.__coord_dims = ("yvertex", "xvertex", "ncoords")
+
+    def make_cartesian_grid(self, nFirst, nSecond, res):
+        """
+        Create a cartesian grid with regular spacing at a given resolution.
+
+        :param nFirst: Number of rows (first dimension)
+        :param nSecond: Number of columns (seond dimension)
+        :param res: Model resolution [m]
+        """
+
+        # Set the array dimensions and resolution
+        self.__res = res
+
+        # Initialise the arrays
+        self.__init_vars__(nFirst, nSecond)
+
+        self.__init_file()
+
+        # Array coordinates
+        x = np.zeros((self.__nFirst + 1, self.__nSecond + 1))
+        y = np.zeros((self.__nFirst + 1, self.__nSecond + 1))
+        for j in range(self.__nFirst + 1):
+            for i in range(self.__nSecond + 1):
+                x[j, i] = i * self.__res
+                y[j, i] = j * self.__res
+
+        coords = self.__ncFile.createVariable("coords", "f8", self.__coord_dims)
+        coords[:, :, 0] = x
+        coords[:, :, 1] = y
+
+        px = np.zeros((self.__nFirst, self.__nSecond))
+        py = np.zeros((self.__nFirst, self.__nSecond))
+        for j in range(self.__nFirst):
+            for i in range(self.__nSecond):
+                px[j, i] = (j + 0.5) * self.__res
+                py[j, i] = (i + 0.5) * self.__res
+
+        elem_x = self.__ncFile.createVariable("x", "f8", self.__field_dims)
+        elem_x[:, :] = px
+        elem_y = self.__ncFile.createVariable("y", "f8", self.__field_dims)
+        elem_y[:, :] = py
+
+        grid_azimuth = self.__ncFile.createVariable("grid_azimuth", "f8", self.__field_dims)
+        grid_azimuth[:, :] = 0.
+
     def __testFields__(self):
         """
         Check if arrays are non-zero and the right size. Print a warning if
         they're zero (this may be ok). Raise a RuntimeError if the shape is wrong.
         """
-        if (self.mask==0).all():
+        if (self.mask == 0).all():
             print("Error: 'mask' is not set (all values are zero, meaning land everywhere)")
             raise RuntimeError("'mask' is not set")
 
-        for check in [["cice", (self.cice==0).all(), self.cice.shape==(self.__nFirst,self.__nSecond)],
-                      ["hice", (self.hice==0).all(), self.hice.shape==(self.__nFirst,self.__nSecond)],
-                      ["hsnow", (self.hsnow==0).all(), self.hsnow.shape==(self.__nFirst,self.__nSecond)],
-                      ["damage", (self.damage==0).all(), self.damage.shape==(self.__nFirst,self.__nSecond)],
-                      ["uice", (self.uice==0).all(), self.uice.shape==(self.__nFirst,self.__nSecond)],
-                      ["vice", (self.vice==0).all(), self.vice.shape==(self.__nFirst,self.__nSecond)],
-                      ["sss", (self.sss==0).all(), self.sss.shape==(self.__nFirst,self.__nSecond)],
-                      ["sst", (self.sst==0).all(), self.sst.shape==(self.__nFirst,self.__nSecond)],
-                      ["azimuth", (self.azimuth==0).all(), self.azimuth.shape==(self.__nFirst,self.__nSecond)]]:
+        for check in [["cice", (self.cice == 0).all(), self.cice.shape == (self.__nFirst, self.__nSecond)],
+                      ["hice", (self.hice == 0).all(), self.hice.shape == (self.__nFirst, self.__nSecond)],
+                      ["hsnow", (self.hsnow == 0).all(), self.hsnow.shape == (self.__nFirst, self.__nSecond)],
+                      ["damage", (self.damage == 0).all(), self.damage.shape == (self.__nFirst, self.__nSecond)],
+                      ["uice", (self.uice == 0).all(), self.uice.shape == (self.__nFirst, self.__nSecond)],
+                      ["vice", (self.vice == 0).all(), self.vice.shape == (self.__nFirst, self.__nSecond)],
+                      ["sss", (self.sss == 0).all(), self.sss.shape == (self.__nFirst, self.__nSecond)],
+                      ["sst", (self.sst == 0).all(), self.sst.shape == (self.__nFirst, self.__nSecond)],
+                      ["azimuth", (self.azimuth == 0).all(), self.azimuth.shape == (self.__nFirst, self.__nSecond)]]:
 
             if self.__checkZeros and check[1]:
-                print("Warning: '"+check[0]+"' is all zeros (this may be ok, if that's what you want).")
+                print("Warning: '" + check[0] + "' is all zeros (this may be ok, if that's what you want).")
 
             if not check[2]:
-                print("Error: '"+check[0]+"' is the wrong shape")
+                print("Error: '" + check[0] + "' is the wrong shape")
                 raise RuntimeError("Incorrect array shape")
 
     def __del__(self):
@@ -99,82 +168,37 @@ class initMaker:
 
         self.__testFields__()
 
-        ncFile = netCDF4.Dataset(self.__fname, "w", format="NETCDF4")
-
-        structure_name = "parametric_rectangular"
-        ncFile.structure_name = structure_name
-
-        ncFile.createDimension("ydim", self.__nFirst)
-        ncFile.createDimension("xdim", self.__nSecond)
-        ncFile.createDimension("yvertex", self.__nFirst + 1)
-        ncFile.createDimension("xvertex", self.__nSecond + 1)
-        ncFile.createDimension("y_cg", self.__nFirst * self.__nCg + 1)
-        ncFile.createDimension("x_cg", self.__nSecond * self.__nCg + 1)
-        ncFile.createDimension("dg_comp", self.__nDg)
-        ncFile.createDimension("dgstress_comp", self.__nDgStress)
-        ncFile.createDimension("ncoords", self.__nCoords)
-
-        field_dims = ("ydim", "xdim")
-        coord_dims = ("yvertex", "xvertex", "ncoords")
-
-        # Array coordinates
-        x = np.zeros((self.__nFirst + 1, self.__nSecond + 1))
-        y = np.zeros((self.__nFirst + 1, self.__nSecond + 1))
-        for j in range(self.__nFirst + 1):
-            for i in range(self.__nSecond + 1):
-                x[j, i] = i * self.__res
-                y[j, i] = j * self.__res
-
-        coords = ncFile.createVariable("coords", "f8", coord_dims)
-        coords[:, :, 0] = x
-        coords[:, :, 1] = y
-
-        px = np.zeros((self.__nFirst, self.__nSecond))
-        py = np.zeros((self.__nFirst, self.__nSecond))
-        for j in range(self.__nFirst):
-            for i in range(self.__nSecond):
-                px[j, i] = (j + 0.5) * self.__res
-                py[j, i] = (i + 0.5) * self.__res
-
-        elem_x = ncFile.createVariable("x", "f8", field_dims)
-        elem_x[:, :] = px
-        elem_y = ncFile.createVariable("y", "f8", field_dims)
-        elem_y[:, :] = py
-
-        grid_azimuth = ncFile.createVariable("grid_azimuth", "f8", field_dims)
-        grid_azimuth[:, :] = self.azimuth
-
         # Set the mask
-        mask = ncFile.createVariable("mask", "f8", field_dims)
+        mask = self.__ncFile.createVariable("mask", "f8", self.__field_dims)
         mask[:, :] = self.mask
         antimask = 1 - mask[:, :]
 
         # Set the concentration
-        cice = ncFile.createVariable("cice", "f8", field_dims)
+        cice = self.__ncFile.createVariable("cice", "f8", self.__field_dims)
         cice[:, :] = self.cice
 
         # Set the thickness
-        hice = ncFile.createVariable("hice", "f8", field_dims)
+        hice = self.__ncFile.createVariable("hice", "f8", self.__field_dims)
         hice[:, :] = self.hice
 
         # Set snow thickness
-        hsnow = ncFile.createVariable("hsnow", "f8", field_dims)
+        hsnow = self.__ncFile.createVariable("hsnow", "f8", self.__field_dims)
         hsnow[:, :] = self.hsnow
 
         # Set snow thickness
-        damage = ncFile.createVariable("damage", "f8", field_dims)
+        damage = self.__ncFile.createVariable("damage", "f8", self.__field_dims)
         damage[:, :] = self.damage
 
         # Set ice velocity
-        u = ncFile.createVariable("u", "f8", field_dims)
+        u = self.__ncFile.createVariable("u", "f8", self.__field_dims)
         u[:, :] = self.uice
-        v = ncFile.createVariable("v", "f8", field_dims)
+        v = self.__ncFile.createVariable("v", "f8", self.__field_dims)
         v[:, :] = self.vice
 
         # Set ocean state
-        sst = ncFile.createVariable("sst", "f8", field_dims)
+        sst = self.__ncFile.createVariable("sst", "f8", self.__field_dims)
         sst[:, :] = self.sst
-        sss = ncFile.createVariable("sss", "f8", field_dims)
+        sss = self.__ncFile.createVariable("sss", "f8", self.__field_dims)
         sss[:, :] = self.sss
 
         # mask data
@@ -191,8 +215,7 @@ class initMaker:
         damage[:, :] = damage[:, :] * mask[:, :]
         u[:, :] = u[:, :] * mask[:, :]
         v[:, :] = v[:, :] * mask[:, :]
-        grid_azimuth[:, :] = grid_azimuth[:, :] * mask[:, :]
         sss[:, :] = sss[:, :] * mask[:, :]
         sst[:, :] = sst[:, :] * mask[:, :]
 
-        ncFile.close()
+        self.__ncFile.close()
