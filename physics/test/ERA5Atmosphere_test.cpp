e2ee5c7ddf241fb093b7225568ffeb40528bb266
Tim Spain <timothy.spain@nersc.no>
2025-10-06 12:54:15 +0200

    Create full-sized ERA5 test files.

--- physics/test/ERA5Atmosphere_test.cpp
+++ physics/test/ERA5Atmosphere_test.cpp
@@ -11,6 +11,11 @@
 
 #include <string>
 
+#include <ncDim.h>
+#include <ncDouble.h>
+#include <ncFile.h>
+#include <ncVar.h>
+
 namespace Nextsim {
 // Signatures of the helper functions
 using era5Buffer = Eigen::Array<double, Eigen::Dynamic, 1>;
@@ -21,6 +26,82 @@ size_t timeIndexFromTM(const std::tm* tm);
 era5Buffer getVarTimeData(const std::string& era5Name, const TimePoint& time);
 ModelArray maFromERA5Buffer(const era5Buffer& buffer);
 era5Buffer era5BufferHypot(const era5Buffer& x, const era5Buffer& y);
+
+#ifndef TEST_FILES_DIR
+#define TEST_FILES_DIR "."
+#endif
+
+const std::string testFilesDir = TEST_FILES_DIR;
+
+int testFileYear = 2000;
+const std::string era5NameLat = "lat";
+const std::string latFileName = "ERA5_lat_y2000.nc";
+const std::string era5NameLon = "lon";
+const std::string lonFileName = "ERA5_lon_y2000.nc";
+const std::string era5NameTimeTest = "timet";
+const std::string timeTestFileName = "ERA5_timet_y2000.nc";
+
+size_t nx = 1440;
+size_t ny = 265;
+double dlon = 0.25;
+double lon0 = 0.0;
+double dlat = -0.25;
+double lat0 = 90.;
+size_t nt = 8760;
+double t0 = 0.;
+double dt = 1.;
+
+// Create longitude and latitude unprojected data files.
+void createERA5TestFiles()
+{
+    // Spatial interpolation test files
+    netCDF::NcFile latFile(testFilesDir + "/" + latFileName, netCDF::NcFile::replace, netCDF::NcFile::nc4);
+    netCDF::NcFile lonFile(testFilesDir + "/" + lonFileName, netCDF::NcFile::replace, netCDF::NcFile::nc4);
+
+    era5Buffer longitudeDim(nx, 1);
+    era5Buffer latitudeDim(ny, 1);
+    era5Buffer timeDim(nt, 1);
+    era5Buffer lat2d(ny*nx, 1);
+    era5Buffer lon2d(ny*nx, 1);
+
+    for (size_t i = 0; i < nx; ++i) {
+        longitudeDim(i, 0) = lon0 + dlon * i;
+    }
+
+    for (size_t j = 0; j < ny; ++j) {
+        latitudeDim(j, 0) = lat0 + dlat * j;
+        for (size_t i = 0; i < nx; ++i) {
+            size_t idx = i + nx*(j);
+            lon2d(idx, 0) = longitudeDim(i, 0);
+            lat2d(idx, 0) = latitudeDim(j, 0);
+        }
+    }
+
+    for (size_t t = 0; t < nt; ++t) {
+        timeDim(t, 0) = t0 + t * dt;
+    }
+
+    std::vector<std::pair<netCDF::NcFile*, era5Buffer*>> fileDataPairs = {{&lonFile, &lon2d}, {&latFile, &lat2d}};
+    for (std::pair<netCDF::NcFile*, era5Buffer*> fileDataPair : fileDataPairs) {
+        netCDF::NcFile& file = *(fileDataPair.first);
+        era5Buffer& data = *(fileDataPair.second);
+        netCDF::NcDim lonDim = file.addDim("longitude", nx);
+        netCDF::NcDim latDim = file.addDim("latitude", ny);
+        netCDF::NcDim timDim = file.addDim("time", nt);
+        file.addVar("longitude", netCDF::ncDouble, {lonDim,}).putVar({0,}, {nx,}, longitudeDim.data());
+        file.addVar("latitude", netCDF::ncDouble, {latDim,}).putVar({0,}, {ny,}, latitudeDim.data());
+        netCDF::NcVar timeVar(file.addVar("time", netCDF::ncDouble, {timDim,}));
+        timeVar.putVar({0,}, {nt,}, timeDim.data());
+        std::string timeUnits = "hours since 2010-01-01 00:00:00";
+        timeVar.putAtt(std::string("units"), timeUnits);
+
+        netCDF::NcVar dataVar(file.addVar("data", netCDF::ncDouble, {timDim, latDim, lonDim}));
+        for (size_t t = 0; t < nt; ++t) {
+            dataVar.putVar({t, 0, 0}, {1, ny, nx}, data.data());
+        }
+        file.close();
+    }
+}
 
 TEST_SUITE_BEGIN("ERA5Atmosphere");
 TEST_CASE("e5Filename")
@@ -78,5 +159,9 @@ TEST_CASE("hypot")
         REQUIRE(c(i,0) == doctest::Approx(d(i,0)).epsilon(1e-12));
     }
 }
+
+TEST_CASE("Interpolation tests")
+{
+    createERA5TestFiles();
 }
-#undef ERA5TESTING
+}TEST_SUITE_END();
