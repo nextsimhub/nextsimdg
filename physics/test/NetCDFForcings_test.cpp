33a9de215b95afc4841828cf94152ca103c4c8e0
Tim Spain <timothy.spain@nersc.no>
2025-10-31 12:04:52 +0100

    Break up the test into sub-cases.

--- physics/test/NetCDFForcings_test.cpp
+++ physics/test/NetCDFForcings_test.cpp
@@ -24,6 +24,38 @@ double degrees(double rad)
 
 TEST_CASE("Spatial interpolation from field")
 {
+    // Longitude and latitude of the target grid
+    size_t nxt = 154;
+    size_t nyt = 121;
+    double lonCentreDeg = 180.;
+    double lonC = radians(lonCentreDeg);
+    double latCentreDeg = 82.;
+    double latC = radians(latCentreDeg);
+    double dDeg = 0.25; // Resolution in degrees
+
+    ModelArray::setDimension(ModelArray::Dimension::X, nxt);
+    ModelArray::setDimension(ModelArray::Dimension::Y, nyt);
+    ModelArray lonTarg(ModelArray::Type::H);
+    ModelArray latTarg(ModelArray::Type::H);
+    lonTarg.resize();
+    latTarg.resize();
+    // Create the target longitude & latitude arrays: polar stereographic
+    int ic = nxt/2;
+    int jc = nyt/2;
+
+    for (int j = 0; j < nyt; ++j) {
+        double y = (j - jc) * radians(dDeg);
+        for (int i = 0; i < nxt; ++i) {
+            double x = (i - ic) * radians(dDeg);
+            double rho = sqrt(x*x + y*y);
+            double c = 2 * atan(rho / 2);
+            latTarg(i, j) = degrees(asin(cos(c)*sin(latC) + y*sin(c)*cos(latC)/rho));
+            lonTarg(i, j) = degrees(lonC + atan2(x*sin(c), rho*cos(latC)*cos(c) - y*sin(latC)*sin(c)));
+        }
+    }
+
+    SUBCASE("ERA5-like test")
+    {
     using std::sqrt;
     using std::cos;
     using std::sin;
@@ -55,35 +87,6 @@ TEST_CASE("Spatial interpolation from field")
         }
     }
 
-    // Longitude and latitude of the target grid
-    size_t nxt = 154;
-    size_t nyt = 121;
-    double lonCentreDeg = 180.;
-    double lonC = radians(lonCentreDeg);
-    double latCentreDeg = 82.;
-    double latC = radians(latCentreDeg);
-    double dDeg = 0.25; // Resolution in degrees
-
-    ModelArray::setDimension(ModelArray::Dimension::X, nxt);
-    ModelArray::setDimension(ModelArray::Dimension::Y, nyt);
-    ModelArray lonTarg(ModelArray::Type::H);
-    ModelArray latTarg(ModelArray::Type::H);
-    lonTarg.resize();
-    latTarg.resize();
-    // Create the target longitude & latitude arrays: polar stereographic
-    int ic = nxt/2;
-    int jc = nyt/2;
-
-    for (int j = 0; j < nyt; ++j) {
-        double y = (j - jc) * radians(dDeg);
-        for (int i = 0; i < nxt; ++i) {
-            double x = (i - ic) * radians(dDeg);
-            double rho = sqrt(x*x + y*y);
-            double c = 2 * atan(rho / 2);
-            latTarg(i, j) = degrees(asin(cos(c)*sin(latC) + y*sin(c)*cos(latC)/rho));
-            lonTarg(i, j) = degrees(lonC + atan2(x*sin(c), rho*cos(latC)*cos(c) - y*sin(latC)*sin(c)));
-        }
-    }
     ModelArray iFrac = lonTarg / dlon;
     ModelArray jFrac = (latTarg - lat0) / dlat;
 
@@ -99,6 +102,7 @@ TEST_CASE("Spatial interpolation from field")
     testj = 20;
     REQUIRE(testLat(testi, testj) == doctest::Approx(latTarg(testi, testj)).epsilon(prec));
     REQUIRE(testLon(testi, testj) == doctest::Approx(lonTarg(testi, testj)).epsilon(prec));
+    }
 }
 
 } /* namespace Nextsim */
