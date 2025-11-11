563ab162ee48cd27a5a33e33539734e36429290a
Joe Wallwork <jw2423@cam.ac.uk>
2025-11-11 11:16:41 +0000

    Separate out setupFields and reorder

--- core/src/include/Xios.hpp
+++ core/src/include/Xios.hpp
@@ -159,8 +159,7 @@ private:
     int mpi_size { 0 };
     int cStrLen { 20 }; // Length of C-strings passed to XIOS
 
-    /* Configuration */
-    void setupFiles();
+    /* Context */
     void setupContext();
 
     /* Calendar, date and duration */
@@ -224,6 +223,7 @@ private:
     std::set<std::string> configGetFieldNames(const bool readAccess);
     bool configCheckField(const std::string fieldId, const bool readAccess);
     std::map<std::string, ModelArray::Type> fieldTypes;
+    void setupFields();
 
     /* File */
     xios::CFileGroup* getFileGroup();
@@ -241,6 +241,7 @@ private:
         { DIAGNOSTIC, diagnosticFileId },
         { FORCING, forcingFileId },
     };
+    void setupFiles();
 
     /* I/O */
     void read(const std::string fieldId, ModelArray& modelarray);
