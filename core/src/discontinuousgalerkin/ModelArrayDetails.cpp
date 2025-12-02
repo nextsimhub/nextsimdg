35cd875a69e9c280804b004e3fa029600846e024
Joe Wallwork <jw2423@cam.ac.uk>
2025-12-02 09:23:49 +0000

    Make underscore version primary

--- core/src/discontinuousgalerkin/ModelArrayDetails.cpp
+++ core/src/discontinuousgalerkin/ModelArrayDetails.cpp
@@ -27,10 +27,10 @@ namespace Nextsim {
 // clang-format off
 std::map<ModelArray::Dimension, ModelArray::DimensionSpec> ModelArray::definedDimensions = {
     // set default global size, local size and start position for each dimension
-    { ModelArray::Dimension::X, { "xdim", "x_dim", 0, 0, 0 } },
-    { ModelArray::Dimension::Y, { "ydim", "y_dim", 0, 0, 0 } },
-    { ModelArray::Dimension::XVERTEX, { "xvertex", "x_vertex", 1, 1, 0 } }, // defined as x + 1
-    { ModelArray::Dimension::YVERTEX, { "yvertex", "y_vertex", 1, 1, 0 } }, // defined as y + 1
+    { ModelArray::Dimension::X, { "x_dim", "xdim", 0, 0, 0 } },
+    { ModelArray::Dimension::Y, { "y_dim", "ydim", 0, 0, 0 } },
+    { ModelArray::Dimension::XVERTEX, { "x_vertex", "xvertex", 1, 1, 0 } }, // defined as x + 1
+    { ModelArray::Dimension::YVERTEX, { "y_vertex", "yvertex", 1, 1, 0 } }, // defined as y + 1
     { ModelArray::Dimension::XCG, { "x_cg", "x_cg", CGDEGREE, CGDEGREE, 0 } },
     { ModelArray::Dimension::YCG, { "y_cg", "y_cg", CGDEGREE, CGDEGREE, 0 } },
     // The DG components are also included here to store the names
