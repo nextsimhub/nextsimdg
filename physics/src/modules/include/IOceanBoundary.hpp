cf57dff1ca9051323fb21e41919011373e08bc22
Joe Wallwork <jw2423@cam.ac.uk>
2026-02-11 16:11:46 +0000

    Set XIOS field types for sss and sst in IOceanBoundary constructor

--- physics/src/modules/include/IOceanBoundary.hpp
+++ physics/src/modules/include/IOceanBoundary.hpp
@@ -8,6 +8,9 @@
 
 #include "include/CheckingModelComponent.hpp"
 #include "include/constants.hpp"
+#ifdef USE_XIOS
+#include "include/Xios.hpp"
+#endif
 #include "include/gridNames.hpp"
 
 namespace Nextsim {
@@ -71,6 +74,13 @@ public:
         getStore().registerArray(Protected::SSH, &ssh, RO);
         getStore().registerArray(Shared::Q_SW_OW, &qswow, RW);
         getStore().registerArray(Shared::Q_SW_BASE, &qswBase, RW);
+
+#ifdef USE_XIOS
+        // Set XIOS field types
+        Xios& xiosHandler = Xios::getInstance();
+        xiosHandler.setPrognosticFieldType(sssName, ModelArray::Type::H);
+        xiosHandler.setPrognosticFieldType(sstName, ModelArray::Type::H);
+#endif
     }
     virtual ~IOceanBoundary() = default;
 
