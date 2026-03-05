c63b9829afa2c706ab05916f368d9d70c029328d
Joe Wallwork <jw2423@cam.ac.uk>
2026-03-05 10:12:48 +0000

    Align XIOS tests with midnight

--- core/test/XiosReadDiagnostic_test.cpp
+++ core/test/XiosReadDiagnostic_test.cpp
@@ -18,7 +18,7 @@
 
 const std::string testFilesDir = TEST_FILES_DIR;
 const std::string diagnosticFilename
-    = testFilesDir + "/diagnostic_2023-03-17T17:11:00Z-2023-03-17T20:10:59Z.nc";
+    = testFilesDir + "/diagnostic_2023-03-17T00:00:00Z-2023-03-17T02:59:59Z.nc";
 
 static const int DGCOMP = 6;
 static const int DGSTRESSCOMP = 8;
@@ -37,8 +37,8 @@ MPI_TEST_CASE("TestXiosReadDiagnostic", 3)
 {
     std::stringstream config;
     config << "[model]" << std::endl;
-    config << "start = 2023-03-17T17:11:00Z" << std::endl;
-    config << "stop = 2023-03-17T23:11:00Z" << std::endl;
+    config << "start = 2023-03-17T00:00:00Z" << std::endl;
+    config << "stop = 2023-03-17T00:00:00Z" << std::endl;
     config << "time_step = P0-0T01:30:00" << std::endl;
     config << "init_file = " << diagnosticFilename << std::endl;
     config << "restart_period = P0-0T03:00:00" << std::endl;
@@ -91,8 +91,8 @@ MPI_TEST_CASE("TestXiosReadDiagnostic", 3)
     int rank;
     MPI_Comm_rank(test_comm, &rank);
     if (rank == 0) {
-        std::filesystem::remove("diagnostic_2023-03-17T17:11:00Z-2023-03-17T20:10:59Z.nc");
-        std::filesystem::remove("diagnostic_2023-03-17T20:11:00Z-2023-03-17T23:10:59Z.nc");
+        std::filesystem::remove("diagnostic_2023-03-17T00:00:00Z-2023-03-17T02:59:59Z.nc");
+        std::filesystem::remove("diagnostic_2023-03-17T03:00:00Z-2023-03-17T05:59:59Z.nc");
     }
 
     xiosHandler.context_finalize();
