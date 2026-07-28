48137a79717abc86aebf95e44440ab253c40df1e
Einar Örn Ólason <einar.olason@nersc.no>
2026-07-28 09:54:14 +0200

    Implement spatial interpolation
    This includes implementing weights calculation for the two lat/lon
    representation posibilities; a Mercator projection (lat and lon are two
    vectors --- untested!) and a general projection with lat and lon as 2D
    fields. Lots of refactoring and fixing as well.

--- core/src/include/ParaGridInputs.hpp
+++ core/src/include/ParaGridInputs.hpp
@@ -19,9 +19,9 @@ class ParaGridInputs {
 public:
     ParaGridInputs() = default;
 
-    void configure(const std::string& filePathIn, const std::string& ncLonNameIn,
-        const std::string& ncLatNameIn, const std::string& ncTimeNameIn,
-        const std::set<std::string>& forcingsIn,
+    void setData(const TimePoint& time, const std::string& filePathIn,
+        const std::string& ncLonNameIn, const std::string& ncLatNameIn,
+        const std::string& ncTimeNameIn, const std::set<std::string>& forcingsIn,
         const std::set<std::pair<std::string, std::string>>& vectorsIn,
         const ModelArray& modelLonsIn, const ModelArray& modelLatsIn);
 
@@ -33,14 +33,32 @@ public:
     }
 
 private:
-    [[nodiscard]] ModelState interpolateSpatially(
-        const std::map<std::string, std::vector<FloatType>>& rawData);
-    void rotateInputVectors(std::map<std::string, std::vector<FloatType>>& rawData);
+    typedef struct {
+        std::vector<size_t> dims;
+        std::map<std::string, std::vector<FloatType>> data;
+    } RawDataMap;
+
+    void setWeights();
+    void setWeights1D();
+    void setWeights2D();
+    bool recursiveBisectSearch(size_t k, FloatType targetLon, FloatType targetLat, size_t i,
+        size_t ii, size_t j, size_t jj);
+    void orthographicProjection(FloatType lon, FloatType lat, FloatType lon0, FloatType lat0,
+        FloatType& x, FloatType& y) const;
+    [[nodiscard]] bool pointInBoundingBox(
+        const std::vector<FloatType>& xCorners, const std::vector<FloatType>& yCorners);
+    [[nodiscard]] bool findLocalCoordinates(size_t k, FloatType x00, FloatType y00, FloatType x10,
+        FloatType y10, FloatType x01, FloatType y01, FloatType x11, FloatType y11);
+    [[nodiscard]] ModelState interpolateSpatially(const RawDataMap& rawData);
+    void rotateInputVectors(RawDataMap& rawData);
 
     std::string filePath, ncTimeName, ncLonName, ncLatName;
+    ModelArray modelLons, modelLats;
+    RawDataMap forcingLonLats;
     std::set<std::string> forcings;
     std::set<std::pair<std::string, std::string>> vectors;
-    ModelArray modelLons, modelLats;
+    std::vector<size_t> ij00, ij01, ij10, ij11;
+    ModelArray xi, eta;
 
     struct {
         TimePoint before = std::numeric_limits<TimePoint>::min(),
@@ -55,11 +73,20 @@ private:
         const std::vector<FloatType>& vectorIn2nd, std::vector<FloatType>& vectorOut1st,
         std::vector<FloatType>& vectorOut2nd);
 
-    void readRawForcing(std::map<std::string, std::vector<FloatType>>& rawDataBefore,
-        std::map<std::string, std::vector<FloatType>>& rawDataAfter);
+    void readRawForcing(RawDataMap& rawDataBefore, RawDataMap& rawDataAfter);
 
-    [[nodiscard]] std::map<std::string, std::vector<FloatType>> readRawData(
-        const std::string& fileName, size_t timeIndex) const;
+    [[nodiscard]] RawDataMap readRawData(const std::string& fileName,
+        const std::set<std::string>& fields, size_t timeIndex = 0) const;
+
+    [[nodiscard]] double wrapLon(const double lon) const
+    {
+        double wrappedLon = std::fmod(lon + 180., 360.);
+        if (wrappedLon < 0.0)
+            wrappedLon += 360.;
+
+        wrappedLon -= 180.;
+        return wrappedLon;
+    }
 };
 
 }
