b3dd83323217399c272f2a3a4a6dc4638df14e65
Einar Örn Ólason <einar.olason@nersc.no>
2026-08-03 16:54:12 +0200

    Ruff changes
    Ruff wants me to reformat things.

--- run/flood_TOPAZ.py
+++ run/flood_TOPAZ.py
@@ -1,6 +1,8 @@
-import glob, os
-import xarray as xr
+import glob
+import os
 from pathlib import Path
+
+import xarray as xr
 
 """
 Create a flooded version of TOPAZ input files. Assume we have a directory structure like this:
@@ -16,7 +18,7 @@ daily_mean
     ├── topaz_rean_200703.nc
     ├── topaz_rean_200704.nc
     └── topaz_rean_200705.nc
-    
+
 and create this:
 
 daily_mean_flooded
@@ -33,23 +35,27 @@ daily_mean_flooded
 """
 
 # Create an output directory. Fail if it exists
-out_dir = root_dir.rstrip('/') + '_flooded'
+out_dir = root_dir.rstrip("/") + "_flooded"
 os.makedirs(out_dir, exist_ok=False)
 
-# Just pick a random netCDF in "dir" to build the FloodInterpolator
-ds = xr.load_dataset(list(Path(root_dir).rglob("*.nc"))[0])
+# Just pick a random netCDF in "root_dir" to build the FloodInterpolator
+ds = xr.load_dataset(next(iter(Path(root_dir).rglob("*.nc"))))
 flooder = FloodInterpolator(ds.x.values, ds.y.values, ds.mask.values.astype(bool))
 ds.close()
 
-for dir in glob.glob(root_dir + '/*'):
-    basedir = os.path.basename(dir)
-    os.mkdir(out_dir + '/' + basedir)
-    for file in glob.glob(root_dir + '/' + basedir + '/*.nc'):
+for directory in glob.glob(root_dir + "/*"):
+    basedir = os.path.basename(directory)
+    os.mkdir(out_dir + "/" + basedir)
+    for file in glob.glob(root_dir + "/" + basedir + "/*.nc"):
         ds = xr.open_dataset(file)
         flooder.flood_dataset(ds)
-        flooder.flood_zeros(ds, ['vxo', 'vyo', 'vxsi', 'vysi'])
+        flooder.flood_zeros(ds, ["vxo", "vyo", "vxsi", "vysi"])
 
-        encoding = {var: {"zlib": True, "complevel": 9, "shuffle": True} for var in ds.data_vars}
+        encoding = {
+            var: {"zlib": True, "complevel": 9, "shuffle": True} for var in ds.data_vars
+        }
         print(os.path.join(out_dir, basedir, os.path.basename(file)))
-        ds.to_netcdf(os.path.join(out_dir, basedir, os.path.basename(file)), encoding=encoding)
+        ds.to_netcdf(
+            os.path.join(out_dir, basedir, os.path.basename(file)), encoding=encoding
+        )
         ds.close()
--- run/flooding.py
+++ run/flooding.py
@@ -13,12 +13,14 @@ class FloodInterpolator:
 
     def __init__(self, x, y, land_mask):
         """
-        Initialise an object containing the Delaunay triangulation used for flooding. Example usage:
+        Initialise an object containing the Delaunay triangulation used for flooding.
+
+        Example usage:
 
         >> from flooding import FloodInterpolator
         >> flooder = FloodInterpolator(ds.x.values, ds.y.values, ds.mask.values.astype(bool))
 
-        where ds is an xarray Dataset object containing the selected variables.
+        where ds is a xarray Dataset object containing the selected variables.
 
         Parameters
         ----------
@@ -29,13 +31,14 @@ class FloodInterpolator:
         land_mask : (ny,nx) bool
             True over land, False over ocean.
         """
-        self.A, self.ocean, self.land, self.valid = self.__build_flood_operator(x, y, land_mask)
+        self.A, self.ocean, self.land, self.valid = self.__build_flood_operator(
+            x, y, land_mask
+        )
         self.land_mask = land_mask
 
     def __build_flood_operator(self, x, y, land_mask):
         """
-        Construct a sparse interpolation operator that floods land cells
-        from neighbouring ocean cells using linear interpolation.
+        Construct a sparse interpolation operator that floods land cells from neighbouring ocean cells using linear interpolation.
 
         Parameters
         ----------
@@ -57,7 +60,6 @@ class FloodInterpolator:
         valid : bool array
             Land points inside the convex hull.
         """
-
         ocean = ~land_mask
         land = land_mask
 
@@ -82,7 +84,10 @@ class FloodInterpolator:
 
         rows = np.repeat(np.arange(valid.sum()), 3)
 
-        A = csr_matrix((weights.ravel(), (rows, vertices.ravel())), shape=(valid.sum(), ocean.sum()))
+        A = csr_matrix(
+            (weights.ravel(), (rows, vertices.ravel())),
+            shape=(valid.sum(), ocean.sum()),
+        )
 
         return A, ocean, land, valid
 
@@ -97,7 +102,6 @@ class FloodInterpolator:
             correspond to the grid used to build the interpolation operator.
             The input array is modified in place.
         """
-
         nland = self.land.sum()
 
         # Choose an appropriate floating-point dtype
@@ -105,13 +109,11 @@ class FloodInterpolator:
         filled_land = np.empty(nland, dtype=dtype)
 
         if data.ndim == 2:
-
             filled_land.fill(np.nan)
             filled_land[self.valid] = self.A @ data[self.ocean]
             data[self.land] = filled_land
 
         else:
-
             for index in np.ndindex(data.shape[:-2]):
                 field = data[index]
 
@@ -122,18 +124,18 @@ class FloodInterpolator:
 
         return data
 
-    def flood_dataset(self, ds, fields = "all"):
+    def flood_dataset(self, ds, fields="all"):
         """
-        Flood selected variables of an xarray dataset.
+        Flood selected variables of a xarray dataset.
 
         Parameters
         ----------
-        ds : xarray dataset to be flooded.
+        ds : xarray dataset
+            The dataset to be flooded.
         fields : iterable of str
             A list of the variable names to be flooded, or the keyword "all" to
             process all fields which have more than two dimensions.
         """
-
         my_fields = []
         if fields == "all":
             for var in list(ds.variables):
@@ -147,14 +149,14 @@ class FloodInterpolator:
 
     def flood_zeros(self, ds, fields):
         """
-        Flood selected variables of an xarray dataset with zeros
+        Flood selected variables of a xarray dataset with zeros.
 
         Parameters
         ----------
-        ds : xarray dataset to be flooded.
+        ds : xarray dataset
+            The dataset to be flooded.
         fields : iterable of str
             A list of the variable names to be flooded.
         """
-
         for var in fields:
             ds[var].values[np.stack([self.land_mask] * ds[var].values.shape[0])] = 0
