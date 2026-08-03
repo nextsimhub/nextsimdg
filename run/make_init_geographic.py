2283b726536239a068c45ebfad3f0d442a650b41
Einar Örn Ólason <einar.olason@nersc.no>
2026-08-03 16:30:48 +0200

    Update the masking in make_init_geographic.py
    This is to make sure that the new interpolation done in flooding.py fits
    with the land mask, i.e. that we don't get ocean cells in the land mask
    where there are NaNs in the forcing. Also, some minor fixes.

--- run/make_init_geographic.py
+++ run/make_init_geographic.py
@@ -1,8 +1,10 @@
-import time
-from pathlib import Path
+#! /usr/bin/env python3
 
 import netCDF4
 import numpy as np
+import time
+from pathlib import Path
+
 from interpolators import topaz4_interpolate
 from make_init_base import initMaker
 
@@ -75,13 +77,13 @@ if __name__ == "__main__":
 
     if args.grid_type == "regular":
         init_base.make_geographic_grid(
-            grid_name + ".nc", "p", plon_name="plon", plat_name="plat"
+            grid_file, "p", plon_name="plon", plat_name="plat"
         )
-        mask = netCDF4.Dataset(grid_name + ".nc", "r")
+        mask = netCDF4.Dataset(grid_file, "r")
         init_base.mask[:, :] = mask["mask"][:, :]
     elif args.grid_type == "NEMO":
         init_base.make_geographic_grid(
-            grid_name + ".nc",
+            grid_file,
             "ur",
             plon_name="glamt",
             plat_name="gphit",
@@ -169,3 +171,5 @@ if __name__ == "__main__":
         source_y,
         proj_string,
     )
+
+    init_base.mask[:, :] *= ~np.isnan(init_base.sst[:, :])
