80354d5749699baf4b957fe549d06b4a6a39df17
Einar Örn Ólason <einar.olason@nersc.no>
2026-08-04 07:47:29 +0200

    More Ruff changes

--- run/make_init_geographic.py
+++ run/make_init_geographic.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 
-import netCDF4
-import numpy as np
 import time
 from pathlib import Path
 
+import netCDF4
+import numpy as np
 from interpolators import topaz4_interpolate
 from make_init_base import initMaker
 
