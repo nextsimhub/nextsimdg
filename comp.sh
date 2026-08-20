441e837cd91db22eb2a2e9c9c1d87018d01820c7
Nirav Shah <nvs31@cpu-q-65.data.cluster>
2026-08-20 16:27:23 +0100

    comp.sh updated to run as single script

--- comp.sh
+++ comp.sh
@@ -5,6 +5,8 @@ export VENV_PATH=/home/${USER}/nextsimdg
 export SPACK_ROOT=/home/${USER}/spack
 export SPACK_ENV_NAME=nextsimdg-scorep-gcc
 export NEXTSIMDG_PATH=/home/${USER}/rds/rds-iccs-DKRMHAHoC3M/${USER}/nextsimdg
+export BENCHMARK_PATH=/home/${USER}/rds/rds-iccs-DKRMHAHoC3M/${USER}/nextsimdg/run/run_june23
+echo "$0 $1 $2"
 
 source ${VENV_PATH}/.venv/bin/activate # CHANGE ME
 source ${SPACK_ROOT}/share/spack/setup-env.sh # CHANGE ME
@@ -41,8 +43,8 @@ elif [[ "$1" == "--enable-openmp" ]]; then
 	echo "OpenMP build: -DENABLE_MPI=OFF -DWITH_THREADS=ON"
         cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CXX_COMPILER=$(which g++) -DENABLE_MPI=OFF -DWITH_THREADS=ON -DPython_EXECUTABLE=$(which python) -DCMAKE_BUILD_TYPE=Release
 elif [[ "$1" == "--enable-scorep-mpi" ]];then
-	echo "Score-P MPI Build: -DCMAKE_C_COMPILER=$(which scorep-mpicc) -DCMAKE_CXX_COMPILER=$(which scorep-mpicxx) -DENABLE_MPI=ON -DWITH_THREADS=OFF"
-	SCOREP_WRAPPER=off cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=$(which scorep-mpicc) -DCMAKE_CXX_COMPILER=$(which scorep-mpicxx) -DENABLE_MPI=ON -DWITH_THREADS=OFF -DPython_EXECUTABLE=$(which python) -DCMAKE_BUILD_TYPE=Release
+	echo "Score-P MPI Build: -DCMAKE_C_COMPILER=$(which scorep-mpicc) -DCMAKE_CXX_COMPILER=$(which scorep-mpic++) -DENABLE_MPI=ON -DWITH_THREADS=OFF"
+	SCOREP_WRAPPER=off cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=$(which scorep-mpicc) -DCMAKE_CXX_COMPILER=$(which scorep-mpic++) -DENABLE_MPI=ON -DWITH_THREADS=OFF -DPython_EXECUTABLE=$(which python) -DCMAKE_BUILD_TYPE=Release
 else
 	echo "Serial Build"
 	cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CXX_COMPILER=$(which g++) -DENABLE_MPI=OFF -DWITH_THREADS=OFF -DPython_EXECUTABLE=$(which python) -DCMAKE_BUILD_TYPE=Release
@@ -50,3 +52,27 @@ fi
 
 make -j 4
 make test
+
+cd ${BENCHMARK_PATH}
+
+for MPI_SIZE in 64
+do
+	        echo "MPI Size: ${MPI_SIZE}"
+		mpiexec -n ${MPI_SIZE} ${DD_PATH}/build/decomp -g ${BENCHMARK_PATH}/init_25km_NH.nc -x xdim -y ydim
+		ln -sf ${BENCHMARK_PATH}/partition_metadata_${MPI_SIZE}.nc ${BENCHMARK_PATH}/partition.nc
+		for NUM_THREADS in 1
+		do
+			export OMP_NUM_THREADS=${NUM_THREADS}
+			echo "MPI Size: ${MPI_SIZE}; Number of threads ${NUM_THREADS}, ${OMP_NUM_THREADS}"
+			if [[ "$1" == "--enable-scorep-mpi" ]]; then
+			       mpiexec -n ${MPI_SIZE} ${NEXTSIMDG_PATH}/build/nextsim --config-file config_june23.cfg
+			elif [[ "$1" == "--enable-mpi" ]]; then
+			       time mpiexec -n ${MPI_SIZE} ${NEXTSIMDG_PATH}/build/nextsim --config-file config_june23.cfg
+		        elif [[ "$1" == "--enable-openmp" ]]; then
+				time ${NEXTSIMDG_PATH}/build/nextsim --config-file config_june23.cfg
+			else
+				${NEXTSIMDG_PATH}/build/nextsim --config-file config_june23.cfg
+			fi
+                               			       
+		done
+done
