9a0f567bd05499d04ed395df82adf08bdc2764b6
Einar Örn Ólason <einar.olason@nersc.no>
2026-09-16 08:51:18 +0200

    Mask and toss out high-order DG components in output
    We don't need high-order DG components, except in restarts and some
    exceptional cases I haven't thought of. We could always add
    "output_DG_components" option later. Masking is always good.

--- core/src/modules/DiagnosticOutputModule/ConfigOutput.cpp
+++ core/src/modules/DiagnosticOutputModule/ConfigOutput.cpp
@@ -200,8 +200,21 @@ void ConfigOutput::outputState(const ModelState& diagState)
         && (everyTS || std::fmod(timeSinceOutput.seconds(), outputPeriod.seconds()) == 0.0_ft)) {
         Logged::info("ConfigOutput: Outputting " + std::to_string(state.data.size()) + " fields to "
             + currentFileName + " at " + meta.time().format() + "\n");
-        meta.affixCoordinates(state);
-        StructureFactory::fileFromState(state, currentFileName, false);
+
+        // Mask and toss out the higher order DG components
+        ModelState outputState = { .data = {}, .config = state.config };
+        for (const auto& [key, arrayRef] : state.data) {
+            auto modelArray = ModelArray(ModelArray::Type::H);
+            if (arrayRef.getType() == ModelArray::Type::DG)
+                modelArray.setData(arrayRef.component(0));
+            else
+                modelArray = arrayRef;
+
+            outputState.data[key] = mask(modelArray);
+        }
+
+        meta.affixCoordinates(outputState);
+        StructureFactory::fileFromState(outputState, currentFileName, false);
         lastOutput = meta.time();
     }
 }
